79ec8e61053918dd72023861b48ffad4a81d8871
chrwend <chwend@microsoft.com>
2016-06-15 11:51:30 -0700

    Add project structure with basic module setup (dynamic frameworks)

---
+++ AvalancheCore/AvalancheCore.h
@@ -0,0 +1,19 @@
+//
+//  AvalancheCore.h
+//  AvalancheCore
+//
+//  Created by Christoph Wendt on 6/15/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AvalancheCore.
+FOUNDATION_EXPORT double AvalancheCoreVersionNumber;
+
+//! Project version string for AvalancheCore.
+FOUNDATION_EXPORT const unsigned char AvalancheCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AvalancheCore/PublicHeader.h>
+
+
---
+++ AvalancheCrashes/AvalancheCrashes.h
@@ -0,0 +1,19 @@
+//
+//  AvalancheCrashes.h
+//  AvalancheCrashes
+//
+//  Created by Christoph Wendt on 6/15/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AvalancheCrashes.
+FOUNDATION_EXPORT double AvalancheCrashesVersionNumber;
+
+//! Project version string for AvalancheCrashes.
+FOUNDATION_EXPORT const unsigned char AvalancheCrashesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AvalancheCrashes/PublicHeader.h>
+
+
