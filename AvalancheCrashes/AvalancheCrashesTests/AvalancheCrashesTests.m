87b0967f4979b2bc545ded6b14248d36ee6b2e09
chrwend <chwend@microsoft.com>
2016-06-24 15:13:18 -0700

    Add crashes target to AvalancheCrashes

---
+++ AvalancheCrashes/AvalancheCrashesTests/AvalancheCrashesTests.m
@@ -0,0 +1,39 @@
+//
+//  AvalancheCrashesTests.m
+//  AvalancheCrashesTests
+//
+//  Created by Christoph Wendt on 6/24/16.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AvalancheCrashesTests : XCTestCase
+
+@end
+
+@implementation AvalancheCrashesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
