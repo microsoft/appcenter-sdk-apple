535f8ade4337a6ebf11ab2fe537585d8b5a2f220
Mehrdad Mozafari <memoz@microsoft.com>
2016-06-27 11:42:55 -0700

    Fixed Puppet app

--- AvalancheHub/AvalancheHub/AvalancheHub.h
+++ AvalancheHub/AvalancheHub/AvalancheHub.h
@@ -1,4 +1,9 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
 #import <Foundation/Foundation.h>
 
 #import "AVAAvalanche.h"
-//#import "AVALogging.h"
+#import "AVAConstants.h"
+#import "AVAFeature.h"
