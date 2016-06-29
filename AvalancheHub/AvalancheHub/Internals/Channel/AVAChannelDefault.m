70cafadf69ff6ca8cda8ad774a5f3351f8a23e27
chrwend <chwend@microsoft.com>
2016-06-29 10:16:07 -0700

    Add basic implementation of enqueueItem:

--- AvalancheHub/AvalancheHub/Internals/Channel/AVAChannelDefault.m
+++ AvalancheHub/AvalancheHub/Internals/Channel/AVAChannelDefault.m
@@ -36,6 +36,26 @@ static NSInteger const AVADefaultFlushInterval = 15;
 
 - (void)enqueueItem:(id<AVALog>) item {
   
+  if (!item) {
+    AVALogWarning(@"WARNING: TelemetryItem was nil.");
+    return;
+  }
+  
+  __weak typeof(self) weakSelf = self;
+  dispatch_async(self.dataItemsOperations, ^{
+    typeof(self) strongSelf = weakSelf;
+    
+    // TODO: Pass object to storage
+    
+    if (strongSelf->_itemsCount >= self.batchSize) {
+      
+      // TODO: Get batch from storage and forward it to sender
+      
+    } else if (strongSelf->_itemsCount == 1) {
+
+      // TODO: Start timer
+    }
+  });
 }
 
 @end
