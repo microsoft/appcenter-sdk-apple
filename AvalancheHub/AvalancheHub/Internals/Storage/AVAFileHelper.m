c61bb5019c261bb91718bc3046acc7a33c9fd044
chrwend <chwend@microsoft.com>
2016-06-29 18:01:16 -0700

    Set up implementation to use shared instance internally

--- AvalancheHub/AvalancheHub/Internals/Storage/AVAFileHelper.m
+++ AvalancheHub/AvalancheHub/Internals/Storage/AVAFileHelper.m
@@ -1,9 +1,54 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- */
-
 #import "AVAFileHelper.h"
 
+@interface AVAFileHelper ()
+
+@property(nonatomic, strong) NSFileManager *fileManager;
+
+@end
+
 @implementation AVAFileHelper
+
+#pragma mark - Initialisation
+
++ (id)sharedInstance {
+  static AVAFileHelper *sharedInstance = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    sharedInstance = [[self alloc] init];
+  });
+  return sharedInstance;
+}
+
++ (void)setFileManager:(NSFileManager *)fileManager {
+  [self.sharedInstance setFileManager:fileManager];
+}
+
+- (NSFileManager *)fileManager {
+  if (_fileManager) {
+    return _fileManager;
+  } else {
+    return [NSFileManager defaultManager];
+  }
+}
+
+#pragma mark - File I/O
+
++ (BOOL)appendData:(NSData *)data toFileWithPath:(NSString *)filePath {
+  return YES;
+}
+
++ (BOOL)deleteFileWithPath:(NSString *)filePath {
+
+  return YES;
+}
+
++ (NSData *)dataForFileWithPath:(NSString *)filePath {
+  return nil;
+}
+
++ (NSArray *)fileNamesForDirectory:(NSString *)directoryPath
+                 withFileExtension:(NSString *)fileExtension {
+  return nil;
+}
 
 @end
