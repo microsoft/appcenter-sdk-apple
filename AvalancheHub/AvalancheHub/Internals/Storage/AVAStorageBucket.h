dc53e5deb889fcee71b6e85cef13d6c64cdb1514
chrwend <chwend@microsoft.com>
2016-07-05 10:05:32 -0700

    Add model that manages pending and available files

---
+++ AvalancheHub/AvalancheHub/Internals/Storage/AVAStorageBucket.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AVAStorageBucket : NSObject
+
+/**
+ * The path to a file which contains events for the current batch.
+ */
+@property(nonatomic, copy) NSString *currentFilePath;
+
+/**
+ * A list of file names that are currently used by other components.
+ */
+@property(nonatomic, strong) NSMutableArray *blockedFiles;
+
+/**
+ * A list of file names that can be accessed by other components.
+ */
+@property(nonatomic, strong) NSMutableArray *availableFiles;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AvalancheHub/AvalancheHub/Internals/Storage/AVAStorageBucket.m
@@ -0,0 +1,5 @@
+#import "AVAStorageBucket.h"
+
+@implementation AVAStorageBucket
+
+@end
