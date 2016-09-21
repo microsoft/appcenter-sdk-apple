43dfbfa1a187108428bf56175022d7f7fd88ffb3
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2016-09-21 15:35:48 -0700

    Fix anonymization of paths

---
+++ SonomaCrashes/SonomaCrashes/Internals/Utils/SNMErrorLogFormatterPrivate.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+
+#import "SNMErrorLogFormatter.h"
+
+#ifndef SNMErrorLogFormatterPrivate_h
+#define SNMErrorLogFormatterPrivate_h
+
+@interface SNMErrorLogFormatter ()
+
++ (NSString *)anonymizedPathFromPath:(NSString *)path;
+
+@end
+
+
+#endif /* SNMErrorLogFormatterPrivate_h */
