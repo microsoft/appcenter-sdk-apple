bb5d31ec924d92996f36d778a5976340f33fbf87
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2016-09-21 23:25:00 -0700

    move isDebuggerAttached from SNMCrashes to SNMCore.

--- SonomaCrashes/SonomaCrashes/Internals/Utils/SNMCrashesHelper.m
+++ SonomaCrashes/SonomaCrashes/Internals/Utils/SNMCrashesHelper.m
@@ -3,7 +3,6 @@
  */
 
 #import "SNMCrashesHelper.h"
-#import <sys/sysctl.h>
 
 static NSString *const kSNMCrashesDirectory = @"com.microsoft.sonoma/crashes";
 
@@ -55,42 +54,6 @@ NSString *snm_crashesDir(void);
   });
 
   return isRunningInAppExtension;
-}
-
-/**
- * Check if the debugger is attached
- *
- * Taken from
- * https://github.com/plausiblelabs/plcrashreporter/blob/2dd862ce049e6f43feb355308dfc710f3af54c4d/Source/Crash%20Demo/main.m#L96
- *
- * @return `YES` if the debugger is attached to the current process, `NO`
- * otherwise
- */
-+ (BOOL)isDebuggerAttached {
-  static BOOL debuggerIsAttached = NO;
-
-  static dispatch_once_t debuggerPredicate;
-  dispatch_once(&debuggerPredicate, ^{
-    struct kinfo_proc info;
-    size_t info_size = sizeof(info);
-    int name[4];
-
-    name[0] = CTL_KERN;
-    name[1] = KERN_PROC;
-    name[2] = KERN_PROC_PID;
-    name[3] = getpid();
-
-    if (sysctl(name, 4, &info, &info_size, NULL, 0) == -1) {
-      NSLog(@"[SNMCrashes] ERROR: Checking for a running debugger via sysctl() "
-            @"failed.");
-      debuggerIsAttached = false;
-    }
-
-    if (!debuggerIsAttached && (info.kp_proc.p_flag & P_TRACED) != 0)
-      debuggerIsAttached = true;
-  });
-
-  return debuggerIsAttached;
 }
 
 @end
