5550ee95dc6bc1cf49dac7a162a14286e71fbd30
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2016-11-03 01:44:26 -0700

    SonomaCrashes => MobileCenterCrashes

--- Puppet/Puppet/AppDelegate.m
+++ Puppet/Puppet/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-  // Start Sonoma SDK.
+  // Start Mobile Center SDK.
   [MSMobileCenter setLogLevel:MSLogLevelVerbose];
 
   [MSMobileCenter start:@"7dfb022a-17b5-4d4a-9c75-12bc3ef5e6b7" withFeatures:@[[MSAnalytics class], [MSCrashes class]]];
