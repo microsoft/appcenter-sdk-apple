87dcd565b9b2978238da7e06c3d00169485496a1
Jae Lim <jaelim@microsoft.com>
2016-11-04 11:14:26 -0700

    Disabling trackPage in Analytics

--- MobileCenterAnalytics/MobileCenterAnalytics/Internals/MSAnalyticsInternal.h
+++ MobileCenterAnalytics/MobileCenterAnalytics/Internals/MSAnalyticsInternal.h
@@ -7,4 +7,35 @@
 
 @interface MSAnalytics () <MSFeatureInternal>
 
+// Temporarily hiding trakcing page feature.
+/**
+ *  Track a page.
+ *
+ *  @param pageName  page name.
+ */
++ (void)trackPage:(NSString *)pageName;
+
+/**
+ *  Track a page.
+ *
+ *  @param pageName  page name.
+ *  @param properties dictionary of properties.
+ */
++ (void)trackPage:(NSString *)pageName withProperties:(NSDictionary *)properties;
+
+/**
+ *  Set the page auto-tracking property.
+ *
+ *  @param isEnabled is page tracking enabled or disabled.
+ */
+
++ (void)setAutoPageTrackingEnabled:(BOOL)isEnabled;
+
+/**
+ *  Indicate if auto page tracking is enabled or not.
+ *
+ *  @return YES is page tracking is enabled and NO if disabled.
+ */
++ (BOOL)isAutoPageTrackingEnabled;
+
 @end
