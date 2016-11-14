0eab07edee86a6cdec03ae878bd338bf3d2d1a47
Clement Polet <clpolet@microsoft.com>
2016-11-14 10:24:52 -0800

    Add unit tests for the MSApplicationHelper class

--- MobileCenter/MobileCenter/Internals/Utils/MSApplicationHelper.m
+++ MobileCenter/MobileCenter/Internals/Utils/MSApplicationHelper.m
@@ -12,13 +12,17 @@
 
   // App extentions must not access sharedApplication.
   if (!MS_IS_APP_EXTENSION) {
-
-    // Compute selector at runtime for more discretion.
-    SEL sharedAppSel = NSSelectorFromString(@"sharedApplication");
-    return [[((UIApplication * (*)(id, SEL))[[UIApplication class] methodForSelector:sharedAppSel])(
-        [UIApplication class], sharedAppSel) valueForKey:@"applicationState"] intValue];
+    return (MSApplicationState)[[self class] sharedAppState];
   }
   return MSApplicationStateUnknown;
+}
+
++ (UIApplicationState)sharedAppState {
+
+  // Compute selector at runtime for more discretion.
+  SEL sharedAppSel = NSSelectorFromString(@"sharedApplication");
+  return [[((UIApplication * (*)(id, SEL))[[UIApplication class] methodForSelector:sharedAppSel])(
+      [UIApplication class], sharedAppSel) valueForKey:@"applicationState"] longValue];
 }
 
 @end
