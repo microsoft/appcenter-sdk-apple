99e04541a3d9efdc126a19f66c40e1868954a49f
Vineet Choudhary <vineetchoudhary@live.in>
2016-11-23 12:42:45 +0530

    Redesign swift demo

---
+++ SwiftDemo/SwiftDemo/MainViewController/ViewController.swift
@@ -0,0 +1,182 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by Benjamin Reimold on 11/4/16.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+import MobileCenter
+import MobileCenterAnalytics
+import MobileCenterCrashes
+
+class ViewController: UIViewController {
+    enum MSCellType : Int{
+        case Title, Switch, Details
+    }
+    
+    enum MSAnalyticsCases : Int {
+        case SetEnabled, TrackEvent, TrackEventWithProperties, TrackPage, TrackPageWithProperties
+        
+        var cellSetting : (title:String, type:MSCellType){
+            switch self {
+            case .SetEnabled:
+                return ("Set Enabled", .Switch)
+            case .TrackEvent:
+                return ("Track Event", .Details)
+            case .TrackEventWithProperties:
+                return ("Track Event with Properties", .Details)
+            case .TrackPage:
+                return ("Track Page", .Details)
+            case .TrackPageWithProperties:
+                return ("Track Page with Properties", .Details)
+            }
+        }
+        
+        static let allCases = [MSAnalyticsCases.SetEnabled, MSAnalyticsCases.TrackEvent, MSAnalyticsCases.TrackEventWithProperties,MSAnalyticsCases.TrackPage, MSAnalyticsCases.TrackPageWithProperties]
+    }
+    
+    enum MSCrashesCases : Int{
+        case SetEnabled, GenerateTestCrash, AppCrashInLastSession
+        var cellSetting : (title:String, type:MSCellType){
+            switch self {
+            case .SetEnabled:
+                return ("Set Enabled", .Switch)
+            case .GenerateTestCrash:
+                return ("Generate Test Crash", .Details)
+            case .AppCrashInLastSession:
+                return ("App Crash in Last Session", .Details)
+            }
+        }
+        
+        static let allCases = [MSCrashesCases.SetEnabled, MSCrashesCases.GenerateTestCrash, MSCrashesCases.AppCrashInLastSession]
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.estimatedRowHeight = 50;
+        self.tableView.sectionHeaderHeight = 50;
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.register(MSTitleTableViewCell.nib(), forCellReuseIdentifier: MSTitleTableViewCell.name())
+        self.tableView.register(MSSwitchTableViewCell.nib(), forCellReuseIdentifier: MSSwitchTableViewCell.name())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func showAlertWithMessage(title:String, message:String){
+        let alert = UIAlertController.init(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
+
+extension ViewController : UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? MSAnalyticsCases.allCases.count : MSCrashesCases.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellSetting = indexPath.section == 0 ?  MSAnalyticsCases.allCases[indexPath.row].cellSetting : MSCrashesCases.allCases[indexPath.row].cellSetting
+        if cellSetting.type == .Switch{
+            if let cell = tableView.dequeueReusableCell(withIdentifier: MSSwitchTableViewCell.name(), for: indexPath) as? MSSwitchTableViewCell{
+                cell.titleNameLabel.text = cellSetting.title
+                return cell;
+            }
+        }else{
+            if let cell = tableView.dequeueReusableCell(withIdentifier: MSTitleTableViewCell.name(), for: indexPath) as? MSTitleTableViewCell{
+                cell.titleNameLabel.text = cellSetting.title
+                return cell;
+            }
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            return "Analytics"
+        }else{
+            return "Crashes"
+        }
+    }
+}
+
+extension ViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath.section == 0{
+            switch MSAnalyticsCases.allCases[indexPath.row] {
+            //Track Event
+            case .SetEnabled:
+                //Enable/Disable MSAnalytics
+                MSAnalytics.setEnabled(!MSAnalytics.isEnabled())
+                
+            case .TrackEvent:
+                //Track event with name only
+                MSAnalytics.trackEvent("Row Clicked")
+                
+            case .TrackEventWithProperties:
+                //Track Event with Properties
+                MSAnalytics.trackEvent("Row Clicked", withProperties: ["Name" : "Track Event", "Row Number" : "\(indexPath.row)"])
+                
+            case .TrackPage:
+                
+                break
+                
+            case .TrackPageWithProperties:
+                break
+            }
+        }else{
+            switch MSCrashesCases.allCases[indexPath.row] {
+            case .SetEnabled:
+                MSCrashes.setEnabled(!MSCrashes.isEnabled())
+
+            case .GenerateTestCrash:
+                //Test either debugger attached
+                if MSMobileCenter.isDebuggerAttached(){
+                    self.showAlertWithMessage(title: "", message: "Detecting crashes is NOT enabled due to running the app with a debugger attached.")
+                }else{
+                    //Generate Crash
+                    MSCrashes.generateTestCrash()
+                }
+            case .AppCrashInLastSession:
+                //Check either app was crashed in last session
+                let message = "App \(MSCrashes.hasCrashedInLastSession() ? "was" : "wasn't") crashed in last session"
+                let alert = UIAlertController.init(title: "", message: message, preferredStyle: .alert)
+                if MSCrashes.hasCrashedInLastSession(){
+                    alert.addAction(UIAlertAction(title: "Show Description", style: .default, handler: { (alert) in
+                        
+                        //Get last session crash report
+                        let report = MSCrashes.lastSessionCrashReport()
+                        var message = ""
+                        if let appVersion = report?.device.appVersion{
+                            message.append("App Version - \(appVersion) \n\n")
+                        }
+                        if let appErrorTime = report?.appErrorTime{
+                            message.append("App Error Time - \(appErrorTime) \n\n")
+                        }
+                        if let appStartTime = report?.appStartTime{
+                            message.append("App Start Time - \(appStartTime) \n\n")
+                        }
+                        message.append("MSCrashes.lastSessionCrashReport() will provides you more details about the crash that occurred in the last app session")
+                        self.showAlertWithMessage(title: "Last Session Crash Report", message: message)
+                    }))
+                }
+                alert.addAction(UIAlertAction(title: "Okay", style: .cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                break
+            }
+        }
+    }
+}
