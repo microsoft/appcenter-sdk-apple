a959bf97ed3a0b021e4005b17faafaf55f3b3e9c
Jae Lim <jaelim@jaes-mbp.guest.corp.microsoft.com>
2016-11-30 15:47:24 -0800

    Add script to publish podspec to CocoaPods

--- bitrise/publish-internal-podspec.sh
+++ bitrise/publish-internal-podspec.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
-## I. Get publish version for information
+## I. Check parameter
+if [ -z $1 ] || ( [ "$1" != "internal" ] && [ "$1" != "external" ] ); then
+  echo "Invalid parameter.";
+  echo "  Usage: $0 {internal|external}";
+  exit 1;
+fi
+
+## II. Get publish version for information
 publish_version="$(grep "VERSION_STRING" $BITRISE_SOURCE_DIR/$VERSION_FILE | head -1 | awk -F "[= ]" '{print $4}')"
 echo "Publishing podspec for version" $publish_version
 
-## II. Get path of internal podspec local repo
-local_repo_path="$(pod repo | grep "$GIT_SPEC_REPO_NAME" | grep Path | head -1 | awk -F ": " '{print $2}')"
+if [ "$1" == "internal" ]; then
 
-## III. Update podspec to the internal podspec local repo
-resp="$(pod repo push $GIT_SPEC_REPO_NAME $BITRISE_SOURCE_DIR/$PODSPEC_FILENAME --allow-warnings)"
-echo $resp
-error="$(echo $resp | grep -i error\|fatal)"
-if [ "$error" ]; then
-  echo "Cannot publish to private repo"
-  exit 1
+  ## 1. Get path of internal podspec local repo
+  local_repo_path="$(pod repo | grep "$GIT_SPEC_REPO_NAME" | grep Path | head -1 | awk -F ": " '{print $2}')"
+
+  ## 2. Update podspec to the internal podspec local repo
+  resp="$(pod repo push $GIT_SPEC_REPO_NAME $BITRISE_SOURCE_DIR/$PODSPEC_FILENAME --allow-warnings)"
+  echo $resp
+
+  # Check error from the response
+  error="$(echo $resp | grep -i error\|fatal)"
+  if [ "$error" ]; then
+    echo "Cannot publish to internal repo"
+    exit 1
+  fi
+
+  ## 3. Push podspec to the internal podspec remote repo
+  cd $local_repo_path
+  git push
+  cd $BITRISE_SOURCE_DIR
+
+  echo "Podspec published to internal repo successfully"
+
+else
+
+  ## 1. Run lint to validate podspec. This step will automatically push the spec to CocoaPods if the spec is valid
+  resp="$(pod spec lint $BITRISE_SOURCE_DIR/$PODSPEC_FILENAME --allow-warnings)"
+  echo $resp
+
+  # Check error from the response
+  error="$(echo $resp | grep -i error\|fatal)"
+  if [ "$error" ]; then
+    echo "Cannot publish to CocoaPods"
+    exit 1
+  fi
+
+  echo "Podspec published to CocoaPods successfully"
+
 fi
-
-## IV. Push podspec to the interanl podspec remote repo
-cd $local_repo_path
-git push
-cd $BITRISE_SOURCE_DIR
