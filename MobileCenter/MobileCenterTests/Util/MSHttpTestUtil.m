4f846058204cea665ceab115b8ea66603fe42a9b
Clement Polet <clpolet@microsoft.com>
2017-01-11 15:41:36 -0800

    Fix sender unit tests

--- MobileCenter/MobileCenterTests/Util/MSHttpTestUtil.m
+++ MobileCenter/MobileCenterTests/Util/MSHttpTestUtil.m
@@ -1,7 +1,7 @@
-#import "MSHttpTestUtil.h"
 #import "MSConstants+Internal.h"
+#import "MSHttpTestUtil.h"
 
-static NSTimeInterval const kMSStubbedResponseTimeout = 100.0;
+static NSTimeInterval const kMSStubbedResponseTimeout = UID_MAX;
 static NSString *const kMSStub500Name = @"httpStub_500";
 static NSString *const kMSStub200Name = @"httpStub_200";
 static NSString *const kMSStubNetworkDownName = @"httpStub_NetworkDown";
@@ -9,52 +9,51 @@ static NSString *const kMSStubLongResponseTimeOutName = @"httpStub_LongResponseT
 
 @implementation MSHttpTestUtil
 
-+(void)stubHttp500Response{
-  [[self class]stubResponseWithCode:MSHTTPCodesNo500InternalServerError name:kMSStub500Name];
++ (void)stubHttp500Response {
+  [[self class] stubResponseWithCode:MSHTTPCodesNo500InternalServerError name:kMSStub500Name];
 }
 
-+(void)stubHttp200Response{
-  [[self class]stubResponseWithCode:MSHTTPCodesNo200OK name:kMSStub200Name];
++ (void)stubHttp200Response {
+  [[self class] stubResponseWithCode:MSHTTPCodesNo200OK name:kMSStub200Name];
 }
 
-+(void)stubNetworkDownResponse{
-  NSError *error =
-  [NSError errorWithDomain:NSURLErrorDomain code:kCFURLErrorNotConnectedToInternet userInfo:nil];
-  [[self class]stubResponseWithError:error name:kMSStubNetworkDownName];
++ (void)stubNetworkDownResponse {
+  NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:kCFURLErrorNotConnectedToInternet userInfo:nil];
+  [[self class] stubResponseWithError:error name:kMSStubNetworkDownName];
 }
 
-+(void)stubLongTimeOutResponse{
++ (void)stubLongTimeOutResponse {
   [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
     return YES;
   }
-                      withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-                        OHHTTPStubsResponse *responseStub = [OHHTTPStubsResponse new];
-                        responseStub.statusCode = MSHTTPCodesNo200OK;
-                        return [responseStub responseTime:kMSStubbedResponseTimeout];
-                      }].name = kMSStubLongResponseTimeOutName;
+      withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+        OHHTTPStubsResponse *responseStub = [OHHTTPStubsResponse new];
+        responseStub.statusCode = MSHTTPCodesNo200OK;
+        return [responseStub responseTime:kMSStubbedResponseTimeout];
+      }]
+      .name = kMSStubLongResponseTimeOutName;
 }
 
-+(void)stubResponseWithCode:(NSInteger)code name:(NSString*)aName{
++ (void)stubResponseWithCode:(NSInteger)code name:(NSString *)aName {
   [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
     return YES;
   }
-                      withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-                        OHHTTPStubsResponse *responseStub = [OHHTTPStubsResponse new];
-                        responseStub.statusCode = code;
-                        return responseStub;
-                      }]
-  .name = aName;
+      withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+        OHHTTPStubsResponse *responseStub = [OHHTTPStubsResponse new];
+        responseStub.statusCode = (int)code;
+        return responseStub;
+      }]
+      .name = aName;
 }
 
-+(void)stubResponseWithError:(NSError*)error name:(NSString*)aName{
++ (void)stubResponseWithError:(NSError *)error name:(NSString *)aName {
   [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
     return YES;
   }
-                      withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-                        return [OHHTTPStubsResponse responseWithError:error];
-                      }]
-  .name = aName;
+      withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
+        return [OHHTTPStubsResponse responseWithError:error];
+      }]
+      .name = aName;
 }
-
 
 @end
