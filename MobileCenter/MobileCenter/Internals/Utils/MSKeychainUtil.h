95fddd4de3c7244910d8793197a83006abe2774e
Jae Lim <jaelim@Jaes-Macbook-Pro.local>
2017-02-01 09:37:45 -0800

    Refactor method signatures

--- MobileCenter/MobileCenter/Internals/Utils/MSKeychainUtil.h
+++ MobileCenter/MobileCenter/Internals/Utils/MSKeychainUtil.h
@@ -43,36 +43,36 @@
  *
  * @param string A string data to be placed in Keychain.
  * @param key A unique key for the data.
- * @param serviceName A service name for the key.
+ * @param service A service name for the key.
  * @return YES if stored successfully, NO otherwise.
  */
-+ (BOOL)storeString:(NSString *)string forKey:(NSString *)key andService:(NSString *)serviceName;
++ (BOOL)storeString:(NSString *)string forKey:(NSString *)key service:(NSString *)service;
 
 /**
  * Delete a key and a string for the given service name.
  *
  * @param key A unique key for the data.
- * @param serviceName A service name for the key.
+ * @param service A service name for the key.
  * @return A string data that was deleted.
  */
-+ (NSString *)deleteStringForKey:(NSString *)key andService:(NSString *)serviceName;
++ (NSString *)deleteStringForKey:(NSString *)key service:(NSString *)service;
 
 /**
  * Get a string from Keychain with the given key and service name.
  *
  * @param key A unique key for the data.
- * @param serviceName A service name for the key.
+ * @param service A service name for the key.
  * @return A string data if exists.
  */
-+ (NSString *)stringForKey:(NSString *)key andService:(NSString *)serviceName;
++ (NSString *)stringForKey:(NSString *)key service:(NSString *)service;
 
 /**
  * Clear all keys and strings associated with the given service name.
  *
- * @param serviceName A service name for keys.
+ * @param service A service name for keys.
  * @return YES if cleared successfully, NO otherwise.
  */
-+ (BOOL)clearForService:(NSString *)serviceName;
++ (BOOL)clearForService:(NSString *)service;
 
 @end
 
