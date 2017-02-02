699b07ef6e20be45c5c0362f53089ab7e7f3a4ce
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-02-01 16:45:59 -0800

    Create separate directory for buffered logs

--- MobileCenterCrashes/MobileCenterCrashes/Internals/Utils/MSCrashesUtil.h
+++ MobileCenterCrashes/MobileCenterCrashes/Internals/Utils/MSCrashesUtil.h
@@ -14,6 +14,14 @@
 + (NSString *)crashesDir;
 
 /**
+ * Returns the directory for storing and reading buffered logs. It will be used in case we crash to make sure we don't
+ * loose any data.
+ *
+ * @return The directory containing buffered events for an app
+ */
++ (NSString *)logBufferDir;
+
+/**
  * Determines if the SDK is used inside an app extension.
  *
  * @return YES, if the SDK is used as inside an app extension.
