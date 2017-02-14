9c91626bcfdb4a4d18fd82924ced0b2ddb273c61
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-02-13 22:46:35 -0800

    Cleanup #ifndef

--- MobileCenter/MobileCenter/Internals/Utils/MSConstants+Internal.h
+++ MobileCenter/MobileCenter/Internals/Utils/MSConstants+Internal.h
@@ -2,8 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  */
 
-#ifndef MSConstants_Internal_h
-#define MSConstants_Internal_h
 #import <Foundation/Foundation.h>
 
 // Device manufacturer
@@ -127,5 +125,3 @@ typedef NS_ENUM(NSInteger, MSHTTPCodesNo) {
   MSHTTPCodesNo598NetworkReadTimeoutErrorUnknown = 598,
   MSHTTPCodesNo599NetworkConnectTimeoutErrorUnknown = 599
 };
-
-#endif /* MSConstants_Internal_h */
--- MobileCenter/MobileCenter/Internals/Utils/MSUtil.h
+++ MobileCenter/MobileCenter/Internals/Utils/MSUtil.h
@@ -5,9 +5,6 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#ifndef MSUtil_h
-#define MSUtil_h
-
 #define MS_USER_DEFAULTS [MSUserDefaults shared]
 #define MS_NOTIFICATION_CENTER [NSNotificationCenter defaultCenter]
 #define MS_DEVICE [UIDevice currentDevice]
@@ -16,7 +13,6 @@
 #define MS_LOCALE [NSLocale currentLocale]
 #define MS_CLASS_NAME_WITHOUT_PREFIX [NSStringFromClass([self class]) substringFromIndex:2]
 #define MS_IS_APP_EXTENSION [[[NSBundle mainBundle] executablePath] containsString:@".appex/"]
-#endif /* MSUtil_h */
 
 
 /**
