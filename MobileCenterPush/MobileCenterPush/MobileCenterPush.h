38707910f8dee8de097736459e608c61917ddf6c
Rychagov Evgeny <erychagov@gmail.com>
2017-02-20 15:02:53 +0300

    Add new empty project

---
+++ MobileCenterPush/MobileCenterPush/MobileCenterPush.h
@@ -0,0 +1,13 @@
+//
+//  MobileCenterPush.h
+//  MobileCenterPush
+//
+//  Created by Евгений Рычагов on 20/02/2017.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MobileCenterPush : NSObject
+
+@end
---
+++ MobileCenterPush/MobileCenterPush/MobileCenterPush.m
@@ -0,0 +1,13 @@
+//
+//  MobileCenterPush.m
+//  MobileCenterPush
+//
+//  Created by Евгений Рычагов on 20/02/2017.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import "MobileCenterPush.h"
+
+@implementation MobileCenterPush
+
+@end
