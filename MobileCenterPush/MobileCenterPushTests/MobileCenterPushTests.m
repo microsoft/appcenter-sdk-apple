a37c064d0e8f10655b6e77a728e4c5600d55b83d
Rychagov Evgeny <erychagov@gmail.com>
2017-02-21 13:36:01 +0300

    Added tests

---
+++ MobileCenterPush/MobileCenterPushTests/MobileCenterPushTests.m
@@ -0,0 +1,39 @@
+//
+//  MobileCenterPushTests.m
+//  MobileCenterPushTests
+//
+//  Created by Евгений Рычагов on 21/02/2017.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MobileCenterPushTests : XCTestCase
+
+@end
+
+@implementation MobileCenterPushTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
