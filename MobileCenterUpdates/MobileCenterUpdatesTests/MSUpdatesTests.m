6723ecb3ee3bdd58b0d1dc4f10366a93c642eae0
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-02-23 14:25:38 -0800

    Add unit test and delete duplicate reference

--- MobileCenterUpdates/MobileCenterUpdatesTests/MSUpdatesTests.m
+++ MobileCenterUpdates/MobileCenterUpdatesTests/MSUpdatesTests.m
@@ -1,4 +1,5 @@
 #import <XCTest/XCTest.h>
+#import "MSUpdateUtil.h"
 
 @interface MobileCenterUpdatesTests : XCTestCase
 
@@ -6,15 +7,34 @@
 
 @implementation MobileCenterUpdatesTests
 
-- (void)setUp {
-    [super setUp];
+- (void)testGetMainBundle {
+  
+  // When
+  NSBundle *bundle = MSUpdatesBundle();
+  
+  // Then
+  XCTAssertNotNil(bundle);
 }
 
-- (void)tearDown {
-    [super tearDown];
-}
-
-- (void)testExample {
+- (void)testLocalizedString {
+  
+  // When
+  NSString *test = MSUpdatesLocalizedString(@"");
+  
+  // Then
+  XCTAssertTrue([test isEqualToString:@""]);
+  
+  // When
+  test = MSUpdatesLocalizedString(@"NonExistendString");
+  
+  // Then
+  XCTAssertTrue([test isEqualToString:@"NonExistendString"]);
+  
+  // When
+  test = MSUpdatesLocalizedString(@"Working");
+  
+  // Then
+  XCTAssertTrue([test isEqualToString:@"Yes, this works!"]);
 }
 
 @end
