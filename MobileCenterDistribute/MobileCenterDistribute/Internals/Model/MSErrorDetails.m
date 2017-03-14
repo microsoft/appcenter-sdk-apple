1b0de61286e26eab83304a459a66cd94e32fb3c3
Jae Lim <jaelim@microsoft.com>
2017-03-14 15:21:11 -0700

    Add error details data model

---
+++ MobileCenterDistribute/MobileCenterDistribute/Internals/Model/MSErrorDetails.m
@@ -0,0 +1,20 @@
+#import "MSErrorDetails.h"
+
+static NSString *const kMSCode = @"code";
+static NSString *const kMSMessage = @"message";
+
+@implementation MSErrorDetails
+
+- (instancetype)initWithDictionary:(NSMutableDictionary *)dictionary {
+  if ((self = [super init])) {
+    if (dictionary[kMSCode]) {
+      self.code = dictionary[kMSCode];
+    }
+    if (dictionary[kMSMessage]) {
+      self.message = dictionary[kMSMessage];
+    }
+  }
+  return self;
+}
+
+@end
