c7d7a690ec751d6419cd783c518636296cab3abf
Ivan Matkov <v-ivmatk@microsoft.com>
2017-03-29 16:14:38 +0300

    Fix distribute alerts in puppet after merge

--- Puppet/Puppet/MSDistributeViewController.m
+++ Puppet/Puppet/MSDistributeViewController.m
@@ -1,5 +1,6 @@
 #import "MSDistributeViewController.h"
 #import "MobileCenterDistribute.h"
+#import "MSDistributePrivate.h"
 
 @interface MSDistributeViewController ()
 
@@ -26,6 +27,25 @@
 }
 
 #pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
+  
+  switch ([indexPath section]) {
+      
+    // Section with alerts.
+    case 0: {
+      switch (indexPath.row) {
+        case 0:
+          [[MSDistribute sharedInstance] showConfirmationAlert:nil];
+          break;
+        case 1:
+          [[MSDistribute sharedInstance] showDistributeDisabledAlert];
+          break;
+      }
+    }
+  }
+}
 
 - (IBAction)enabledSwitchUpdated:(UISwitch *)sender {
   [MSDistribute setEnabled:sender.on];
