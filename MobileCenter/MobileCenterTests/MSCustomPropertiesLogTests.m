f1d955d16e4f54de0ae715b729759fd56a682455
Ivan Matkov <v-ivmatk@microsoft.com>
2017-03-31 14:38:46 +0300

    Add custom properties serialization tests

--- MobileCenter/MobileCenterTests/MSCustomPropertiesLogTests.m
+++ MobileCenter/MobileCenterTests/MSCustomPropertiesLogTests.m
@@ -1,14 +1,81 @@
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
 #import <XCTest/XCTest.h>
 
+#import "MSCustomPropertiesLog.h"
+
 @interface MSCustomPropertiesLogTests : XCTestCase
+
+@property(nonatomic, strong) MSCustomPropertiesLog *sut;
 
 @end
 
 @implementation MSCustomPropertiesLogTests
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+@synthesize sut = _sut;
+
+#pragma mark - Setup
+
+- (void)setUp {
+  [super setUp];
+  self.sut = [MSCustomPropertiesLog new];
+}
+
+#pragma mark - Tests
+
+- (void)testSerializingEventToDictionaryWorks {
+  
+  // If
+  NSString *string = @"test";
+  NSDate *date = [NSDate dateWithTimeIntervalSince1970:0];
+  NSNumber *number = @0;
+  BOOL boolean = NO;
+  NSDictionary<NSString *, NSObject *> *properties = @{@"t1": string,
+                                                       @"t2": date,
+                                                       @"t3": number,
+                                                       @"t4": @(boolean),
+                                                       @"t5": [NSNull null],
+                                                       @"t6": [NSData new]};
+  self.sut.properties = properties;
+  
+  // When
+  NSMutableDictionary *actual = [self.sut serializeToDictionary];
+  
+  // Then
+  assertThat(actual, notNilValue());
+  NSArray *actualProperties = actual[@"properties"];
+  assertThat(actualProperties, hasCountOf(5));
+}
+
+- (void)testNSCodingSerializationAndDeserializationWorks {
+  
+  // If
+  NSString *string = @"test";
+  NSDate *date = [NSDate dateWithTimeIntervalSince1970:0];
+  NSNumber *number = @0;
+  BOOL boolean = NO;
+  NSDictionary<NSString *, NSObject *> *properties = @{@"t1": string,
+                                                       @"t2": date,
+                                                       @"t3": number,
+                                                       @"t4": @(boolean),
+                                                       @"t5": [NSNull null]};
+  self.sut.properties = properties;
+  
+  // When
+  NSData *serializedLog = [NSKeyedArchiver archivedDataWithRootObject:self.sut];
+  id actual = [NSKeyedUnarchiver unarchiveObjectWithData:serializedLog];
+  
+  // Then
+  assertThat(actual, notNilValue());
+  assertThat(actual, instanceOf([MSCustomPropertiesLog class]));
+  
+  MSCustomPropertiesLog *log = actual;
+  NSDictionary<NSString *, NSObject *> *actualProperties = log.properties;
+  XCTAssertEqual(actualProperties.count, properties.count);
+  for (NSString *key in actualProperties) {
+    NSObject *actualValue = [actualProperties objectForKey:key];
+    NSObject *value = [properties objectForKey:key];
+    assertThat(actualValue, equalTo(value));
+  }
 }
 
 @end
