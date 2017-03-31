b57b4b73406051547ecd0e47952e4d622cf10368
Ivan Matkov <v-ivmatk@microsoft.com>
2017-03-31 15:32:53 +0300

    Add comments to public custom properties api

--- MobileCenter/MobileCenter/MSCustomProperties.h
+++ MobileCenter/MobileCenter/MSCustomProperties.h
@@ -1,15 +1,61 @@
 #import <Foundation/Foundation.h>
 
+/**
+ * Custom properties builder.
+ * Collect multiple properties for send its at once in the same log.
+ */
 @interface MSCustomProperties : NSObject
 
+/**
+ * Set the specified property value with the specified key.
+ * If the properties previously contained a property for the key, the old
+ * value is replaced.
+ *
+ * @param key   key with which the specified value is to be set.
+ * @param value value to be set with the specified key.
+ * @return this instance.
+ */
 - (MSCustomProperties *)setString:(NSString *)value forKey:(NSString *)key;
 
+/**
+ * Set the specified property value with the specified key.
+ * If the properties previously contained a property for the key, the old
+ * value is replaced.
+ *
+ * @param key   key with which the specified value is to be set.
+ * @param value value to be set with the specified key.
+ * @return this instance.
+ */
 - (MSCustomProperties *)setNumber:(NSNumber *)value forKey:(NSString *)key;
 
+/**
+ * Set the specified property value with the specified key.
+ * If the properties previously contained a property for the key, the old
+ * value is replaced.
+ *
+ * @param key   key with which the specified value is to be set.
+ * @param value value to be set with the specified key.
+ * @return this instance.
+ */
 - (MSCustomProperties *)setBool:(BOOL)value forKey:(NSString *)key;
 
+/**
+ * Set the specified property value with the specified key.
+ * If the properties previously contained a property for the key, the old
+ * value is replaced.
+ *
+ * @param key   key with which the specified value is to be set.
+ * @param value value to be set with the specified key.
+ * @return this instance.
+ */
 - (MSCustomProperties *)setDate:(NSDate *)value forKey:(NSString *)key;
 
+/**
+ * Clear the property for the specified key.
+ *
+ * @param key key whose mapping is to be cleared.
+ * @return this instance.
+ */
 - (MSCustomProperties *)clearPropertyForKey:(NSString *)key;
 
 @end
