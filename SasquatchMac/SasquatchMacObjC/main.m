20b9939ad24e534a684c292a3a4367214f1e530d
Rychagov Evgeny <erychagov@gmail.com>
2017-03-31 15:21:08 +0300

    New projects for macOS

---
+++ SasquatchMac/SasquatchMacObjC/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SasquatchMacObjC
+//
+//  Created by Евгений Рычагов on 31/03/2017.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
