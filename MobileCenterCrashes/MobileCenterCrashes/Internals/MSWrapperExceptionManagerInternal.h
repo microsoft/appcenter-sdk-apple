90b47f4482e11e987088c787aa7e1c7df1f298ed
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-03-31 17:42:51 -0700

    Formatting and making properties non-class properties as it feels more natural and also fixes warnings

--- MobileCenterCrashes/MobileCenterCrashes/Internals/MSWrapperExceptionManagerInternal.h
+++ MobileCenterCrashes/MobileCenterCrashes/Internals/MSWrapperExceptionManagerInternal.h
@@ -8,31 +8,31 @@
 @property CFUUIDRef currentUUIDRef;
 @property(weak, nonatomic) id<MSWrapperCrashesInitializationDelegate> crashesDelegate;
 
-@property(class, copy, readonly) NSString *dataFileExtension;
-@property(class, copy, readonly) NSString *directoryName;
-@property(class, copy, readonly) NSString *directoryPath;
+@property(atomic, copy, readonly) NSString *dataFileExtension;
+@property(atomic, copy, readonly) NSString *directoryName;
+@property(atomic, copy, readonly) NSString *directoryPath;
 
-+ (MSWrapperExceptionManager*)sharedInstance;
++ (MSWrapperExceptionManager *)sharedInstance;
 - (BOOL)hasException;
-- (MSException*)loadWrapperException:(CFUUIDRef)uuidRef;
+- (MSException *)loadWrapperException:(CFUUIDRef)uuidRef;
 - (void)saveWrapperException:(CFUUIDRef)uuidRef;
 - (void)deleteWrapperExceptionWithUUID:(CFUUIDRef)uuidRef;
 - (void)deleteAllWrapperExceptions;
 - (void)deleteAllWrapperExceptionData;
 - (void)saveWrapperExceptionData:(CFUUIDRef)uuidRef;
 
-- (NSData*)loadWrapperExceptionDataWithUUIDString:(NSString*)uuidString;
-- (void)deleteWrapperExceptionDataWithUUIDString:(NSString*)uuidString;
+- (NSData *)loadWrapperExceptionDataWithUUIDString:(NSString *)uuidString;
+- (void)deleteWrapperExceptionDataWithUUIDString:(NSString *)uuidString;
 
-+ (NSString*)directoryPath;
+- (NSString *)directoryPath;
 
-+ (NSString*)getFilename:(NSString*)uuidString;
-+ (NSString*)getDataFilename:(NSString*)uuidString;
-+ (NSString*)getFilenameWithUUIDRef:(CFUUIDRef)uuidRef;
-+ (NSString*)getDataFilenameWithUUIDRef:(CFUUIDRef)uuidRef;
-+ (void)deleteFile:(NSString*)path;
-+ (BOOL)isDataFile:(NSString*)path;
-+ (NSString*)uuidRefToString:(CFUUIDRef)uuidRef;
++ (NSString *)getFilename:(NSString *)uuidString;
++ (NSString *)getDataFilename:(NSString *)uuidString;
++ (NSString *)getFilenameWithUUIDRef:(CFUUIDRef)uuidRef;
++ (NSString *)getDataFilenameWithUUIDRef:(CFUUIDRef)uuidRef;
++ (void)deleteFile:(NSString *)path;
++ (BOOL)isDataFile:(NSString *)path;
++ (NSString *)uuidRefToString:(CFUUIDRef)uuidRef;
 + (BOOL)isCurrentUUIDRef:(CFUUIDRef)uuidRef;
 - (void)startCrashReportingFromWrapperSdk;
 
