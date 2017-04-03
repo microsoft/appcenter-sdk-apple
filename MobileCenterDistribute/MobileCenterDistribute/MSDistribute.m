18c61cda4a636ee5c140c85b19885716ef467790
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-04-03 16:49:48 -0700

    Declare closeApp() in MSDistribute as noreturn

--- MobileCenterDistribute/MobileCenterDistribute/MSDistribute.m
+++ MobileCenterDistribute/MobileCenterDistribute/MSDistribute.m
@@ -550,7 +550,7 @@ static NSString *const kMSUpdateTokenURLInvalidErrorDescFormat = @"Invalid updat
       }];
 }
 
-- (void)closeApp {
+- (void)closeApp __attribute__((noreturn)) {
   exit(0);
 }
 
