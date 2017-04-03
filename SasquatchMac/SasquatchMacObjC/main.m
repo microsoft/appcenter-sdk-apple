ec20235e0594168f25b9eb7de377f7b7b04153d4
Rychagov Evgeny <erychagov@gmail.com>
2017-04-03 08:40:00 +0300

    Remove copyright header

--- SasquatchMac/SasquatchMacObjC/main.m
+++ SasquatchMac/SasquatchMacObjC/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  SasquatchMacObjC
-//
-//  Created by Евгений Рычагов on 31/03/2017.
-//  Copyright © 2017 Microsoft. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
