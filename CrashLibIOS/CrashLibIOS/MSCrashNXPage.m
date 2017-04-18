1c4afb2308e8e5a03b080b2181f3826b19c19541
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-04-18 10:24:25 -0700

    fix jump to nx page

--- CrashLibIOS/CrashLibIOS/MSCrashNXPage.m
+++ CrashLibIOS/CrashLibIOS/MSCrashNXPage.m
@@ -3,6 +3,7 @@
  */
 
 #import "MSCrashNXPage.h"
+#import <sys/mman.h>
 
 @implementation MSCrashNXPage
 
@@ -19,7 +20,17 @@
 }
 
 - (void)crash {
-  ((void (*)(void)) NULL)();
+  /**
+   * Solution and explanation by Gwynne:
+   * When generating an NX crash, previously the code would explicitly jump to NULL, which modern versions of Clang
+   * correctly optimize out as provable undefined behavior (the compiler is free to do whatever it wants if it can
+   * prove that the code will always dereference NULL). Instead, map a valid memory space without the execute
+   * permission and jump to that pointer.
+   */
+  void *ptr = mmap(NULL, (size_t)getpagesize(), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+  if (ptr != MAP_FAILED) {
+    ((void (*)(void))ptr)();
+  }
 }
 
 @end
