7a358f16c593e5d435458f126ef6c15face478c3
Jae Lim <jaelim@microsoft.com>
2017-04-19 12:14:20 -0700

    Fix SasquatchTV build and start MobileCenter

--- SasquatchTV/SasquatchTVObjC/AppDelegate.m
+++ SasquatchTV/SasquatchTVObjC/AppDelegate.m
@@ -1,5 +1,11 @@
 #import "AppDelegate.h"
 
+@import MobileCenter;
+@import MobileCenterAnalytics;
+@import MobileCenterCrashes;
+// TODO: SafariServices framework not found
+//@import MobileCenterDistribute;
+
 @interface AppDelegate ()
 
 @end
@@ -7,30 +13,57 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
+
+  [MSMobileCenter setLogLevel:MSLogLevelVerbose];
+  [MSMobileCenter setLogUrl:@"https://in-integration.dev.avalanch.es"];
+  [MSMobileCenter start:@"7ee5f412-02f7-45ea-a49c-b4ebf2911325"
+           withServices:@[ [MSAnalytics class], [MSCrashes class] ]];
   return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+  /*
+   * Sent when the application is about to move from active to inactive state. This can occur for certain types of
+   * temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and
+   * it begins the transition to the background state.
+   * Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use
+   * this method to pause the game.
+   */
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+  /*
+   * Use this method to release shared resources, save user data, invalidate timers, and store enough application state
+   * information to restore your application to its current state in case it is terminated later.
+   * If your application supports background execution, this method is called instead of applicationWillTerminate: when
+   * the user quits.
+   */
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-  // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
+  /*
+   * Called as part of the transition from the background to the active state; here you can undo many of the changes
+   * made on entering the background.
+   */
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+  /*
+   * Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was
+   * previously in the background, optionally refresh the user interface.
+   */
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
+  /*
+   * Called when the application is about to terminate. Save data if appropriate. See also
+   * applicationDidEnterBackground:.
+   */
 }
 
 @end
--- SasquatchTV/SasquatchTVSwift/AppDelegate.swift
+++ SasquatchTV/SasquatchTVSwift/AppDelegate.swift
@@ -1,34 +1,67 @@
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+import MobileCenter
+import MobileCenterAnalytics
+import MobileCenterCrashes
+// TODO: SafariServices framework not found
+//import MobileCenterDistribute;
 
-  var window: UIWindow?
+@UIApplicationMain
+
+class AppDelegate : UIResponder, UIApplicationDelegate {
+
+  var window : UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
     // Override point for customization after application launch.
+    MSMobileCenter.setLogLevel(MSLogLevel.verbose)
+    MSMobileCenter.start("0dbca56b-b9ae-4d53-856a-7c2856137d85", withServices: [MSAnalytics.self, MSCrashes.self])
     return true
   }
 
-  func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  func applicationWillResignActive(_ application : UIApplication) {
+
+    /*
+     * Sent when the application is about to move from active to inactive state. This can occur for certain types of
+     * temporary interruptions (such as an  * incoming phone call or SMS message) or when the user quits the application
+     * and it begins the transition to the background state.
+     * Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use
+     * this method to pause the game.
+     */
   }
 
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  func applicationDidEnterBackground(_ application : UIApplication) {
+
+    /*
+     * Use this method to release shared resources, save user data, invalidate timers, and store enough application
+     * state information to restore your application to its current state in case it is terminated later.
+     * If your application supports background execution, this method is called instead of applicationWillTerminate:
+     * when the user quits.
+     */
   }
 
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+  func applicationWillEnterForeground(_ application : UIApplication) {
+
+    /*
+     * Called as part of the transition from the background to the active state; here you can undo many of the changes
+     * made on entering the background.
+    */
   }
 
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  func applicationDidBecomeActive(_ application : UIApplication) {
+
+    /*
+     * Restart any tasks that were paused (or not yet started) while the application was inactive. If the application
+     * was previously in the background, optionally refresh the user interface.
+     */
   }
 
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  func applicationWillTerminate(_ application : UIApplication) {
+
+    /*
+     * Called when the application is about to terminate. Save data if appropriate. See also
+     * applicationDidEnterBackground:.
+     */
   }
 }
