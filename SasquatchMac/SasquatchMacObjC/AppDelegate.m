9fe39b98f8349ad9e8ea33e612a268ac48dd6a41
Jae Lim <jaelim@microsoft.com>
2017-04-21 17:16:16 -0700

    Fix build issues and start Analytics in macOS application

--- SasquatchMac/SasquatchMacObjC/AppDelegate.m
+++ SasquatchMac/SasquatchMacObjC/AppDelegate.m
@@ -1,5 +1,8 @@
 #import "AppDelegate.h"
 
+@import MobileCenterMac;
+@import MobileCenterAnalyticsMac;
+
 @interface AppDelegate ()
 
 @end
@@ -7,7 +10,12 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+
   // Insert code here to initialize your application
+  [MSMobileCenter setLogLevel:MSLogLevelVerbose];
+  [MSMobileCenter setLogUrl:@"https://in-integration.dev.avalanch.es"];
+  [MSMobileCenter start:@"7ee5f412-02f7-45ea-a49c-b4ebf2911325"
+           withServices:@[ [MSAnalytics class] ]];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
