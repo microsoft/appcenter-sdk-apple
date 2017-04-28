f9d14b42d655cad0af4b59e42ff81906ce1ad339
Jae Lim <jaelim@microsoft.com>
2017-04-28 13:29:34 -0700

    Remove other platform dependencies

--- Sasquatch/SasquatchWatchObjC Extension/ExtensionDelegate.m
+++ Sasquatch/SasquatchWatchObjC Extension/ExtensionDelegate.m
@@ -1,44 +1,17 @@
 #import "ExtensionDelegate.h"
 
-@import MobileCenter;
-@import MobileCenterAnalytics;
-@import MobileCenterCrashes;
-// TODO: SafariServices framework not found
-//@import MobileCenterDistribute;
-
 @implementation ExtensionDelegate
 
 - (void)applicationDidFinishLaunching {
-
-  [MSMobileCenter setLogLevel:MSLogLevelVerbose];
-  [MSMobileCenter setLogUrl:@"https://in-integration.dev.avalanch.es"];
-  [MSMobileCenter start:@"7ee5f412-02f7-45ea-a49c-b4ebf2911325"
-           withServices:@[ [MSAnalytics class], [MSCrashes class] ]];
 }
 
 - (void)applicationDidBecomeActive {
-
-  /*
-   * Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was
-   * previously in the background, optionally refresh the user interface.
-   */
 }
 
 - (void)applicationWillResignActive {
-
-  /*
-   * Sent when the application is about to move from active to inactive state. This can occur for certain types of
-   * temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and
-   * it begins the transition to the background state.  Use this method to pause ongoing tasks, disable timers, etc.
-   */
 }
 
 - (void)handleBackgroundTasks:(NSSet<WKRefreshBackgroundTask *> *)backgroundTasks {
-
-  /*
-   * Sent when the system needs to launch the application in the background to process tasks. Tasks arrive in a set, so
-   * loop through and process each one.
-   */
   for (WKRefreshBackgroundTask *task in backgroundTasks) {
 
     // Check the Class of each task to decide how to process it
--- Sasquatch/SasquatchWatchSwift Extension/ExtensionDelegate.swift
+++ Sasquatch/SasquatchWatchSwift Extension/ExtensionDelegate.swift
@@ -1,42 +1,17 @@
 import WatchKit
-import MobileCenter
-import MobileCenterAnalytics
-import MobileCenterCrashes
-// TODO: SafariServices framework not found
-//import MobileCenterDistribute;
 
 class ExtensionDelegate : NSObject, WKExtensionDelegate {
 
   func applicationDidFinishLaunching() {
-
-    // Perform any final initialization of your application.
-    MSMobileCenter.setLogLevel(MSLogLevel.verbose)
-    MSMobileCenter.start("0dbca56b-b9ae-4d53-856a-7c2856137d85", withServices: [MSAnalytics.self, MSCrashes.self])
   }
 
   func applicationDidBecomeActive() {
-
-    /*
-     * Restart any tasks that were paused (or not yet started) while the application was inactive. If the application
-     * was previously in the background, optionally refresh the user interface.
-     */
   }
 
   func applicationWillResignActive() {
-
-    /*
-     * Sent when the application is about to move from active to inactive state. This can occur for certain types of
-     * temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application
-     * and it begins the transition to the background state. Use this method to pause ongoing tasks, disable timers, etc.
-     */
   }
 
   func handle(_ backgroundTasks : Set<WKRefreshBackgroundTask>) {
-
-    /*
-     * Sent when the system needs to launch the application in the background to process tasks. Tasks arrive in a set,
-     * so loop through and process each one.
-     */
     for
       task in backgroundTasks {
 
