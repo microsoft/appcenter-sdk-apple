f152f2d266c33911e124498b66ad81cae9b23c3d
Jae Lim <jaelim@microsoft.com>
2017-05-03 09:34:32 -0700

    Missing file for cleaning up channel delegate

--- MobileCenterPush/MobileCenterPush/MSPush.m
+++ MobileCenterPush/MobileCenterPush/MSPush.m
@@ -113,7 +113,7 @@ static dispatch_once_t onceToken;
     [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
   } else {
 
-    // Ignore the partial availability warning as the compiler doesn't get that we checked for pre-iOS 10 already.
+// Ignore the partial availability warning as the compiler doesn't get that we checked for pre-iOS 10 already.
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wpartial-availability"
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
@@ -157,50 +157,6 @@ static dispatch_once_t onceToken;
   self.pushTokenHasBeenSent = YES;
 }
 
-#pragma mark - MSChannelDelegate
-
-- (void)channel:(id<MSChannel>)channel willSendLog:(id<MSLog>)log {
-  (void)channel;
-  if (!self.delegate) {
-    return;
-  }
-  NSObject *logObject = (NSObject *)log;
-  if (![logObject isKindOfClass:[MSPushLog class]] ||
-      ![self.delegate respondsToSelector:@selector(push:willSendInstallationLog:)]) {
-    return;
-  }
-  MSPushLog *installationLog = (MSPushLog *)log;
-  [self.delegate push:self willSendInstallationLog:installationLog];
-}
-
-- (void)channel:(id<MSChannel>)channel didSucceedSendingLog:(id<MSLog>)log {
-  (void)channel;
-  if (!self.delegate) {
-    return;
-  }
-  NSObject *logObject = (NSObject *)log;
-  if (![logObject isKindOfClass:[MSPushLog class]] ||
-      ![self.delegate respondsToSelector:@selector(push:didSucceedSendingInstallationLog:)]) {
-    return;
-  }
-  MSPushLog *installationLog = (MSPushLog *)log;
-  [self.delegate push:self didSucceedSendingInstallationLog:installationLog];
-}
-
-- (void)channel:(id<MSChannel>)channel didFailSendingLog:(id<MSLog>)log withError:(NSError *)error {
-  (void)channel;
-  if (!self.delegate) {
-    return;
-  }
-  NSObject *logObject = (NSObject *)log;
-  if (![logObject isKindOfClass:[MSPushLog class]] ||
-      ![self.delegate respondsToSelector:@selector(push:didFailSendingInstallLog:withError:)]) {
-    return;
-  }
-  MSPushLog *installationLog = (MSPushLog *)log;
-  [self.delegate push:self didFailSendingInstallLog:installationLog withError:error];
-}
-
 #pragma mark - Register callbacks
 
 - (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
@@ -213,12 +169,6 @@ static dispatch_once_t onceToken;
 - (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
   MSLogVerbose([MSPush logTag], @"Registering for push notifications has been finished with error: %@",
                error.description);
-}
-
-#pragma mark - Delegate
-
-+ (void)setDelegate:(nullable id<MSPushDelegate>)delegate {
-  [[self sharedInstance] setDelegate:delegate];
 }
 
 @end
