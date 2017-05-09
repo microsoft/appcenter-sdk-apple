064f364876c07d133dbc98293b14695ea5d51f68
Jae Lim <jaelim@microsoft.com>
2017-05-09 11:15:08 -0700

    Change app secret to macOS app

--- SasquatchMac/SasquatchMacObjC/AppDelegate.m
+++ SasquatchMac/SasquatchMacObjC/AppDelegate.m
@@ -15,7 +15,7 @@
   // Insert code here to initialize your application
   [MSMobileCenter setLogLevel:MSLogLevelVerbose];
   [MSMobileCenter setLogUrl:@"https://in-integration.dev.avalanch.es"];
-  [MSMobileCenter start:@"7ee5f412-02f7-45ea-a49c-b4ebf2911325"
+  [MSMobileCenter start:@"d7382cb6-a64d-4ef1-91a4-d32e885d3029"
            withServices:@[ [MSAnalytics class], [MSCrashes class] ]];
 }
 
