50bcbcab8dceda10644c37c961be028662ed66d9
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-06-12 19:25:03 -0700

    Add custompropertiesviewcontroller to Sasquatch app

--- Puppet/Puppet/MSCustomPropertiesViewController.m
+++ Puppet/Puppet/MSCustomPropertiesViewController.m
@@ -11,77 +11,75 @@
 #pragma mark - view controller
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
 }
 
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
   [tableView deselectRowAtIndexPath:indexPath animated:YES];
-  
+
   switch ([indexPath section]) {
-      
-    // Actions
+
+  // Actions
+  case 0: {
+    switch (indexPath.row) {
+
+    // Set String
     case 0: {
-      switch (indexPath.row) {
-          
-        // Set String
-        case 0: {
-          MSCustomProperties *customProperties = [MSCustomProperties new];
-          [customProperties setString:@"test" forKey:@"test"];
-          [MSMobileCenter setCustomProperties:customProperties];
-          break;
-        }
-          
-        // Set Number
-        case 1: {
-          MSCustomProperties *customProperties = [MSCustomProperties new];
-          [customProperties setNumber:@42 forKey:@"test"];
-          [MSMobileCenter setCustomProperties:customProperties];
-          break;
-        }
-          
-        // Set Boolean
-        case 2: {
-          MSCustomProperties *customProperties = [MSCustomProperties new];
-          [customProperties setBool:NO forKey:@"test"];
-          [MSMobileCenter setCustomProperties:customProperties];
-          break;
-        }
-          
-        // Set Date
-        case 3: {
-          MSCustomProperties *customProperties = [MSCustomProperties new];
-          [customProperties setDate:[NSDate date] forKey:@"test"];
-          [MSMobileCenter setCustomProperties:customProperties];
-          break;
-        }
-          
-        // Set Multiple
-        case 4: {
-          MSCustomProperties *customProperties = [MSCustomProperties new];
-          [customProperties setString:@"test" forKey:@"t1"];
-          [customProperties setDate:[NSDate date] forKey:@"t2"];
-          [customProperties setNumber:@42 forKey:@"t3"];
-          [customProperties setBool:NO forKey:@"t4"];
-          [MSMobileCenter setCustomProperties:customProperties];
-          break;
-        }
-          
-        // Clear
-        case 5: {
-          MSCustomProperties *customProperties = [MSCustomProperties new];
-          [customProperties clearPropertyForKey:@"test"];
-          [MSMobileCenter setCustomProperties:customProperties];
-          break;
-        }
-      }
-      
+      MSCustomProperties *customProperties = [MSCustomProperties new];
+      [customProperties setString:@"test" forKey:@"test"];
+      [MSMobileCenter setCustomProperties:customProperties];
       break;
     }
+
+    // Set Number
+    case 1: {
+      MSCustomProperties *customProperties = [MSCustomProperties new];
+      [customProperties setNumber:@42 forKey:@"test"];
+      [MSMobileCenter setCustomProperties:customProperties];
+      break;
+    }
+
+    // Set Boolean
+    case 2: {
+      MSCustomProperties *customProperties = [MSCustomProperties new];
+      [customProperties setBool:NO forKey:@"test"];
+      [MSMobileCenter setCustomProperties:customProperties];
+      break;
+    }
+
+    // Set Date
+    case 3: {
+      MSCustomProperties *customProperties = [MSCustomProperties new];
+      [customProperties setDate:[NSDate date] forKey:@"test"];
+      [MSMobileCenter setCustomProperties:customProperties];
+      break;
+    }
+
+    // Set Multiple
+    case 4: {
+      MSCustomProperties *customProperties = [MSCustomProperties new];
+      [customProperties setString:@"test" forKey:@"t1"];
+      [customProperties setDate:[NSDate date] forKey:@"t2"];
+      [customProperties setNumber:@42 forKey:@"t3"];
+      [customProperties setBool:NO forKey:@"t4"];
+      [MSMobileCenter setCustomProperties:customProperties];
+      break;
+    }
+
+    // Clear
+    case 5: {
+      MSCustomProperties *customProperties = [MSCustomProperties new];
+      [customProperties clearPropertyForKey:@"test"];
+      [MSMobileCenter setCustomProperties:customProperties];
+      break;
+    }
+    }
+
+    break;
+  }
   }
 }
-
-
 
 @end
---
+++ Sasquatch/Sasquatch/MSCustomPropertiesViewController.swift
@@ -0,0 +1,69 @@
+import UIKit
+import MobileCenter
+
+class MSCustomPropertiesViewController : UITableViewController, MobileCenterProtocol {
+  var mobileCenter: MobileCenterDelegate!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    let customProperties = MSCustomProperties()
+    
+    switch indexPath.section {
+    case 0:
+      
+      switch indexPath.row {
+        
+      // Set String
+      case 0:
+        customProperties.setString("test", forKey: "test")
+        mobileCenter.setCustomProperties(customProperties)
+        break
+        
+      // Set Number
+      case 1:
+        customProperties.setNumber(42, forKey: "test")
+        mobileCenter.setCustomProperties(customProperties)
+        break
+        
+      // Set Boolean
+      case 2:
+        customProperties.setBool(false, forKey: "test")
+        mobileCenter.setCustomProperties(customProperties)
+        break
+        
+        
+      // Set Date
+      case 3:
+        customProperties.setDate(Date(), forKey: "test")
+        mobileCenter.setCustomProperties(customProperties)
+        break;
+        
+        
+      // Set Mutliple
+      case 4:
+        customProperties.setString("test", forKey: "t1")
+        customProperties.setDate(Date(), forKey: "t2")
+        customProperties.setNumber(42, forKey: "t3")
+        customProperties.setBool(false, forKey: "t4")
+        mobileCenter.setCustomProperties(customProperties)
+        break
+        
+        
+      // Clear
+      case 5:
+        customProperties.clearProperty(forKey: "test")
+        mobileCenter.setCustomProperties(customProperties)
+        break
+        
+      default: break
+      }
+      
+    default: break
+    }
+  }
+  
+}
