e8addb6e08d1565f4439c44addf3d18c61ff9f75
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-06-13 09:20:57 -0700

    Fix code formatting

--- Puppet/Puppet/MSCustomPropertiesViewController.m
+++ Puppet/Puppet/MSCustomPropertiesViewController.m
@@ -24,61 +24,66 @@
   // Actions
   case 0: {
     switch (indexPath.row) {
+      
+      // Set String
+      case 0: {
+        MSCustomProperties *customProperties = [MSCustomProperties new];
+        [customProperties setString:@"test" forKey:@"test"];
+        [MSMobileCenter setCustomProperties:customProperties];
+        break;
+      }
+        
+      // Set Number
+      case 1: {
+        MSCustomProperties *customProperties = [MSCustomProperties new];
+        [customProperties setNumber:@42 forKey:@"test"];
+        [MSMobileCenter setCustomProperties:customProperties];
+        break;
+      }
+        
+      // Set Boolean
+      case 2: {
+        MSCustomProperties *customProperties = [MSCustomProperties new];
+        [customProperties setBool:NO forKey:@"test"];
+        [MSMobileCenter setCustomProperties:customProperties];
+        break;
+      }
+        
+      // Set Date
+      case 3: {
+        MSCustomProperties *customProperties = [MSCustomProperties new];
+        [customProperties setDate:[NSDate date] forKey:@"test"];
+        [MSMobileCenter setCustomProperties:customProperties];
+        break;
+      }
+        
+      // Set Multiple
+      case 4: {
+        MSCustomProperties *customProperties = [MSCustomProperties new];
+        [customProperties setString:@"test" forKey:@"t1"];
+        [customProperties setDate:[NSDate date] forKey:@"t2"];
+        [customProperties setNumber:@42 forKey:@"t3"];
+        [customProperties setBool:NO forKey:@"t4"];
+        [MSMobileCenter setCustomProperties:customProperties];
+        break;
+      }
+        
+      // Clear
+      case 5: {
+        MSCustomProperties *customProperties = [MSCustomProperties new];
+        [customProperties clearPropertyForKey:@"test"];
+        [MSMobileCenter setCustomProperties:customProperties];
+        break;
+      }
+        
+      default:
+        break;
 
-    // Set String
-    case 0: {
-      MSCustomProperties *customProperties = [MSCustomProperties new];
-      [customProperties setString:@"test" forKey:@"test"];
-      [MSMobileCenter setCustomProperties:customProperties];
+    }
+
+    default:
       break;
     }
-
-    // Set Number
-    case 1: {
-      MSCustomProperties *customProperties = [MSCustomProperties new];
-      [customProperties setNumber:@42 forKey:@"test"];
-      [MSMobileCenter setCustomProperties:customProperties];
-      break;
-    }
-
-    // Set Boolean
-    case 2: {
-      MSCustomProperties *customProperties = [MSCustomProperties new];
-      [customProperties setBool:NO forKey:@"test"];
-      [MSMobileCenter setCustomProperties:customProperties];
-      break;
-    }
-
-    // Set Date
-    case 3: {
-      MSCustomProperties *customProperties = [MSCustomProperties new];
-      [customProperties setDate:[NSDate date] forKey:@"test"];
-      [MSMobileCenter setCustomProperties:customProperties];
-      break;
-    }
-
-    // Set Multiple
-    case 4: {
-      MSCustomProperties *customProperties = [MSCustomProperties new];
-      [customProperties setString:@"test" forKey:@"t1"];
-      [customProperties setDate:[NSDate date] forKey:@"t2"];
-      [customProperties setNumber:@42 forKey:@"t3"];
-      [customProperties setBool:NO forKey:@"t4"];
-      [MSMobileCenter setCustomProperties:customProperties];
-      break;
-    }
-
-    // Clear
-    case 5: {
-      MSCustomProperties *customProperties = [MSCustomProperties new];
-      [customProperties clearPropertyForKey:@"test"];
-      [MSMobileCenter setCustomProperties:customProperties];
-      break;
-    }
-    }
-
-    break;
-  }
   }
 }
 
