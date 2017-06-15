ff3e68e0815fe43d74ed65585c9dc7a2ad43306c
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-06-14 17:26:40 -0700

    Create MSDistribute.m

--- MobileCenterDistribute/MobileCenterDistribute/MSDistribute.m
+++ MobileCenterDistribute/MobileCenterDistribute/MSDistribute.m
@@ -360,8 +360,8 @@ static NSString *const kMSUpdateTokenURLInvalidErrorDescFormat = @"Invalid updat
     MSLogDebug(
         [MSDistribute logTag],
         @"Distribute won't check if a new release is available because of one of the following reasons: 1. A debugger "
-         "is attached. 2. You are running the debug configuration. 3. The app is running in a non-adhoc environment."
-         "Detach the debugger and restart the app and/or run the app with the release configuration to enable the"
+         "is attached. 2. You are running the debug configuration. 3. The app is running in a non-adhoc environment. "
+         "Detach the debugger and restart the app and/or run the app with the release configuration to enable the "
          "feature.");
   }
 }
