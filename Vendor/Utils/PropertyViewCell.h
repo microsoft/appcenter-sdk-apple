0d46a95ea0958a9532c769ad238c1ee5c73ef581
Murat Baysangurov <v-mubays@microsoft.com>
2017-06-16 11:43:36 +0300

    Added TODO comments

--- Vendor/Utils/PropertyViewCell.h
+++ Vendor/Utils/PropertyViewCell.h
@@ -2,6 +2,8 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  */
 
+// TODO: The file should be relocated under iOS sub folder once multiple-platforms branch is merged into develop.
+
 #import <UIKit/UIKit.h>
 
 @interface PropertyViewCell : UITableViewCell
--- Vendor/Utils/PropertyViewCell.m
+++ Vendor/Utils/PropertyViewCell.m
@@ -2,6 +2,8 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  */
 
+// TODO: The file should be relocated under iOS sub folder once multiple-platforms branch is merged into develop.
+
 #import "PropertyViewCell.h"
 
 @implementation PropertyViewCell
