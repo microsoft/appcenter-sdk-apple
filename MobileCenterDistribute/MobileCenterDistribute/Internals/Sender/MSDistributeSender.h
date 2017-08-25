a8bd4e8c4a37f8837c434aa3a021700534e58b9b
Jae Lim <jaelim@microsoft.com>
2017-08-25 10:42:12 -0700

    Revise doc for distribution group ID

--- MobileCenterDistribute/MobileCenterDistribute/Internals/Sender/MSDistributeSender.h
+++ MobileCenterDistribute/MobileCenterDistribute/Internals/Sender/MSDistributeSender.h
@@ -22,7 +22,7 @@ static NSString *const kMSHeaderUpdateApiToken = @"x-api-token";
  * @param appSecret A unique and secret key used to identify the application.
  * @param updateToken The update token stored in keychain. This parameter is optional and the update will be considered
  * as public distribution if it is nil.
- * @param distributionGroupId The distribution group Id stored in keychain.
+ * @param distributionGroupId The distribution group Id stored in storage.
  * @param queryStrings An array of query strings.
  *
  * @return A sender instance.
