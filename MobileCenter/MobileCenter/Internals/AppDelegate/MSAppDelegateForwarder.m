ab94cd3846316cdf0fc97587196d401be6750f96
Jae Lim <jaelim@microsoft.com>
2017-09-11 11:35:04 -0700

    Not using trace buffer if the buffer is already flushed

--- MobileCenter/MobileCenter/Internals/AppDelegate/MSAppDelegateForwarder.m
+++ MobileCenter/MobileCenter/Internals/AppDelegate/MSAppDelegateForwarder.m
@@ -28,11 +28,15 @@ static NSMutableSet<NSString *> *_selectorsToSwizzle = nil;
 static NSArray<NSString *> *_selectorsNotToOverride = nil;
 static NSDictionary<NSString *, NSString *> *_deprecatedSelectors = nil;
 static NSMutableDictionary<NSString *, NSValue *> *_originalImplementations = nil;
-static NSMutableArray<dispatch_block_t> *_traceBuffer = nil;
+static NSMutableArray<dispatch_block_t> *traceBuffer = nil;
 static IMP _originalSetDelegateImp = NULL;
 static BOOL _enabled = YES;
 
 @implementation MSAppDelegateForwarder
+
++ (void)initialize {
+  traceBuffer = [NSMutableArray new];
+}
 
 + (void)load {
 
@@ -50,11 +54,11 @@ static BOOL _enabled = YES;
 
   // Swizzle `setDelegate:` of Application class.
   if (MSAppDelegateForwarder.enabled) {
-    [MSAppDelegateForwarder.traceBuffer addObject:^{
+    [self addTraceBlock:^{
       MSLogDebug([MSMobileCenter logTag], @"Application delegate forwarder is enabled. It may use swizzling.");
     }];
   } else {
-    [MSAppDelegateForwarder.traceBuffer addObject:^{
+    [self addTraceBlock:^{
       MSLogDebug([MSMobileCenter logTag], @"Application delegate forwarder is disabled. It won't use swizzling.");
     }];
   }
@@ -107,8 +111,20 @@ static BOOL _enabled = YES;
   return _originalImplementations ?: (_originalImplementations = [NSMutableDictionary new]);
 }
 
-+ (NSMutableArray<dispatch_block_t> *)traceBuffer {
-  return _traceBuffer ?: (_traceBuffer = [NSMutableArray new]);
++ (void)addTraceBlock:(void (^)())block {
+  @synchronized(traceBuffer) {
+    if (traceBuffer) {
+      static dispatch_once_t onceToken = 0;
+      dispatch_once(&onceToken, ^{
+        [traceBuffer addObject:^{
+          MSLogVerbose([MSMobileCenter logTag], @"Trace buffer started.");
+        }];
+      });
+      [traceBuffer addObject:block];
+    } else {
+      block();
+    }
+  }
 }
 
 + (IMP)originalSetDelegateImp {
@@ -245,7 +261,7 @@ static BOOL _enabled = YES;
   // Validate swizzling.
   if (!skipped) {
     if (!originalImp && !methodAdded) {
-      [self.traceBuffer addObject:^{
+      [self addTraceBlock:^{
         NSString *message = [NSString
             stringWithFormat:@"Cannot swizzle selector '%@' of class '%@'.", originalSelectorStr, originalClass];
         if (warningMsg) {
@@ -255,7 +271,7 @@ static BOOL _enabled = YES;
         }
       }];
     } else {
-      [self.traceBuffer addObject:^{
+      [self addTraceBlock:^{
         MSLogDebug([MSMobileCenter logTag], @"Selector '%@' of class '%@' is swizzled.", originalSelectorStr,
                    originalClass);
       }];
@@ -518,10 +534,14 @@ static BOOL _enabled = YES;
   // Only trace once.
   static dispatch_once_t traceOnceToken;
   dispatch_once(&traceOnceToken, ^{
-    for (dispatch_block_t traceBlock in self.traceBuffer) {
-      traceBlock();
+    @synchronized(traceBuffer) {
+      for (dispatch_block_t traceBlock in traceBuffer) {
+        traceBlock();
+      }
+      [traceBuffer removeAllObjects];
+      traceBuffer = nil;
+      MSLogVerbose([MSMobileCenter logTag], @"Flushed trace buffer.");
     }
-    [self.traceBuffer removeAllObjects];
   });
 }
 
