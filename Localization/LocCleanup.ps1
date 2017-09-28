602892a3da932bb2fc3a4acf5ef82d734ec8af65
Jacob Wallraff <jawal@microsoft.com>
2017-09-27 17:00:32 -0700

    Changing cleanup script to shell script and updating a korean translation

--- Localization/LocCleanup.ps1
+++
@@ -1,4 +0,0 @@
-﻿$deleteLocalBranches = "git branch -D ``git branch | grep -E 'TouchDown*'``"
-$deleteRemoteBranches = "git branch -r | awk -F/ '/\/TouchDown/{print `$2}' | xargs -I {} git push origin :{}"
-iex $deleteLocalBranches
-iex $deleteRemoteBranches
