f577e7d19b018b21d315e5950af859b8062237ee
Alexander Chocron <alchocro@microsoft.com>
2017-10-06 10:42:38 -0700

    Clean up using statements to fix issue compiling tests

--- MobileCenterCrashes/MobileCenterCrashesTests/MSWrapperCrashesHelperTests.m
+++ MobileCenterCrashes/MobileCenterCrashesTests/MSWrapperCrashesHelperTests.m
@@ -1,19 +1,6 @@
 #import "MSCrashHandlerSetupDelegate.h"
 #import "MSTestFrameworks.h"
 #import "MSWrapperCrashesHelper.h"
-#import "MSCrashesTestUtil.h"
-#import "MSErrorAttachmentLog.h"
-#import "MSChannelDefault.h"
-#import "MSLogManagerDefault.h"
-#import "MSCrashesInternal.h"
-#import "MSCrashesPrivate.h"
-#import "MSCrashesDelegate.h"
-#import "MSUtility.h"
-#import "MSUserDefaults.h"
-#import "MSErrorLogFormatter.h"
-#import "MSCrashReporter.h"
-#import "MSMobileCenter.h"
-#import "MSMobileCenterInternal.h"
 
 @interface MSWrapperCrashesHelperTests : XCTestCase
 @end
