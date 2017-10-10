f2b00b0cac7a2ab3056140bb919e74320686edcd
Alexander Chocron <alchocro@microsoft.com>
2017-10-10 09:46:29 -0700

    Add tests for MSWrapperCrashesHelper

--- MobileCenterCrashes/MobileCenterCrashesTests/MSWrapperCrashesHelperTests.m
+++ MobileCenterCrashes/MobileCenterCrashesTests/MSWrapperCrashesHelperTests.m
@@ -1,6 +1,8 @@
 #import "MSCrashHandlerSetupDelegate.h"
 #import "MSTestFrameworks.h"
 #import "MSWrapperCrashesHelper.h"
+#import "MSCrashesInternal.h"
+
 
 @interface MSWrapperCrashesHelperTests : XCTestCase
 @end
@@ -8,10 +10,84 @@
 @implementation MSWrapperCrashesHelperTests
 
 - (void)testSettingAndGettingDelegateWorks {
+  
+  // If
   id<MSCrashHandlerSetupDelegate> delegateMock = OCMProtocolMock(@protocol(MSCrashHandlerSetupDelegate));
   [MSWrapperCrashesHelper setCrashHandlerSetupDelegate:delegateMock];
+  
+  // When
   id<MSCrashHandlerSetupDelegate> retrievedDelegate = [MSWrapperCrashesHelper getCrashHandlerSetupDelegate];
+  
+  // Then
   assertThat(delegateMock, equalTo(retrievedDelegate));
 }
 
+- (void)testSetAutomaticProcessing {
+  
+  // If
+  MSCrashes *crashesMock = [self getSharedCrashesMock];
+  
+  // When
+  [MSWrapperCrashesHelper setAutomaticProcessing:YES];
+  
+  // Then
+  OCMVerify([crashesMock setAutomaticProcessing:YES]);
+  
+  // When
+  [MSWrapperCrashesHelper setAutomaticProcessing:NO];
+  
+  // Then
+  OCMVerify([crashesMock setAutomaticProcessing:NO]);
+}
+
+- (void)testGetUnprocessedCrashReports {
+  
+  // If
+  MSCrashes *crashesMock = [self getSharedCrashesMock];
+  NSArray *unprocessedReports = @[];
+  OCMStub([crashesMock getUnprocessedCrashReports]).andReturn(unprocessedReports);
+  
+  // When
+  NSArray *retrievedReports = [MSWrapperCrashesHelper getUnprocessedCrashReports];
+  
+  // Then
+  OCMVerify([crashesMock getUnprocessedCrashReports]);
+  XCTAssertEqual(unprocessedReports, retrievedReports);
+}
+
+- (void)testSendCrashReportsOrAwaitUserConfirmationForFilteredList {
+  
+  // If
+  MSCrashes *crashesMock = [self getSharedCrashesMock];
+  NSArray *filteredList = @[];
+  
+  // When
+  [MSWrapperCrashesHelper sendCrashReportsOrAwaitUserConfirmationForFilteredList:filteredList];
+  
+  // Then
+  OCMVerify([crashesMock sendCrashReportsOrAwaitUserConfirmationForFilteredList:filteredList]);
+}
+
+- (void)testsendErrorAttachmentsWithIncidentIdentifier {
+  
+  // If
+  MSCrashes *crashesMock = [self getSharedCrashesMock];
+  NSArray *errorAttachments = @[];
+  NSString *incidentId = @"incident id";
+  
+  // When
+  [MSWrapperCrashesHelper sendErrorAttachments:errorAttachments withIncidentIdentifier:incidentId];
+  
+  // Then
+  OCMVerify([crashesMock sendErrorAttachments:errorAttachments withIncidentIdentifier:incidentId]);
+}
+
+#pragma mark Helpers
+
+- (MSCrashes*)getSharedCrashesMock {
+  MSCrashes *crashesMock = OCMPartialMock([MSCrashes new]);
+  id crashesClassMock = OCMClassMock([MSCrashes class]);
+  OCMStub(ClassMethod([crashesClassMock sharedInstance])).andReturn(crashesMock);
+  return crashesMock;
+}
 @end
