0114981b9eae60ae16b96c8071dfa82fd32d87e3
Jae Lim <jaelim@microsoft.com>
2017-10-18 14:07:59 -0700

    Change to update build number before building binaries

---
+++ bitrise/internal-version-increment.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## 1. Get the latest version
+echo "Get the latest version to determine a build number"
+build_number=0
+resp="$(curl -s $INTERNAL_RELEASE_VERSION_FILENAME)"
+version="$(echo $resp | jq -r '.version')"
+
+# Exit if response doesn't contain an array
+if [ -z $version ] || [ "$version" == "" ] || [ "$version" == "null" ]; then
+  echo "Cannot retrieve the latest version"
+  echo "Response:" $resp
+  exit 1
+fi
+
+# Determine the next version
+if [[ "$version" == $publish_version-* ]]; then
+  build_number="$(echo $version | awk -F "[-]" '{print $2}')"
+  build_number=$(($build_number + 1))
+fi
+
+publish_version=$publish_version-$build_number
+echo "New version:" $publish_version
+envman add --key SDK_PUBLISH_VERSION --value "$publish_version"
+
+## 2. Update version file
+echo {\"version\":\"$publish_version\"} > ios_version.txt
+azure telemetry --disable
+echo "Y" | azure storage blob upload ios_version.txt sdk
+rm ios_version.txt
+
+## 3. Update version for frameworks
+sed "s/\(VERSION_STRING[[:space:]]*=[[:space:]]*\).*/\1$publish_version/g" Config/Version.xcconfig > Config/Version.xcconfig.tmp; mv Config/Version.xcconfig.tmp Config/Version.xcconfig
