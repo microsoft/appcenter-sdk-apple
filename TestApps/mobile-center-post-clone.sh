dd809d5030dc76590b3d68e9420eeac9287a3bf8
Jae Lim <jaelim@microsoft.com>
2017-11-13 21:40:45 -0800

    Rename to App Center in App Center Build post script

--- TestApps/mobile-center-post-clone.sh
+++ TestApps/mobile-center-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Download App Center SDK frameworks
-curl -O https://mobilecentersdkdev.blob.core.windows.net/sdk/MobileCenter-SDK-Apple.zip
+curl -O https://mobilecentersdkdev.blob.core.windows.net/sdk/AppCenter-SDK-Apple.zip
 
 # Unzip files to framework location
-unzip MobileCenter-SDK-Apple.zip
+unzip AppCenter-SDK-Apple.zip
