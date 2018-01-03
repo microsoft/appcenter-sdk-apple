3a4ea5b68566671387815c8d8c66d65fb176ace0
Jae Lim <jaelim@microsoft.com>
2018-01-03 14:55:08 -0800

    Move NS_OPTION definition to Core

--- AppCenterPush/AppCenterPush/Internal/MSPushAppDelegate.m
+++ AppCenterPush/AppCenterPush/Internal/MSPushAppDelegate.m
@@ -41,14 +41,6 @@
 
 #pragma mark - Forwarding
 
-// Enum used to represent all kind of executors running the completion handler.
-typedef NS_OPTIONS(NSUInteger, MSCompletionExecutor) {
-  MSCompletionExecutorNone = (1 << 0),
-  MSCompletionExecutorOriginal = (1 << 1),
-  MSCompletionExecutorCustom = (1 << 2),
-  MSCompletionExecutorForwarder = (1 << 3)
-};
-
 @implementation MSAppDelegateForwarder (MSPush)
 
 + (void)load {
