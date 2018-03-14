e385e5dfd094f2825af56a894f6b73244a466a49
Alexander Chocron <alchocro@microsoft.com>
2018-03-14 11:56:06 -0700

    Fix tests

--- AppCenterDistribute/AppCenterDistributeTests/MSDistributeTests.m
+++ AppCenterDistribute/AppCenterDistributeTests/MSDistributeTests.m
@@ -164,7 +164,7 @@ static NSURL *sfURL;
 
   // Disable for now to bypass initializing sender.
   [distributeMock setEnabled:NO];
-  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret];
+  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret tenantId:nil];
 
   // Enable again.
   [distributeMock setEnabled:YES];
@@ -983,7 +983,7 @@ static NSURL *sfURL;
 
   // Disable for now to bypass initializing sender.
   [distributeMock setEnabled:NO];
-  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret];
+  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret tenantId:nil];
 
   // Enable again.
   [distributeMock setEnabled:YES];
@@ -1054,7 +1054,7 @@ static NSURL *sfURL;
 
   // Disable for now to bypass initializing sender.
   [distributeMock setEnabled:NO];
-  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret];
+  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret tenantId:nil];
 
   // Enable again.
   [distributeMock setEnabled:YES];
@@ -1121,7 +1121,7 @@ static NSURL *sfURL;
 
   // Disable for now to bypass initializing sender.
   [distributeMock setEnabled:NO];
-  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret];
+  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret tenantId:nil];
 
   // Enable again.
   [distributeMock setEnabled:YES];
@@ -1191,7 +1191,7 @@ static NSURL *sfURL;
   OCMStub([distributeMock sharedInstance]).andReturn(distributeMock);
   id appCenterMock = OCMClassMock([MSAppCenter class]);
   OCMStub([appCenterMock isConfigured]).andReturn(YES);
-  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret];
+  [distributeMock startWithChannelGroup:OCMProtocolMock(@protocol(MSChannelGroupProtocol)) appSecret:kMSTestAppSecret tenantId:nil];
 
   // If
   NSURL *url = [NSURL
