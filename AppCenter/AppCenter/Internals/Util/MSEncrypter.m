82b27c748e244abb8c6d8efe11cd430f3c3a2bdd
Murat Baysangurov <v-mubays@microsoft.com>
2018-06-08 00:38:08 +0300

    Use keychain util for storing key

--- AppCenter/AppCenter/Internals/Util/MSEncrypter.m
+++ AppCenter/AppCenter/Internals/Util/MSEncrypter.m
@@ -1,23 +1,12 @@
 #import "MSAppCenterInternal.h"
 #import "MSEncrypter.h"
+#import "MSKeychainUtil.h"
 #import "MSLogger.h"
 #import <CommonCrypto/CommonCryptor.h>
 
-/*
- * This enum is absent in iOS, and was copied from cssmapple.h on the Mac OS X SDK.
- */
-#if !TARGET_OS_OSX
-enum {
-  CSSM_ALGID_NONE = 0x00000000L,
-  CSSM_ALGID_VENDOR_DEFINED = CSSM_ALGID_NONE + 0x80000000L,
-  CSSM_ALGID_AES
-};
-#endif
-
 static int const kMSEncryptionAlgorithm = kCCAlgorithmAES128;
-static int const kMSEncryptionAlgorithmID = CSSM_ALGID_AES;
 static int const kMSCipherKeySize = kCCKeySizeAES256;
-static NSString const *kMSEncryptionKeyTag = @"kMSEncryptionKeyTag";
+static NSString *kMSEncryptionKeyTag = @"kMSEncryptionKeyTag";
 
 @interface MSEncrypter()
 
@@ -102,15 +91,9 @@ static NSString const *kMSEncryptionKeyTag = @"kMSEncryptionKeyTag";
 
 + (NSData *)loadKeyFromKeychain {
   NSData *keyData = nil;
-  NSDictionary *keyQuery = @{(__bridge id)kSecClass: (__bridge id)kSecClassKey,
-                             (__bridge id)kSecAttrApplicationTag: kMSEncryptionKeyTag,
-                             (__bridge id)kSecAttrKeyType: [NSNumber numberWithUnsignedInt:CSSM_ALGID_AES],
-                             (__bridge id)kSecReturnData: [NSNumber numberWithBool:YES]};
-
-  OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)keyQuery, (CFTypeRef *)(void *)&keyData);
-  if (status != errSecSuccess) {
-    keyData = nil;
-    MSLogError([MSAppCenter logTag], @"Could not load key from Keychain. Error code: %d", (int)status);
+  NSString *stringKey = [MSKeychainUtil stringForKey:kMSEncryptionKeyTag];
+  if (stringKey) {
+    keyData = [[NSData alloc] initWithBase64EncodedString: stringKey options:0];
   }
   return keyData;
 }
@@ -118,14 +101,6 @@ static NSString const *kMSEncryptionKeyTag = @"kMSEncryptionKeyTag";
 + (NSData *)generateKey {
   NSData *resultKey = nil;
   uint8_t *keyBytes = nil;
-  NSMutableDictionary *keyQuery = [[NSMutableDictionary alloc] init];
-  [keyQuery setObject:(__bridge id)kSecClassKey forKey:(__bridge id)kSecClass];
-  [keyQuery setObject:kMSEncryptionKeyTag forKey:(__bridge  id)kSecAttrApplicationTag];
-  [keyQuery setObject:[NSNumber numberWithUnsignedInt:kMSEncryptionAlgorithmID] forKey:(__bridge id)kSecAttrKeyType];
-  [keyQuery setObject:[NSNumber numberWithUnsignedInt:(unsigned int)(kMSCipherKeySize << 3)] forKey:(__bridge id)kSecAttrKeySizeInBits];
-  [keyQuery setObject:[NSNumber numberWithUnsignedInt:(unsigned int)(kMSCipherKeySize << 3)]  forKey:(__bridge id)kSecAttrEffectiveKeySize];
-  [keyQuery setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecAttrCanEncrypt];
-  [keyQuery setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecAttrCanDecrypt];
   keyBytes = malloc(kMSCipherKeySize * sizeof(uint8_t));
   memset((void *)keyBytes, 0x0, kMSCipherKeySize);
   OSStatus status = SecRandomCopyBytes(kSecRandomDefault, kMSCipherKeySize, keyBytes);
@@ -134,15 +109,8 @@ static NSString const *kMSEncryptionKeyTag = @"kMSEncryptionKeyTag";
   }
   resultKey = [[NSData alloc] initWithBytes:keyBytes length:kMSCipherKeySize];
   free(keyBytes);
-
-  // Add the wrapped key data to the container dictionary.
-  [keyQuery setObject:resultKey forKey:(__bridge id)kSecValueData];
-
-  // Add key to the keychain.
-  status = SecItemAdd((CFDictionaryRef) keyQuery, nil);
-  if (status != errSecSuccess) {
-    MSLogError([MSAppCenter logTag], @"Error adding encryption key to keychain. Error code: %d", (int)status);
-  }
+  NSString *stringKey = [resultKey base64EncodedStringWithOptions:0];
+  [MSKeychainUtil storeString:stringKey forKey:kMSEncryptionKeyTag];
   return resultKey;
 }
 @end
