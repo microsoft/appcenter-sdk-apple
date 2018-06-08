a04ec2c4a3c050bb71436ccb3728dd1777b3a96d
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-06-07 17:21:10 -0700

    Add UI to enable OneCollector in Sasquatch iOS app.

--- TestApps/Sasquatch/Sasquatch/ViewControllers/MSMainViewController.swift
+++ TestApps/Sasquatch/Sasquatch/ViewControllers/MSMainViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class MSMainViewController: UITableViewController, AppCenterProtocol {
   
   @IBOutlet weak var enabled: UISwitch!
+  @IBOutlet weak var oneCollectorEnabled: UISwitch!
   @IBOutlet weak var installId: UILabel!
   @IBOutlet weak var appSecret: UILabel!
   @IBOutlet weak var logUrl: UILabel!
@@ -12,6 +13,7 @@ class MSMainViewController: UITableViewController, AppCenterProtocol {
   override func viewDidLoad() {
     super.viewDidLoad()
     self.enabled.isOn = appCenter.isAppCenterEnabled()
+    self.oneCollectorEnabled.isOn = UserDefaults.standard.bool(forKey: "isOneCollectorEnabled")
     self.installId.text = appCenter.installId()
     self.appSecret.text = appCenter.appSecret()
     self.logUrl.text = appCenter.logUrl()
@@ -21,6 +23,22 @@ class MSMainViewController: UITableViewController, AppCenterProtocol {
   @IBAction func enabledSwitchUpdated(_ sender: UISwitch) {
     appCenter.setAppCenterEnabled(sender.isOn)
     sender.isOn = appCenter.isAppCenterEnabled()
+  }
+  
+  @IBAction func enableOneCollectorSwitchUpdated(_ sender: UISwitch) {
+    let alert = UIAlertController(title: "Restart", message: "Please restart the app for the change to take effect.",
+                                  preferredStyle: .actionSheet)
+    let exitAction = UIAlertAction(title: "Exit", style: .destructive) {_ in
+      UserDefaults.standard.set(sender.isOn, forKey: "isOneCollectorEnabled")
+      exit(0)
+    }
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {_ in
+      sender.isOn = !sender.isOn
+      alert.dismiss(animated: true, completion: nil)
+    }
+    alert.addAction(exitAction)
+    alert.addAction(cancelAction)
+    self.present(alert, animated: true, completion: nil)
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
