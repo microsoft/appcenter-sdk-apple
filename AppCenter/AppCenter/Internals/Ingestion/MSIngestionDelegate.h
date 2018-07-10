ed1309faba2ebf8e3aaa8ca66c7e6cc8f2ab0973
Jae Lim <jaelim@microsoft.com>
2018-07-10 09:47:42 -0700

    Remove unnecessary __unused mark

--- AppCenter/AppCenter/Internals/Ingestion/MSIngestionDelegate.h
+++ AppCenter/AppCenter/Internals/Ingestion/MSIngestionDelegate.h
@@ -9,20 +9,20 @@
  *
  * @param ingestion Ingestion.
  */
-- (void)ingestionDidSuspend:(__unused id<MSIngestionProtocol>)ingestion;
+- (void)ingestionDidSuspend:(id<MSIngestionProtocol>)ingestion;
 
 /**
  * Triggered after the ingestion has resumed its state.
  *
  * @param ingestion Ingestion.
  */
-- (void)ingestionDidResume:(__unused id<MSIngestionProtocol>)ingestion;
+- (void)ingestionDidResume:(id<MSIngestionProtocol>)ingestion;
 
 /**
  * Triggered when ingestion receives a fatal error.
  *
  * @param ingestion Ingestion.
  */
-- (void)ingestionDidReceiveFatalError:(__unused id<MSIngestionProtocol>)ingestion;
+- (void)ingestionDidReceiveFatalError:(id<MSIngestionProtocol>)ingestion;
 
 @end
