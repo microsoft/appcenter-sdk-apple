73c18e40ee2111bc1b0a4e4785beba213b0ac8a4
Clement Polet <clpolet@microsoft.com>
2018-07-13 09:34:36 -0700

    Remove optimization for puppet + fix category in AnalyticsViewController

--- Puppet/Puppet/MSAnalyticsViewController.m
+++ Puppet/Puppet/MSAnalyticsViewController.m
@@ -11,6 +11,7 @@
 #import "MSAnalyticsTranmissionTargetSelectorViewCell.h"
 // trackPage has been hidden in MSAnalytics temporarily. Use internal until the feature comes back.
 #import "MSAnalyticsInternal.h"
+#import <objc/runtime.h>
 
 static const NSInteger kPropertyIndentationLevel = 0;
 static const NSInteger kDefaultIndentationLevel = 0;
@@ -32,6 +33,18 @@ typedef NS_ENUM(short, MSPropertyType) {
 @implementation UITextField (MSProperty)
 @dynamic propertyType;
 @dynamic associatedKey;
+- (MSPropertyType)propertyType {
+  return [(NSNumber *)objc_getAssociatedObject(self, _cmd) shortValue];
+}
+- (void)setPropertyType:(MSPropertyType)type {
+  objc_setAssociatedObject(self, @selector(propertyType), @(type), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+- (NSString *)associatedKey {
+  return objc_getAssociatedObject(self, _cmd);
+}
+- (void)setAssociatedKey:(NSString *)key {
+  objc_setAssociatedObject(self, @selector(associatedKey), key, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
 @end
 
 @interface MSAnalyticsViewController ()
@@ -44,8 +57,8 @@ typedef NS_ENUM(short, MSPropertyType) {
 @property(nonatomic) NSInteger propertiesCount;
 @property(weak, nonatomic) IBOutlet UILabel *selectedChildTargetTokenLabel;
 @property(nonatomic) MSAnalyticsTranmissionTargetSelectorViewCell *transmissionTargetSelectorCell;
-@property(nonatomic) NSMutableDictionary<NSString*, NSMutableDictionary<NSString*, NSString*>*> *targetProperties;
-@property(nonatomic) NSMutableDictionary<NSString*, NSString*> *eventProperties;
+@property(nonatomic) NSMutableDictionary<NSString *, NSMutableDictionary<NSString *, NSString *> *> *targetProperties;
+@property(nonatomic) NSMutableDictionary<NSString *, NSString *> *eventProperties;
 @property(nonatomic) short propertyCounter;
 
 @end
@@ -59,12 +72,15 @@ typedef NS_ENUM(short, MSPropertyType) {
   self.propertyCounter = 0;
   self.targetProperties = [NSMutableDictionary new];
   self.eventProperties = [NSMutableDictionary new];
-  self.transmissionTargetSelectorCell = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([MSAnalyticsTranmissionTargetSelectorViewCell class]) owner:self options:nil] firstObject];
+  self.transmissionTargetSelectorCell =
+      [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([MSAnalyticsTranmissionTargetSelectorViewCell class])
+                                     owner:self
+                                   options:nil] firstObject];
   for (NSString *targetName in self.transmissionTargetSelectorCell.transmissionTargetMapping) {
     self.targetProperties[targetName] = [NSMutableDictionary new];
   }
   __weak __typeof__(self) weakSelf = self;
-  self.transmissionTargetSelectorCell.didSelectTransmissionTarget = ^(){
+  self.transmissionTargetSelectorCell.didSelectTransmissionTarget = ^() {
     __typeof__(self) strongSelf = weakSelf;
     [strongSelf.tableView reloadData];
   };
@@ -95,8 +111,7 @@ typedef NS_ENUM(short, MSPropertyType) {
 - (IBAction)trackEvent {
   [MSAnalytics trackEvent:self.eventName.text withProperties:self.eventProperties];
   if (self.oneCollectorEnabled.on) {
-    MSAnalyticsTransmissionTarget *target = [MSAnalytics
-        transmissionTargetForToken: kMSRuntimeTargetToken];
+    MSAnalyticsTransmissionTarget *target = [MSAnalytics transmissionTargetForToken:kMSRuntimeTargetToken];
     NSString *childTargetToken =
         [[NSUserDefaults standardUserDefaults] objectForKey:kMSChildTransmissionTargetTokenKey];
     if (childTargetToken) {
@@ -132,44 +147,46 @@ typedef NS_ENUM(short, MSPropertyType) {
      forRowAtIndexPath:(NSIndexPath *)indexPath {
   if ([self isTargetPropertiesRowSection:indexPath.section]) {
     NSString *selectedTarget = [self.transmissionTargetSelectorCell selectedTransmissionTarget];
-    id target = [MSAnalytics transmissionTargetForToken: selectedTarget];
+    id target = [MSAnalytics transmissionTargetForToken:selectedTarget];
     NSString *propertyKey, *propertyValue;
     if (editingStyle == UITableViewCellEditingStyleDelete) {
       // Deleting a property.
-      
+
       // Get the key name from the cell.
-      NSString *propertyKey = ((MSAnalyticsPropertyTableViewCell *)[tableView cellForRowAtIndexPath:indexPath]).keyField.text;
-      
+      propertyKey = ((MSAnalyticsPropertyTableViewCell *)[tableView cellForRowAtIndexPath:indexPath]).keyField.text;
+
       // Remove it everywhere.
       [target removeEventPropertyforKey:propertyKey];
       [self.targetProperties[selectedTarget] removeObjectForKey:selectedTarget];
-      [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+      [tableView deleteRowsAtIndexPaths:@[ indexPath ] withRowAnimation:UITableViewRowAnimationAutomatic];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
       // Adding a property.
-      
+
       // Set the property to default values.
       [self setNewDefaultKey:&propertyKey andValue:&propertyValue];
-      
+
       // Add it everywhere.
       [self.targetProperties[selectedTarget] setObject:propertyValue forKey:propertyKey];
       [target setEventPropertyString:propertyValue forKey:propertyKey];
-      [tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:indexPath.row + 1 inSection:indexPath.section]] withRowAnimation:UITableViewRowAnimationAutomatic];
+      [tableView insertRowsAtIndexPaths:@[ [NSIndexPath indexPathForRow:indexPath.row + 1 inSection:indexPath.section] ]
+                       withRowAnimation:UITableViewRowAnimationAutomatic];
     }
-  } else if ([self isEventPropertiesRowSection: indexPath.section]) {
+  } else if ([self isEventPropertiesRowSection:indexPath.section]) {
     NSString *propertyKey, *propertyValue;
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-      NSString *propertyKey = ((MSAnalyticsPropertyTableViewCell *)[tableView cellForRowAtIndexPath:indexPath]).keyField.text;
+      propertyKey = ((MSAnalyticsPropertyTableViewCell *)[tableView cellForRowAtIndexPath:indexPath]).keyField.text;
       [self.eventProperties removeObjectForKey:propertyKey];
-      [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+      [tableView deleteRowsAtIndexPaths:@[ indexPath ] withRowAnimation:UITableViewRowAnimationAutomatic];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
       [self setNewDefaultKey:&propertyKey andValue:&propertyValue];
       self.eventProperties[propertyKey] = propertyValue;
-      [tableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:indexPath.row + 1 inSection:indexPath.section]] withRowAnimation:UITableViewRowAnimationAutomatic];
+      [tableView insertRowsAtIndexPaths:@[ [NSIndexPath indexPathForRow:indexPath.row + 1 inSection:indexPath.section] ]
+                       withRowAnimation:UITableViewRowAnimationAutomatic];
     }
   }
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
   if ([self isInsertRowAtIndexPath:indexPath]) {
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
     cell.textLabel.text = @"Add Property";
@@ -177,97 +194,109 @@ typedef NS_ENUM(short, MSPropertyType) {
   } else if ([self isTargetSelectionRowAtIndexPath:indexPath]) {
     return self.transmissionTargetSelectorCell;
   } else if ([self isTargetPropertiesRowSection:indexPath.section]) {
-    MSAnalyticsPropertyTableViewCell *cell = [[[NSBundle mainBundle] loadNibNamed: NSStringFromClass([MSAnalyticsPropertyTableViewCell class]) owner:self options:nil] firstObject];
+    MSAnalyticsPropertyTableViewCell *cell =
+        [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([MSAnalyticsPropertyTableViewCell class])
+                                       owner:self
+                                     options:nil] firstObject];
     NSString *selectedTarget = self.transmissionTargetSelectorCell.selectedTransmissionTarget;
     cell.keyField.propertyType = MSPropertyTypeTargetKey;
     cell.keyField.text = self.targetProperties[selectedTarget].allKeys[indexPath.row - 2];
     cell.keyField.propertyType = MSPropertyTypeTargetValue;
     cell.valueField.text = self.targetProperties[selectedTarget][cell.keyField.text];
-    
+
     // Remember this initial key for next text update.
     cell.keyField.associatedKey = cell.keyField.text;
     cell.valueField.associatedKey = cell.keyField.text;
-    
+
     // Subscribe for text update.
-    [cell.keyField addTarget:self action:@selector(textFieldEditingDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [cell.keyField addTarget:self
+                      action:@selector(textFieldEditingDidChange:)
+            forControlEvents:UIControlEventEditingChanged];
     return cell;
   } else if ([self isEventPropertiesRowSection:indexPath.section]) {
-    MSAnalyticsPropertyTableViewCell *cell = [[[NSBundle mainBundle] loadNibNamed: NSStringFromClass([MSAnalyticsPropertyTableViewCell class]) owner:self options:nil] firstObject];
+    MSAnalyticsPropertyTableViewCell *cell =
+        [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([MSAnalyticsPropertyTableViewCell class])
+                                       owner:self
+                                     options:nil] firstObject];
     cell.keyField.propertyType = MSPropertyTypeArgumentKey;
     cell.keyField.text = self.eventProperties.allKeys[indexPath.row - 1];
     cell.keyField.propertyType = MSPropertyTypeArgumentValue;
     cell.valueField.text = self.eventProperties[cell.keyField.text];
-    
+
     // Remember this initial key for next text update.
     cell.keyField.associatedKey = cell.keyField.text;
     cell.valueField.associatedKey = cell.keyField.text;
-    
+
     // Subscribe for text update.
-    [cell.keyField addTarget:self action:@selector(textFieldEditingDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [cell.keyField addTarget:self
+                      action:@selector(textFieldEditingDidChange:)
+            forControlEvents:UIControlEventEditingChanged];
     return cell;
   } else {
     return [super tableView:tableView cellForRowAtIndexPath:indexPath];
   }
 }
 
--(NSUInteger) targetPropertyCount{
+- (NSUInteger)targetPropertyCount {
   return self.targetProperties[[self.transmissionTargetSelectorCell selectedTransmissionTarget]].count;
 }
 
--(BOOL) isTargetSelectionRowAtIndexPath: (NSIndexPath *)indexPath {
+- (NSUInteger)eventPropertyCount {
+  return self.eventProperties.count;
+}
+
+- (BOOL)isTargetSelectionRowAtIndexPath:(NSIndexPath *)indexPath {
   return indexPath.section == kTargetPropertiesSection && indexPath.row == 0;
 }
 
 - (BOOL)isInsertRowAtIndexPath:(NSIndexPath *)indexPath {
-  return (indexPath.section == kEventPropertiesSection &&
-         indexPath.row == 0)||
-  (indexPath.section == kEventPropertiesSection &&
-  indexPath.row == 1);
+  return (indexPath.section == kEventPropertiesSection && indexPath.row == 0) ||
+         (indexPath.section == kTargetPropertiesSection && indexPath.row == 1);
 }
 
 - (BOOL)isEventPropertiesRowSection:(NSInteger)section {
   return section == kEventPropertiesSection;
 }
 
-- (BOOL)isTargetPropertiesRowSection:(NSInteger) section{
+- (BOOL)isTargetPropertiesRowSection:(NSInteger)section {
   return section == kTargetPropertiesSection;
 }
 
-- (void)textFieldEditingDidChange:(UITextField *)sender{
+- (void)textFieldEditingDidChange:(UITextField *)sender {
   NSString *selectedTarget, *currentPropertyKey, *currentPropertyValue;
   MSAnalyticsTransmissionTarget *target;
   switch (sender.propertyType) {
-    case MSPropertyTypeTargetKey:
-      selectedTarget = [self.transmissionTargetSelectorCell selectedTransmissionTarget];
-      currentPropertyKey = sender.associatedKey;
-      sender.associatedKey = sender.text;
-      currentPropertyValue = self.targetProperties[selectedTarget][currentPropertyKey];
-      target = [MSAnalytics transmissionTargetForToken:selectedTarget];
-      [target removeEventPropertyforKey:currentPropertyKey];
-      [target setEventPropertyString:currentPropertyValue forKey:sender.text];
-      [self.targetProperties[selectedTarget] removeObjectForKey:currentPropertyKey];
-      self.targetProperties[selectedTarget][sender.text] = currentPropertyValue;
-      break;
-    case MSPropertyTypeTargetValue:
-      selectedTarget = [self.transmissionTargetSelectorCell selectedTransmissionTarget];
-      currentPropertyKey = sender.associatedKey;
-      target = [MSAnalytics transmissionTargetForToken:selectedTarget];
-      [target setEventPropertyString:sender.text forKey:currentPropertyKey];
-      self.targetProperties[selectedTarget][currentPropertyKey] = sender.text;
-      break;
-    case MSPropertyTypeArgumentKey:
-      currentPropertyKey = sender.associatedKey;
-      sender.associatedKey = sender.text;
-      currentPropertyValue = self.eventProperties[currentPropertyKey];
-      [self.eventProperties removeObjectForKey:currentPropertyKey];
-      self.eventProperties[sender.text] = currentPropertyValue;
-      break;
-    case MSPropertyTypeArgumentValue:
-      currentPropertyKey = sender.associatedKey;
-      self.eventProperties[currentPropertyKey] = sender.text;
-      break;
-    default:
-      break;
+  case MSPropertyTypeTargetKey:
+    selectedTarget = [self.transmissionTargetSelectorCell selectedTransmissionTarget];
+    currentPropertyKey = sender.associatedKey;
+    sender.associatedKey = sender.text;
+    currentPropertyValue = self.targetProperties[selectedTarget][currentPropertyKey];
+    target = [MSAnalytics transmissionTargetForToken:selectedTarget];
+    [target removeEventPropertyforKey:currentPropertyKey];
+    [target setEventPropertyString:currentPropertyValue forKey:sender.text];
+    [self.targetProperties[selectedTarget] removeObjectForKey:currentPropertyKey];
+    self.targetProperties[selectedTarget][sender.text] = currentPropertyValue;
+    break;
+  case MSPropertyTypeTargetValue:
+    selectedTarget = [self.transmissionTargetSelectorCell selectedTransmissionTarget];
+    currentPropertyKey = sender.associatedKey;
+    target = [MSAnalytics transmissionTargetForToken:selectedTarget];
+    [target setEventPropertyString:sender.text forKey:currentPropertyKey];
+    self.targetProperties[selectedTarget][currentPropertyKey] = sender.text;
+    break;
+  case MSPropertyTypeArgumentKey:
+    currentPropertyKey = sender.associatedKey;
+    sender.associatedKey = sender.text;
+    currentPropertyValue = self.eventProperties[currentPropertyKey];
+    [self.eventProperties removeObjectForKey:currentPropertyKey];
+    self.eventProperties[sender.text] = currentPropertyValue;
+    break;
+  case MSPropertyTypeArgumentValue:
+    currentPropertyKey = sender.associatedKey;
+    self.eventProperties[currentPropertyKey] = sender.text;
+    break;
+  default:
+    break;
   }
 }
 
@@ -277,9 +306,9 @@ typedef NS_ENUM(short, MSPropertyType) {
            editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
   if ([self isInsertRowAtIndexPath:indexPath]) {
     return UITableViewCellEditingStyleInsert;
-  } else if ([self isTargetSelectionRowAtIndexPath:indexPath]){
+  } else if ([self isTargetSelectionRowAtIndexPath:indexPath]) {
     return UITableViewCellEditingStyleNone;
-  }else {
+  } else {
     return UITableViewCellEditingStyleDelete;
   }
 }
@@ -296,8 +325,8 @@ typedef NS_ENUM(short, MSPropertyType) {
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
   if ([self isTargetPropertiesRowSection:section]) {
     return [self targetPropertyCount] + 2;
-  } else if ([self isTargetPropertiesRowSection:section]) {
-    return [self targetPropertyCount] + 1;
+  } else if ([self isEventPropertiesRowSection:section]) {
+    return [self eventPropertyCount] + 1;
   } else {
     return [super tableView:tableView numberOfRowsInSection:section];
   }
@@ -313,7 +342,8 @@ typedef NS_ENUM(short, MSPropertyType) {
 }
 
 - (NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath {
-  if (([self isTargetPropertiesRowSection:indexPath.section] && ![self isTargetSelectionRowAtIndexPath:indexPath]) || [self isEventPropertiesRowSection:indexPath.section]) {
+  if (([self isTargetPropertiesRowSection:indexPath.section] && ![self isTargetSelectionRowAtIndexPath:indexPath]) ||
+      [self isEventPropertiesRowSection:indexPath.section]) {
     return kPropertyIndentationLevel;
   } else {
     return kDefaultIndentationLevel;
@@ -328,7 +358,7 @@ typedef NS_ENUM(short, MSPropertyType) {
   return NO;
 }
 
--(void)setNewDefaultKey:(NSString**)key andValue:(NSString**)value{
+- (void)setNewDefaultKey:(NSString **)key andValue:(NSString **)value {
   *key = [NSString stringWithFormat:@"key%d", self.propertyCounter];
   *value = [NSString stringWithFormat:@"value%d", self.propertyCounter];
   self.propertyCounter++;
