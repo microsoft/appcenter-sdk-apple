05bbf184fc0010f4fd31e270800d03fa8275423f
Jae Lim <jaelim@microsoft.com>
2018-07-19 12:33:13 -0700

    Fix publish script

--- vsts/publish.sh
+++ vsts/publish.sh
@@ -64,7 +64,8 @@ else
 
   ## 0. Get artifact filename and commit hash from build
   prerelease=$(echo "$ARTIFACT_PATH"/*.zip | rev | cut -d/ -f1 | rev)
-  commit_hash="$(echo $prerelease | sed 's/'$FRAMEWORKS_ZIP_FILENAME'-[[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}-[[:digit:]]\{1,\}+\(.\{40\}\)\.zip.*/\1/1')"
+  zip_filename="$(echo $FRAMEWORKS_ZIP_FILENAME | cut -d. -f1)"
+  commit_hash="$(echo $prerelease | sed 's/'$zip_filename'-[[:digit:]]\{1,\}.[[:digit:]]\{1,\}.[[:digit:]]\{1,\}-[[:digit:]]\{1,\}+\(.\{40\}\)\.zip.*/\1/1')"
 
   ### Temporarily remove tvOS framework from binary
   unzip $ARTIFACT_PATH/$prerelease
