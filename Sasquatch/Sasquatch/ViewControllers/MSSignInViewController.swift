17694f04ba64379e5b1a08d9320d38163fe854e7
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-08-13 13:10:19 -0700

    Use separate constant for auth baseUrl and move ? char to different location

--- Sasquatch/Sasquatch/ViewControllers/MSSignInViewController.swift
+++ Sasquatch/Sasquatch/ViewControllers/MSSignInViewController.swift
@@ -11,13 +11,14 @@ class MSSignInViewController: UIViewController, WKNavigationDelegate {
 
   var webView: WKWebView!
 
-  let redirectUrl = "https://login.live.com/oauth20_desktop.srf"
-  let authorizeUrl = "https://login.live.com/oauth20_authorize.srf?"
-  let signOutUrl = "https://login.live.com/oauth20_logout.srf?"
+  let baseUrl = "https://login.live.com"
+  let redirectEndpoint = "/oauth20_desktop.srf"
+  let authorizeEndpoint = "/oauth20_authorize.srf"
+  let signOutEndpoint = "/oauth20_logout.srf"
   let clientId = "000000004C1D3F6C"
   let scope = "service::events.data.microsoft.com::MBI_SSL"
   lazy var clientIdParam = { return "&client_id=" + self.clientId }()
-  lazy var redirectParam = { return "redirect_uri=" + self.redirectUrl.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)! }()
+  lazy var redirectParam = { return "redirect_uri=" + (self.baseUrl + self.redirectEndpoint).addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)! }()
   lazy var scopeParam = { return "&scope=" + self.scope }()
 
   var action: AuthAction = .login
@@ -47,7 +48,7 @@ class MSSignInViewController: UIViewController, WKNavigationDelegate {
   }
 
   func login() {
-    if let signInUrl = URL(string: authorizeUrl + redirectParam + clientIdParam + "&response_type=token" + scopeParam) {
+    if let signInUrl = URL(string: self.baseUrl + self.authorizeEndpoint + "?" + redirectParam + clientIdParam + "&response_type=token" + scopeParam) {
       webView.load(URLRequest(url: signInUrl))
     }
   }
@@ -57,14 +58,14 @@ class MSSignInViewController: UIViewController, WKNavigationDelegate {
   }
 
   func signOut() {
-    if let signOutUrl = URL(string: signOutUrl + redirectParam + clientIdParam) {
-      webView.load(URLRequest(url: signOutUrl))
+    if let url = URL(string: self.baseUrl + self.signOutEndpoint + "?" + redirectParam + clientIdParam) {
+      webView.load(URLRequest(url: url))
     }
   }
 
   func checkSignIn(url: URL) {
-    if url.absoluteString.starts(with: redirectUrl) {
-      if let newUrl = URL(string: redirectUrl + "?" + url.fragment!) {
+    if url.absoluteString.starts(with: (self.baseUrl + self.redirectEndpoint)) {
+      if let newUrl = URL(string: self.baseUrl + self.redirectEndpoint + "?" + url.fragment!) {
         let token = newUrl.valueOf("access_token")!
         let userId = newUrl.valueOf("user_id")!
         let expiresIn = Int64(newUrl.valueOf("expires_in")!)!
@@ -76,7 +77,7 @@ class MSSignInViewController: UIViewController, WKNavigationDelegate {
   }
 
   func checkSignOut(url: URL) {
-    if url.absoluteString.starts(with: redirectUrl) {
+    if url.absoluteString.starts(with: (self.baseUrl + self.redirectEndpoint)) {
       if let error = url.valueOf("error") {
         NSLog("Error while signing out: %@", error)
       } else {
