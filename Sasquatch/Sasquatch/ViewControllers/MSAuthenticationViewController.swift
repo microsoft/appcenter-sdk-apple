16389ee7b73b83dc173c3a459ed55ac9b2de0a8b
Murat Baysangurov <v-mubays@microsoft.com>
2018-08-13 12:49:45 +0300

    Implement manual authentication

--- Sasquatch/Sasquatch/ViewControllers/MSAuthenticationViewController.swift
+++ Sasquatch/Sasquatch/ViewControllers/MSAuthenticationViewController.swift
@@ -3,72 +3,42 @@ import UIKit
 class MSAuthenticationViewController : UITableViewController, AppCenterProtocol {
 
   var appCenter: AppCenterDelegate!
+  var currentAction = MSSignInViewController.AuthAction.login
 
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
-  @IBAction func login() {
-
-  }
-
-  @IBAction func refresh() {
-
-  }
-
-  @IBAction func dismiss() {
-    dismiss(animated: true, completion: nil)
-  }
-
-  /*
-  let clientID = "06181c2a-2403-437f-a490-9bcb06f85281"
-  let scopes = ["User.Read"]
-
-  var application: MSALPublicClientApplication?
-  var authenticationResult: MSALResult?
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    if let app = try? MSALPublicClientApplication.init(clientId: clientID) {
-      self.application = app
-    } else {
-      NSLog("Error creating MSALPublicClientApplication object")
-    }
-  }
-
-  @IBAction func login() {
-    application?.acquireToken(forScopes: scopes, completionBlock: authenticationResultHandler())
-  }
-
-  @IBAction func refresh() {
-    if let result = authenticationResult {
-      application?.acquireTokenSilent(forScopes: scopes, user: result.user, completionBlock: authenticationResultHandler())
-    }
-  }
-
-  @IBAction func dismiss() {
-    dismiss(animated: true, completion: nil)
-  }
-
-  func updateAuthentication(forUser userId: String, expiryDate: Date, withAccessToken accessToken: String) {
-    appCenter.addAuthenticationProvider(withUserId: userId, expiryDate: expiryDate,  andAccessToken: accessToken)
-  }
-
-  func authenticationResultHandler() -> MSALCompletionBlock {
-    return { (result, error) in
-      if error == nil {
-        self.authenticationResult = result
-        self.updateAuthentication(forUser: result!.user.uid, expiryDate: result!.expiresOn, withAccessToken: result!.accessToken)
-        NSLog("Authenticated successfully. User id: %@", result!.user.uid)
-      } else {
-        let nserror = error! as NSError
-        if nserror.code == MSALErrorCode.interactionRequired.rawValue {
-          self.login()
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let signInController = segue.destination as? MSSignInViewController {
+      signInController.action = currentAction
+      signInController.onAuthDataRecieved = { [weak self](token, userId, expiresAt) in
+        if let vc = self {
+          vc.appCenter.addAuthenticationProvider(withUserId: userId, expiryDate: expiresAt, andAccessToken: token)
         }
-        NSLog("Error during authentication %@", nserror.localizedDescription)
       }
     }
   }
- */
+
+  @IBAction func login() {
+    showSignInController(action: MSSignInViewController.AuthAction.login)
+  }
+
+  @IBAction func refresh() {
+    showSignInController(action: MSSignInViewController.AuthAction.refresh)
+  }
+
+  @IBAction func signOut() {
+    showSignInController(action: MSSignInViewController.AuthAction.signout)
+  }
+
+  @IBAction func dismiss() {
+    dismiss(animated: true, completion: nil)
+  }
+
+  func showSignInController(action: MSSignInViewController.AuthAction) {
+    currentAction = action
+    self.performSegue(withIdentifier: "ShowSignIn", sender: self)
+  }
 }
 
--- Sasquatch/Sasquatch/ViewControllers/MSSignInViewController.swift
+++ Sasquatch/Sasquatch/ViewControllers/MSSignInViewController.swift
@@ -3,6 +3,13 @@ import WebKit
 
 class MSSignInViewController: UIViewController, WKNavigationDelegate {
 
+
+  var onAuthDataRecieved: ((_ token: String, _ userId: String, _ expiresAt: Date) -> Void)?
+
+  enum AuthAction {
+    case login, refresh, signout
+  }
+
   var webView: WKWebView!
 
   let redirectUrl = "https://login.live.com/oauth20_desktop.srf"
@@ -14,6 +21,8 @@ class MSSignInViewController: UIViewController, WKNavigationDelegate {
   lazy var redirectParam = { return "redirect_uri=" + self.redirectUrl.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)! }()
   lazy var scopeParam = { return "&scope=" + self.scope }()
 
+  var action: AuthAction = .login
+
   override func loadView() {
     let configuration = WKWebViewConfiguration()
     configuration.preferences.javaScriptEnabled = true
@@ -24,18 +33,80 @@ class MSSignInViewController: UIViewController, WKNavigationDelegate {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    process()
+  }
+
+  func process() {
+    switch action {
+    case .login:
+      login()
+    case .refresh:
+      refresh()
+    case .signout:
+      signOut()
+    }
+  }
+
+  func login() {
     if let signInUrl = URL(string: authorizeUrl + redirectParam + clientIdParam + "&response_type=token" + scopeParam) {
       webView.load(URLRequest(url: signInUrl))
     }
   }
 
-  func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
-    print("redirect to")
-    print(webView.url!)
+  func refresh() {
+
+  }
+
+  func signOut() {
+    if let signOutUrl = URL(string: signOutUrl + redirectParam + clientIdParam) {
+      webView.load(URLRequest(url: signOutUrl))
+    }
+  }
+
+  func checkSignIn(url: URL) {
+    if url.absoluteString.starts(with: redirectUrl) {
+      if let newUrl = URL(string: redirectUrl + "?" + url.fragment!) {
+        let token = newUrl.valueOf("access_token")!
+        let userId = newUrl.valueOf("user_id")!
+        let expiresIn = Int64(newUrl.valueOf("expires_in")!)!
+
+        onAuthDataRecieved?(token, userId, Date().addingTimeInterval(Double(expiresIn)))
+      }
+      close()
+    }
+  }
+
+  func checkSignOut(url: URL) {
+    if url.absoluteString.starts(with: redirectUrl) {
+      if let error = url.valueOf("error") {
+        NSLog("Error while signing out: %@", error)
+      } else {
+        NSLog("Successfully signed out")
+      }
+      close()
+    }
+  }
+
+  func close() {
+    self.dismiss(animated: true, completion: nil)
   }
 
   func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-    print("finish loading")
-    print(webView.url!)
+
+    switch action {
+    case .login:
+      checkSignIn(url: webView.url!)
+    case .signout:
+      checkSignOut(url: webView.url!)
+    case .refresh:
+      checkSignOut(url: webView.url!)
+    }
+  }
+}
+
+extension URL {
+  func valueOf(_ queryParamaterName: String) -> String? {
+    guard let url = URLComponents(string: self.absoluteString) else { return nil }
+    return url.queryItems?.first(where: { $0.name == queryParamaterName })?.value
   }
 }
