32c1e07dcda8627c09122bca1c98d2828b13098e
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-08-15 14:29:56 -0700

    Add header docs to MSOneCollectorIngestionPrivate.h

--- AppCenter/AppCenter/Internals/Ingestion/MSOneCollectorIngestionPrivate.h
+++ AppCenter/AppCenter/Internals/Ingestion/MSOneCollectorIngestionPrivate.h
@@ -4,6 +4,13 @@
 
 @interface MSOneCollectorIngestion()
 
+/**
+ * Obfuscate the header values.
+ *
+ * @param value The value that will be obfuscated.
+ * @param key The HTTP header key.
+ * @return The obfuscated value.
+ */
 - (NSString *)obfuscateHeaderValue:(NSString *)value forKey:(NSString *)key;
 
 @end
