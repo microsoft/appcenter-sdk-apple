3d2bd2e8f20f74e7520fdf038c2ab9252da4f562
Jae Lim <jaelim@microsoft.com>
2018-10-02 16:51:42 -0700

    Reformat SDK implementation in AppCenterPush project

--- AppCenterPush/AppCenterPush/Internal/Model/MSPushNotificationInternal.h
+++ AppCenterPush/AppCenterPush/Internal/Model/MSPushNotificationInternal.h
@@ -2,9 +2,6 @@
 
 @interface MSPushNotification ()
 
-- (instancetype)initWithTitle:(NSString *)title
-                      message:(NSString *)message
-                   customData:
-                       (NSDictionary<NSString *, NSString *> *)customData;
+- (instancetype)initWithTitle:(NSString *)title message:(NSString *)message customData:(NSDictionary<NSString *, NSString *> *)customData;
 
 @end
