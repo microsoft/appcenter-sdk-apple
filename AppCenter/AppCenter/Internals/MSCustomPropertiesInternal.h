95b5fef7b787bb42b867698692169aafdd88c370
Jacob Wallraff <jawal@microsoft.com>
2018-10-18 08:44:10 -0700

    Updating doc for propertiesImmutableCopy

--- AppCenter/AppCenter/Internals/MSCustomPropertiesInternal.h
+++ AppCenter/AppCenter/Internals/MSCustomPropertiesInternal.h
@@ -5,6 +5,11 @@
  */
 @interface MSCustomProperties ()
 
+/**
+ * Create an immutable copy of the properties dictionary to use in synchronized scenarios.
+ *
+ * @return An immutable copy of properties.
+ */
 - (NSDictionary<NSString *, NSObject *> *)propertiesImmutableCopy;
 
 @end
