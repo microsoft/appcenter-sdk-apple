f62600f292cf52f9b85e18778b1d03e1291c796a
Di Hei <dihei@microsoft.com>
2018-10-18 15:25:41 -0700

    Make the clang format script work for fork

--- clang-format-changed-files.sh
+++ clang-format-changed-files.sh
@@ -2,9 +2,10 @@
 
 # This script runs clang_format on all .h, .m, and .mm files that have changes from develop branch.
 # Before running this, ensure that your local develop branch is up to date.
-git fetch
+upstream=$(git remote -v | grep "Microsoft" | awk '{print $1}' | uniq)
+git fetch $upstream
 i=0
-for modified_file in $(git diff origin/develop --name-only *.h *.m *.mm)
+for modified_file in $(git diff $upstream/develop --name-only *.h *.m *.mm)
 do 
   clang-format -i -style=file $modified_file
   exit_code=$?
