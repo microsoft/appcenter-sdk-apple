a645cda31b5a1b7114454c4dc6a8dad1352d5cc9
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2019-01-10 16:41:37 -0800

    Revert change to MSData.
    We need to use MSOrderedDictionary in here.
    
    This reverts part of commit 19ddcf97732c4c7c2d7a68bd0193cb0669939b50.

--- AppCenter/AppCenter/Internals/Model/CommonSchema/MSCSData.m
+++ AppCenter/AppCenter/Internals/Model/CommonSchema/MSCSData.m
@@ -1,13 +1,23 @@
 #import "MSCSData.h"
+#import "MSCSModelConstants.h"
+#import "MSOrderedDictionary.h"
 
 @implementation MSCSData
 
 #pragma mark - MSSerializableObject
 
 - (NSMutableDictionary *)serializeToDictionary {
-  NSMutableDictionary *dict;
+  MSOrderedDictionary *dict;
   if (self.properties) {
-    dict = [NSMutableDictionary new];
+    dict = [MSOrderedDictionary new];
+
+    // ORDER MATTERS: Make sure baseType and baseData appear first in part B
+    if (self.properties[kMSDataBaseType]) {
+      dict[kMSDataBaseType] = self.properties[kMSDataBaseType];
+    }
+    if (self.properties[kMSDataBaseData]) {
+      dict[kMSDataBaseData] = self.properties[kMSDataBaseData];
+    }
     [dict addEntriesFromDictionary:self.properties];
   }
   return dict;
