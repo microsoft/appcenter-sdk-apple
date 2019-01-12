5dbc1720998c32b1868787fc150a376d67b7687c
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2019-01-11 16:03:07 -0800

    Adding some basic header docs for MSOrderedDictionary.

--- AppCenter/AppCenter/Internals/Util/MSOrderedDictionaryPrivate.h
+++ AppCenter/AppCenter/Internals/Util/MSOrderedDictionaryPrivate.h
@@ -2,8 +2,14 @@
 
 @interface MSOrderedDictionary ()
 
+/**
+ * An array containing the keys that are used to maintain the order.
+ */
 @property(nonatomic) NSMutableArray *order;
 
+/**
+ * The backing store for our ordered dictionary.
+ */
 @property(nonatomic) NSMutableDictionary *dictionary;
 
 @end
