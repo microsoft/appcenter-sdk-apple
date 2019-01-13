cf166680fc55c65882994d582b3c3241cbae3490
Andrei Shilkin <andrei.shilkin@akvelon.com>
2019-01-13 12:37:23 +0300

    Add User Id text field to macOS app

--- SasquatchMac/SasquatchMac/ViewControllers/AppCenterViewController.swift
+++ SasquatchMac/SasquatchMac/ViewControllers/AppCenterViewController.swift
@@ -7,6 +7,7 @@ class AppCenterViewController : NSViewController {
   @IBOutlet var installIdLabel : NSTextField?
   @IBOutlet var appSecretLabel : NSTextField?
   @IBOutlet var logURLLabel : NSTextField?
+  @IBOutlet var userIdLabel : NSTextField?
   @IBOutlet var setEnabledButton : NSButton?
 
   override func viewDidLoad() {
@@ -14,11 +15,19 @@ class AppCenterViewController : NSViewController {
     installIdLabel?.stringValue = appCenter.installId()
     appSecretLabel?.stringValue = appCenter.appSecret()
     logURLLabel?.stringValue = appCenter.logUrl()
+    userIdLabel?.stringValue = UserDefaults.standard.string(forKey: "userId") ?? ""
     setEnabledButton?.state = appCenter.isAppCenterEnabled() ? 1 : 0
   }
 
   @IBAction func setEnabled(sender : NSButton) {
     appCenter.setAppCenterEnabled(sender.state == 1)
     sender.state = appCenter.isAppCenterEnabled() ? 1 : 0
+  }
+
+  @IBAction func userIdChanged(sender: NSTextField) {
+    let text = sender.stringValue
+    let userId = !text.isEmpty ? text : nil
+    UserDefaults.standard.set(userId, forKey: "userId")
+    appCenter.setUserId(userId)
   }
 }
