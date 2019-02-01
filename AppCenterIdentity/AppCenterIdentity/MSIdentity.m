996ceb0c1eed8ae01ecd949ffccb5af95f37a61d
Jae Lim <jaelim@microsoft.com>
2019-01-31 16:46:00 -0800

    Removed unnecessary TODO

--- AppCenterIdentity/AppCenterIdentity/MSIdentity.m
+++ AppCenterIdentity/AppCenterIdentity/MSIdentity.m
@@ -235,8 +235,8 @@ static NSObject *const lock = @"lock";
                      (long)response.statusCode);
         }
       }];
-  // TODO we probably want to expose the finishAndInvalidate method from NSURLSession to use it here so we don't leak memory.
 }
+
 - (void)configAuthenticationClient {
 
   // Init MSAL client application.
