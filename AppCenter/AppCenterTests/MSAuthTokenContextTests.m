8911b8190db96c77118188143d7de0b859488e9c
Di Hei <dihei@microsoft.com>
2019-02-12 16:39:40 -0800

    add unit test for set auth token

---
+++ AppCenter/AppCenterTests/MSAuthTokenContextTests.m
@@ -0,0 +1,50 @@
+#import <Foundation/Foundation.h>
+
+#import "MSTestFrameworks.h"
+#import "MSAuthTokenContext.h"
+#import "MSAuthTokenContextDelegate.h"
+
+@interface MSAuthTokenContext ()
+
++ (void)resetSharedInstance;
+
+@end
+
+@interface MSAuthTokenContextTests : XCTestCase
+
+@property(nonatomic) MSAuthTokenContext *sut;
+
+@end
+
+@implementation MSAuthTokenContextTests
+
+#pragma mark - Houskeeping
+
+- (void)setUp {
+  [super setUp];
+  self.sut = [MSAuthTokenContext sharedInstance];
+}
+
+- (void)tearDown {
+  [MSAuthTokenContext resetSharedInstance];
+  [super tearDown];
+}
+
+#pragma mark - Tests
+
+- (void)testSetAuthToken {
+  
+  // If
+  NSString *expectedAuthToken = @"authToken1";
+  id<MSAuthTokenContextDelegate> delegateMock = OCMProtocolMock(@protocol(MSAuthTokenContextDelegate));
+  [self.sut addDelegate:delegateMock];
+  
+  // When
+  self.sut.authToken = expectedAuthToken;
+  
+  // Then
+  XCTAssertEqualObjects(self.sut.authToken, expectedAuthToken);
+  OCMVerify([delegateMock authTokenContext:self.sut didReceiveAuthToken:expectedAuthToken]);
+}
+
+@end
