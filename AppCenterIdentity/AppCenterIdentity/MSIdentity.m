abd6c5ba931c5ceeb5133a89714623b26bcb4a57
Roland Chernov <v-rolche@microsoft.com>
2019-02-18 18:41:31 +0300

    Remove work in progress comments

--- AppCenterIdentity/AppCenterIdentity/MSIdentity.m
+++ AppCenterIdentity/AppCenterIdentity/MSIdentity.m
@@ -156,11 +156,9 @@ static NSObject *lock = @"lock";
   self.loginDelayed = NO;
   [self.clientApplication acquireTokenForScopes:@[ (NSString * _Nonnull) self.identityConfig.identityScope ]
                                 completionBlock:^(MSALResult *result, NSError *e) {
-                                  // TODO: synchronize idToken assignment if it has threading issues
                                   if (e) {
                                     MSLogError([MSIdentity logTag], @"Couldn't initialize authentication client. Error: %@", e);
                                   } else {
-                                    // NSString __unused *accountIdentifier = result.account.homeAccountId.identifier;
                                     self.idToken = result.idToken;
                                   }
                                 }];
