15478f6b7d4dd19b17c2b0cf2edc3feb3103f43e
Quan Jin <v-qujin@microsoft.com>
2019-02-27 18:14:41 +0800

    fix format

--- AppCenterDistribute/AppCenterDistributeTests/Util/MSMockReachabilityUtil.m
+++ AppCenterDistribute/AppCenterDistributeTests/Util/MSMockReachabilityUtil.m
@@ -17,17 +17,17 @@ static NSString *kMSNetworkReachabilityChangedNotificationName = @"kMSNetworkRea
   self = [super init];
   if (self) {
     
-      // Mock MS_Reachability shared method to return this instance.
-      _mockReachabilityUtil = OCMClassMock([MS_Reachability class]);
-      OCMStub([_mockReachabilityUtil reachabilityForInternetConnection]).andReturn(_mockReachabilityUtil);
-      OCMStub([_mockReachabilityUtil currentReachabilityStatus]).andDo(^(NSInvocation *invocation) {
-        NetworkStatus status = self->_currentNetworkStatus;
-        [invocation setReturnValue:&status];
-      });
-      OCMStub([_mockReachabilityUtil startNotifier]).andDo(^(__unused NSInvocation *invocation) {
-        [[NSNotificationCenter defaultCenter] postNotificationName:kMSNetworkReachabilityChangedNotificationName
-                                                            object:self->_mockReachabilityUtil];
-      });
+    // Mock MS_Reachability shared method to return this instance.
+    _mockReachabilityUtil = OCMClassMock([MS_Reachability class]);
+    OCMStub([_mockReachabilityUtil reachabilityForInternetConnection]).andReturn(_mockReachabilityUtil);
+    OCMStub([_mockReachabilityUtil currentReachabilityStatus]).andDo(^(NSInvocation *invocation) {
+      NetworkStatus status = self->_currentNetworkStatus;
+      [invocation setReturnValue:&status];
+    });
+    OCMStub([_mockReachabilityUtil startNotifier]).andDo(^(__unused NSInvocation *invocation) {
+      [[NSNotificationCenter defaultCenter] postNotificationName:kMSNetworkReachabilityChangedNotificationName
+                                                          object:self->_mockReachabilityUtil];
+    });
   }
   return self;
 }
