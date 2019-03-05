db01e8050cd14cabe711d29ee95a995f96128c3c
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-03-05 16:40:02 +0300

    Added one test

--- AppCenterIdentity/AppCenterIdentityTests/MSIdentityTests.m
+++ AppCenterIdentity/AppCenterIdentityTests/MSIdentityTests.m
@@ -444,6 +444,28 @@ static NSString *const kMSTestAppSecret = @"TestAppSecret";
   [identityMock stopMocking];
 }
 
+- (void)testSignInIsNotDelayedWhenNoInternet {
+  
+  // If
+  self.sut.clientApplication = self.clientApplicationMock;
+  id reachability = OCMPartialMock([MS_Reachability reachabilityForInternetConnection]);
+  OCMStub([reachability currentReachabilityStatus]).andReturn(NotReachable);
+  id reachabilityMock = OCMClassMock([MS_Reachability class]);
+  OCMStub(ClassMethod([reachabilityMock reachabilityForInternetConnection])).andReturn(reachability);
+  OCMStub([self.clientApplicationMock acquireTokenForScopes:OCMOCK_ANY completionBlock:OCMOCK_ANY]).andDo(nil);
+  
+  id identityMock = OCMPartialMock(self.sut);
+  OCMStub([identityMock sharedInstance]).andReturn(identityMock);
+  OCMStub([identityMock canBeUsed]).andReturn(YES);
+  
+  // When
+  [MSIdentity signIn];
+  
+  // Then
+  XCTAssertFalse([identityMock signInDelayedAndRetryLater]);
+  [identityMock stopMocking];
+}
+
 - (void)testSignInDelayedWhenNoClientApplication {
 
   // If
