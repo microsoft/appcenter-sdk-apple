30e6708789a28dd4cc11a8aa3cbad56bb0a38115
Quan Jin <v-qujin@bys-mac-pro.local>
2019-03-05 15:19:31 +0800

    Move source files into appcentertests target

--- AppCenter/AppCenterTests/Util/MSMockReachability.h
+++ AppCenter/AppCenterTests/Util/MSMockReachability.h
@@ -3,8 +3,14 @@
 
 @interface MSMockReachability : NSObject
 
+/**
+ * Set current network status for mock MS_Reachability.
+ */
 + (void)setCurrentNetworkStatus:(NetworkStatus)networkStatus;
 
+/**
+ * Start to mock the MS_Reachability.
+ */
 + (id)startMocking;
 
 @end
