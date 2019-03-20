b0b2f6a15d215ff79efc6998f8fe70e412afafa2
Roland Chernov <v-rolche@microsoft.com>
2019-03-20 17:31:12 +0300

    Document MSAuthTokenInfo

--- AppCenter/AppCenter/Internals/Context/AuthToken/MSAuthTokenInfo.h
+++ AppCenter/AppCenter/Internals/Context/AuthToken/MSAuthTokenInfo.h
@@ -5,14 +5,34 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * Represents token data entity.
+ */
 @interface MSAuthTokenInfo : NSObject<NSCoding>
 
+/**
+ * Auth token string.
+ */
 @property(nonatomic, nullable, copy, readonly) NSString *authToken;
 
+/**
+ * Time and date from which the token began to act.
+ */
 @property(nonatomic, nullable, readonly) NSDate *startTime;
 
+/**
+ * Time and date to which token acted.
+ */
 @property(nonatomic, nullable, readonly) NSDate *endTime;
 
+/**
+ * Initialize a token info with required parameters.
+ *
+ * @param authToken Auth token.
+ * @param startTime Start time.
+ * @param endTime End time.
+ * @return Token info instance.
+ */
 - (instancetype)initWithAuthToken:(nullable NSString *)authToken
                      andStartTime:(nullable NSDate *)startTime
                        andEndTime:(nullable NSDate *)endTime;
