75ec0a64b8a4722c33d1d11ae88d95dd05d2c84b
Ivan Matkov <v-ivmatk@microsoft.com>
2019-03-20 09:39:45 -0700

    Fix typo
    Co-Authored-By: jaelim-ms <jaelim@microsoft.com>

--- CrashLib/CrashLib/MSCrashPrivInst.m
+++ CrashLib/CrashLib/MSCrashPrivInst.m
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-mport "MSCrashPrivInst.h"
+#import "MSCrashPrivInst.h"
 
 @implementation MSCrashPrivInst
 
