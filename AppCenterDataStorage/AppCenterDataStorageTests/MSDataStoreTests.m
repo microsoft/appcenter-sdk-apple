647f6ebfaf859a53ca7b2f3aa3be51f27a8d0a2b
Dmitrii Chichuk <jamminroot@gmail.com>
2019-03-22 20:14:40 +0300

    Add MSCosmosDB unit tests
    Revert "Add MSCosmoDB unit tests"
    
    This reverts commit cbeaff4fc678661876dd3da26cdb2afe8c796c51.
    
    Restore changes after reverse commit

--- AppCenterDataStorage/AppCenterDataStorageTests/MSDataStoreTests.m
+++ AppCenterDataStorage/AppCenterDataStorageTests/MSDataStoreTests.m
@@ -1,23 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#import <Foundation/Foundation.h>
-
-#if TARGET_OS_OSX
-#import <AppKit/AppKit.h>
-#else
-#import <UserNotifications/UserNotifications.h>
-#endif
-
-#import "MSCosmosDb.h"
 #import "MSChannelGroupProtocol.h"
-#import "MSChannelUnitProtocol.h"
-#import "MSTestFrameworks.h"
-#import "MSUserIdContextPrivate.h"
-#import "MSDataStore.h"
-#import "MSTokenResult.h"
+#import "MSCosmosDb.h"
 #import "MSCosmosDbIngestion.h"
-#import "MSDataStoreInternal.h"
+#import "MSTestFrameworks.h"
+#import "MSTokenResult.h"
 
 @interface MSTokenResult (Test)
 - (instancetype)initWithPartition:(NSString *)partition
@@ -30,10 +18,13 @@
 @end
 
 @interface MSCosmosDb (Test)
-+ (NSDictionary*) defaultHeaderWithPartition:(NSString *)partition
-dbToken:(NSString *)dbToken
-additionalHeaders:(NSDictionary *_Nullable)additionalHeaders;
-+ (NSString*) documentUrlWithTokenResult: tokenResult documentId:(NSString *)documentId ;
+
++ (NSDictionary *)defaultHeaderWithPartition:(NSString *)partition
+                                     dbToken:(NSString *)dbToken
+                           additionalHeaders:(NSDictionary *_Nullable)additionalHeaders;
+
++ (NSString *)documentUrlWithTokenResult:tokenResult documentId:(NSString *)documentId;
+
 @end
 
 @interface MSDataStoreTests : XCTestCase
@@ -45,72 +36,171 @@ additionalHeaders:(NSDictionary *_Nullable)additionalHeaders;
 @implementation MSDataStoreTests
 
 - (void)setUp {
-  [super setUp];
+    [super setUp];
 }
 
 - (void)tearDown {
-  [super tearDown];
+    [super tearDown];
 }
 
 - (void)testDefaultHeaderWithPartitionWithDictionaryNotNull {
+
+    // If
     NSMutableDictionary *_Nullable additionalHeaders = [NSMutableDictionary new];
-    [additionalHeaders setObject:@"Value1" forKey:@"Typy1"];
-    [additionalHeaders setObject:@"Value2" forKey:@"Typy2"];
-    [additionalHeaders setObject:@"Value3" forKey:@"Typy3"];
-    NSDictionary *dic = [MSCosmosDb defaultHeaderWithPartition:@"partition"
-                                                       dbToken:@"token"
-                                             additionalHeaders:additionalHeaders];
-    
+    additionalHeaders[@"Type1"] = @"Value1";
+    additionalHeaders[@"Type2"] = @"Value2";
+    additionalHeaders[@"Type3"] = @"Value3";
+
+    // When
+    NSDictionary *dic = [MSCosmosDb defaultHeaderWithPartition:@"partition" dbToken:@"token" additionalHeaders:additionalHeaders];
+
+    // Then
     XCTAssertNotNil(dic);
-    XCTAssertTrue(dic[@"Typy1"]);
-    XCTAssertTrue(dic[@"Typy2"]);
-    XCTAssertTrue(dic[@"Typy3"]);
+    XCTAssertTrue(dic[@"Type1"]);
+    XCTAssertTrue(dic[@"Type2"]);
+    XCTAssertTrue(dic[@"Type3"]);
 }
 
 - (void)testDefaultHeaderWithPartitionWithDictionaryNull {
-    NSDictionary *dic = [MSCosmosDb defaultHeaderWithPartition:@"partition"
-                                                       dbToken:@"token"
-                                             additionalHeaders:nil];
+
+    // If
+    NSDictionary *dic;
+
+    // When
+    dic = [MSCosmosDb defaultHeaderWithPartition:@"partition" dbToken:@"token" additionalHeaders:nil];
+
+    // Then
     XCTAssertNotNil(dic);
     XCTAssertTrue(dic[@"Content-Type"]);
 }
 
 - (void)testDocumentUrlWithTokenResultWithStringToken {
+
+    // If
     MSTokenResult *tokenResult = [[MSTokenResult alloc] initWithString:@"token"];
-    NSString *result = [MSCosmosDb documentUrlWithTokenResult:tokenResult
-                                                              documentId:@"documentId"];
+
+    // When
+    NSString *result = [MSCosmosDb documentUrlWithTokenResult:tokenResult documentId:@"documentId"];
+
+    // Then
     XCTAssertNotNil(result);
 }
 
 - (void)testDocumentUrlWithTokenResultWithObjectToken {
-    MSTokenResult *tokenResult = [[MSTokenResult alloc] initWithPartition:@"token"
-                                                                     dbAccount: @"dbAccount"
-                                                                        dbName: @"dbName"
-                                                              dbCollectionName: @"dbCollectionName"
-                                                                         token: @"token"
-                                                                        status: @"status"
-                                                                     expiresOn: @"expiresOn"];
-    NSString *result = [MSCosmosDb documentUrlWithTokenResult:tokenResult
-                                                   documentId:@"documentId"];
-    XCTAssertNotNil(result);
-    XCTAssertTrue([result isEqualToString:@"https://dbAccount.documents.azure.com/dbs/dbName/colls/dbCollectionName/docs/documentId"] );
 
+    // If
+    MSTokenResult *tokenResult;
+    NSString *testResult;
+
+    // When
+    tokenResult = [[MSTokenResult alloc] initWithPartition:@"token"
+                                                 dbAccount:@"dbAccount"
+                                                    dbName:@"dbName"
+                                          dbCollectionName:@"dbCollectionName"
+                                                     token:@"token"
+                                                    status:@"status"
+                                                 expiresOn:@"expiresOn"];
+    testResult = [MSCosmosDb documentUrlWithTokenResult:tokenResult documentId:@"documentId"];
+
+    // Then
+    XCTAssertNotNil(testResult);
+    XCTAssertTrue([testResult isEqualToString:@"https://dbAccount.documents.azure.com/dbs/dbName/colls/dbCollectionName/docs/documentId"]);
 }
 
 - (void)testDocumentUrlWithTokenResultWithDictionaryToken {
-    NSMutableDictionary *_Nullable tokenResult = [NSMutableDictionary new];
-    [tokenResult setObject:@"partition" forKey:@"partition"];
-    [tokenResult setObject:@"dbAccount" forKey:@"dbAccount"];
-    [tokenResult setObject:@"dbName" forKey:@"dbName"];
-    [tokenResult setObject:@"dbCollectionName" forKey:@"dbCollectionName"];
-    [tokenResult setObject:@"token" forKey:@"token"];
-    [tokenResult setObject:@"status" forKey:@"status"];
-    [tokenResult setObject:@"expiresOn" forKey:@"expiresOn"];
-    
-    NSString *result = [MSCosmosDb documentUrlWithTokenResult:tokenResult
-                                                   documentId:@"documentId"];
-    XCTAssertNotNil(result);
-    XCTAssertTrue([result isEqualToString:@"https://dbAccount.documents.azure.com/dbs/dbName/colls/dbCollectionName/docs/documentId"] );
+
+    // If
+    NSMutableDictionary *_Nullable tokenResultDictionary = [NSMutableDictionary new];
+    tokenResultDictionary[@"partition"] = @"partition";
+    tokenResultDictionary[@"dbAccount"] = @"dbAccountTest0";
+    tokenResultDictionary[@"dbName"] = @"dbNameTest1";
+    tokenResultDictionary[@"dbCollectionName"] = @"dbCollectionNameTest2";
+    tokenResultDictionary[@"token"] = @"token";
+    tokenResultDictionary[@"status"] = @"status";
+    tokenResultDictionary[@"expiresOn"] = @"expiresOn";
+    MSTokenResult *tokenResult = [[MSTokenResult alloc] initWithDictionary:tokenResultDictionary];
+    NSString *testResult;
+
+    // When
+
+    testResult = [MSCosmosDb documentUrlWithTokenResult:tokenResult documentId:@"documentId"];
+
+    // Then
+    XCTAssertNotNil(testResult);
+    XCTAssertTrue([testResult containsString:@"documentId"]);
+    XCTAssertTrue([testResult containsString:@"dbAccountTest0"]);
+    XCTAssertTrue([testResult containsString:@"dbNameTest1"]);
+    XCTAssertTrue([testResult containsString:@"dbCollectionNameTest2"]);
 }
 
+- (void)testPerformCosmosDbAsyncOperationWithHttpClientWithAdditionalParams {
+
+    // If
+    MSCosmosDbIngestion *httpClient = [[MSCosmosDbIngestion alloc] init];
+    NSMutableDictionary *tokenResultDictionary = [NSMutableDictionary new];
+    tokenResultDictionary[@"partition"] = @"partition";
+    tokenResultDictionary[@"dbAccount"] = @"dbAccount";
+    tokenResultDictionary[@"dbName"] = @"dbName";
+    tokenResultDictionary[@"dbCollectionName"] = @"dbCollectionName";
+    tokenResultDictionary[@"token"] = @"token";
+    tokenResultDictionary[@"status"] = @"status";
+    tokenResultDictionary[@"expiresOn"] = @"expiresOn";
+    MSTokenResult *tokenResult = [[MSTokenResult alloc] initWithDictionary:tokenResultDictionary];
+    NSMutableDictionary *additionalHeaders = [NSMutableDictionary new];
+    additionalHeaders[@"Foo"] = @"Bar";
+    __block BOOL completionHandlerCalled = NO;
+    XCTestExpectation *completeExpectation = [self expectationWithDescription:@"Task finished"];
+    MSCosmosDbCompletionHandler handler = ^(NSData *_Nullable data, NSError *_Nullable error) {
+      completionHandlerCalled = YES;
+      [completeExpectation fulfill];
+    };
+
+    // When
+    [MSCosmosDb performCosmosDbAsyncOperationWithHttpClient:httpClient
+                                                tokenResult:tokenResult
+                                                 documentId:@"documentID"
+                                                 httpMethod:@"GET"
+                                                       body:nil
+                                          additionalHeaders:additionalHeaders
+                                          completionHandler:handler];
+    [self waitForExpectationsWithTimeout:5 handler:NULL];
+
+    // Then
+    XCTAssertTrue([completeExpectation assertForOverFulfill]);
+    XCTAssertTrue(completionHandlerCalled);
+}
+
+- (void)testPerformCosmosDbAsyncOperationWithHttpClient {
+
+    // If
+    MSCosmosDbIngestion *httpClient = [[MSCosmosDbIngestion alloc] init];
+    NSMutableDictionary *tokenResultDictionary = [NSMutableDictionary new];
+    tokenResultDictionary[@"partition"] = @"partition";
+    tokenResultDictionary[@"dbAccount"] = @"dbAccount";
+    tokenResultDictionary[@"dbName"] = @"dbName";
+    tokenResultDictionary[@"dbCollectionName"] = @"dbCollectionName";
+    tokenResultDictionary[@"token"] = @"token";
+    tokenResultDictionary[@"status"] = @"status";
+    tokenResultDictionary[@"expiresOn"] = @"expiresOn";
+    MSTokenResult *tokenResult = [[MSTokenResult alloc] initWithDictionary:tokenResultDictionary];
+    __block BOOL completionHandlerCalled = NO;
+    XCTestExpectation *completeExpectation = [self expectationWithDescription:@"Task finished"];
+    MSCosmosDbCompletionHandler handler = ^(NSData *_Nullable data, NSError *_Nullable error) {
+      completionHandlerCalled = YES;
+      [completeExpectation fulfill];
+    };
+
+    // When
+    [MSCosmosDb performCosmosDbAsyncOperationWithHttpClient:httpClient
+                                                tokenResult:tokenResult
+                                                 documentId:@"documentID"
+                                                 httpMethod:@"GET"
+                                                       body:nil
+                                          completionHandler:handler];
+    [self waitForExpectationsWithTimeout:5 handler:NULL];
+
+    // Then
+    XCTAssertTrue([completeExpectation assertForOverFulfill]);
+    XCTAssertTrue(completionHandlerCalled);
+}
 @end
