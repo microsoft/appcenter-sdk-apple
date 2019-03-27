ab1f88090d7772cbaa9ae3e0e131f9bcdf28d2fd
Alexander Chocron <alchocro@microsoft.com>
2019-03-27 14:24:25 -0700

    Add comments to http client private

--- AppCenter/AppCenter/Internals/HttpClient/MSHttpClientPrivate.h
+++ AppCenter/AppCenter/Internals/HttpClient/MSHttpClientPrivate.h
@@ -8,11 +8,39 @@
 
 @interface MSHttpClient ()
 
+/**
+ * The HTTP session object.
+ */
 @property(nonatomic) NSURLSession *session;
+
+/**
+ * Reachability library object, which listens for changes in the network state.
+ */
 @property(nonatomic) MS_Reachability *reachability;
+
+/**
+ * Pending http calls.
+ */
 @property(nonatomic) NSMutableSet *pendingCalls;
+
+/**
+ * Time intervals between each retry, in seconds.
+ */
 @property(nonatomic) NSArray *retryIntervals;
+
+/**
+ * A boolean value set to YES if the client is paused or NO otherwise. While paused, the client will store new calls but not send them until resumed.
+ */
 @property(nonatomic) BOOL paused;
+
+/**
+ * A boolean value set to YES if the client is enabled or NO otherwise. While disabled, the client will not store any calls.
+ */
 @property(nonatomic) BOOL enabled;
+
+/**
+ * Configuration object for the NSURLSession. Need to store this because the session will need to be re-created after re-enabling the client.
+ */
 @property(nonatomic) NSURLSessionConfiguration *sessionConfiguration;
+
 @end
