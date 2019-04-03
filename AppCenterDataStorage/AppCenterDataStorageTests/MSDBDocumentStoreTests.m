ad0b14f7c1ea48970fc39541e52de50ad5159214
Alexander Chocron <alchocro@microsoft.com>
2019-04-03 11:04:51 -0700

    Write test code to add document to database

--- AppCenterDataStorage/AppCenterDataStorageTests/MSDBDocumentStoreTests.m
+++ AppCenterDataStorage/AppCenterDataStorageTests/MSDBDocumentStoreTests.m
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#import <sqlite3.h>
+
 #import "MSDBDocumentStorePrivate.h"
 #import "MSTestFrameworks.h"
 #import "MSMockDocument.h"
@@ -33,12 +35,11 @@
   MSMockDocument *document = [MSMockDocument new];
   NSString *accountId = @"account12";
   document.contentDictionary = @{@"key" : @"value"};
-  NSString *eTag = @"";
+  NSString *eTag = @"398";
   MSDBDocumentStore *sut = [MSDBDocumentStore new];
   [sut createUserStorageWithAccountId:accountId];
-  MSDocumentWrapper *addedDocumentWrapper = [[MSDocumentWrapper alloc] initWithDeserializedValue:document partition:partitionKey documentId:documentId eTag:eTag lastUpdatedDate:[NSDate date]];
   NSString *expectedPartition = [NSString stringWithFormat:@"%@-%@", partitionKey, accountId];
-  [sut createWithPartition:partitionKey document:addedDocumentWrapper writeOptions:[MSWriteOptions new]];
+  [self addDocumentToTable:document eTag:eTag partition:expectedPartition documentId:documentId];
 
   // When
   MSDocumentWrapper *documentWrapper = [sut readWithPartition:partitionKey documentId:documentId documentType:[document class] readOptions:[MSReadOptions new]];
@@ -50,6 +51,24 @@
   XCTAssertEqualObjects(documentWrapper.partition, expectedPartition);
   XCTAssertEqualObjects(documentWrapper.documentId, documentId);
 
+}
+
+- (void)addDocumentToTable:(id<MSSerializableDocument>) document eTag:(NSString *)eTag partition:(NSString *)partition documentId:(NSString *)documentId {
+  NSDictionary *documentDict = [document serializeToDictionary];
+  NSData *documentData = [NSKeyedArchiver archivedDataWithRootObject:documentDict];
+  NSString *base64Data = [documentData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
+  sqlite3 *db = [self openDatabase:kMSDBDocumentFileName];
+  NSString *insertQuery = [NSString stringWithFormat:@"INSERT INTO '%@' ('%@', '%@', '%@', '%@', '%@') VALUES ('%@', '%@', '%@', '%@', '%@')", kMSAppDocumentTableName,
+                           kMSIdColumnName, kMSPartitionColumnName, kMSETagColumnName, kMSDocumentColumnName, kMSDocumentIdColumnName, @0, partition, eTag, base64Data, documentId];
+  sqlite3_exec(db, [insertQuery UTF8String], NULL, NULL, NULL);
+  sqlite3_close(db);
+}
+
+- (sqlite3 *)openDatabase:(NSString *)path {
+  sqlite3 *db = NULL;
+  NSURL *dbURL = [MSUtility createFileAtPathComponent:path withData:nil atomically:NO forceOverwrite:NO];
+  sqlite3_open_v2([[dbURL absoluteString] UTF8String], &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_URI, NULL);
+  return db;
 }
 
 @end
