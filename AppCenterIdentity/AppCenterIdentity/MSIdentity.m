b18ec86595a43015308c3d0610ca633ca5164a81
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-04-03 19:23:03 +0300

    Update MSIdentity.m

--- AppCenterIdentity/AppCenterIdentity/MSIdentity.m
+++ AppCenterIdentity/AppCenterIdentity/MSIdentity.m
@@ -65,7 +65,7 @@ static dispatch_once_t onceToken;
                     appSecret:(nullable NSString *)appSecret
       transmissionTargetToken:(nullable NSString *)token
               fromApplication:(BOOL)fromApplication {
-  [[MSAuthTokenContext sharedInstance] doNotResetAuthTokenAfterStart];
+  [[MSAuthTokenContext sharedInstance] preventResetAuthTokenAfterStart];
   [super startWithChannelGroup:channelGroup appSecret:appSecret transmissionTargetToken:token fromApplication:fromApplication];
   MSLogVerbose([MSIdentity logTag], @"Started Identity service.");
 }
