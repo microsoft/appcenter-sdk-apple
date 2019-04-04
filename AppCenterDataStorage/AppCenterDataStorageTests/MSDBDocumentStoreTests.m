df5ccbdfd33a5bd920a0f60531978efca0d5cdc1
Yoann Chaudet <yochaude@microsoft.com>
2019-04-04 16:16:31 -0700

    Fix tests

--- AppCenterDataStorage/AppCenterDataStorageTests/MSDBDocumentStoreTests.m
+++ AppCenterDataStorage/AppCenterDataStorageTests/MSDBDocumentStoreTests.m
@@ -7,25 +7,13 @@
 
 @interface MSDBDocumentStoreTests : XCTestCase
 
-@property(nonatomic, nullable) MSDBDocumentStore *sut;
-
 @end
 
 @implementation MSDBDocumentStoreTests
 
-- (void)setUp {
-  [super setUp];
-  self.sut = [MSDBDocumentStore new];
-}
-
-- (void)tearDown {
-  [super tearDown];
-}
-
 - (void)testCreationOfApplicationLevelTable {
 
   // If
-  NSUInteger expectedSchemaVersion = 1;
   MSDBSchema *expectedSchema = @{kMSAppDocumentTableName : [MSDBDocumentStore columnsSchema]};
   NSDictionary *expectedColumnIndexes = @{
     kMSAppDocumentTableName : @{
@@ -42,24 +30,21 @@
   };
   OCMStub([MSDBStorage columnsIndexes:expectedSchema]).andReturn(expectedColumnIndexes);
 
-  // When - TODO change this pattern, already called in setup.
-  self.sut = [MSDBDocumentStore new];
+  // When
+  MSDBDocumentStore *sut = [MSDBDocumentStore new];
 
   // Then
   OCMVerify([MSDBStorage columnsIndexes:expectedSchema]);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSIdColumnName] integerValue], self.sut.idColumnIndex);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSPartitionColumnName] integerValue], self.sut.partitionColumnIndex);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSDocumentIdColumnName] integerValue], self.sut.documentIdColumnIndex);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSDocumentColumnName] integerValue], self.sut.documentColumnIndex);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSETagColumnName] integerValue], self.sut.eTagColumnIndex);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSExpirationTimeColumnName] integerValue],
-                 self.sut.expirationTimeColumnIndex);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSDownloadTimeColumnName] integerValue],
-                 self.sut.downloadTimeColumnIndex);
-  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSOperationTimeColumnName] integerValue],
-                 self.sut.operationTimeColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSIdColumnName] integerValue], sut.idColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSPartitionColumnName] integerValue], sut.partitionColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSDocumentIdColumnName] integerValue], sut.documentIdColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSDocumentColumnName] integerValue], sut.documentColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSETagColumnName] integerValue], sut.eTagColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSExpirationTimeColumnName] integerValue], sut.expirationTimeColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSDownloadTimeColumnName] integerValue], sut.downloadTimeColumnIndex);
+  XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSOperationTimeColumnName] integerValue], sut.operationTimeColumnIndex);
   XCTAssertEqual([expectedColumnIndexes[kMSAppDocumentTableName][kMSPendingOperationColumnName] integerValue],
-                 self.sut.pendingOperationColumnIndex);
+                 sut.pendingOperationColumnIndex);
 }
 
 - (void)testCreationOfUserLevelTable {
@@ -69,10 +54,13 @@
   NSString *tableName = [NSString stringWithFormat:kMSUserDocumentTableNameFormat, expectedAccountId];
 
   // When
-  [self.sut createUserStorageWithAccountId:expectedAccountId];
+  id dbStorageMock = OCMClassMock([MSDBStorage class]);
+  MSDBDocumentStore *sut = [MSDBDocumentStore new];
+  sut.dbStorage = dbStorageMock;
+  [sut createUserStorageWithAccountId:expectedAccountId];
 
   // Then
-  OCMVerify([self.dbStorageMock createTable:tableName columnsSchema:[MSDBDocumentStore columnsSchema]]);
+  OCMVerify([dbStorageMock createTable:tableName columnsSchema:[MSDBDocumentStore columnsSchema]]);
 }
 
 - (void)testDeletionOfUserLevelTable {
@@ -82,10 +70,13 @@
   NSString *userTableName = [NSString stringWithFormat:kMSUserDocumentTableNameFormat, expectedAccountId];
 
   // When
-  [self.sut deleteUserStorageWithAccountId:expectedAccountId];
+  id dbStorageMock = OCMClassMock([MSDBStorage class]);
+  MSDBDocumentStore *sut = [MSDBDocumentStore new];
+  sut.dbStorage = dbStorageMock;
+  [sut deleteUserStorageWithAccountId:expectedAccountId];
 
   // Then
-  OCMVerify([self.dbStorageMock dropTable:userTableName]);
+  OCMVerify([dbStorageMock dropTable:userTableName]);
 }
 
 @end
