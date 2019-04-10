112ec066bcbbed555f83e3494d4a3f80921fe584
Dmitrii Chichuk <jamminroot@gmail.com>
2019-04-10 10:43:03 +0300

    Remove unnecessary import

--- AppCenter/AppCenter/Internals/Context/UserId/MSUserIdContextDelegate.h
+++ AppCenter/AppCenter/Internals/Context/UserId/MSUserIdContextDelegate.h
@@ -1,8 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
-#import <Foundation/Foundation.h>
-
 NS_ASSUME_NONNULL_BEGIN
 
 @class MSUserIdContext;
