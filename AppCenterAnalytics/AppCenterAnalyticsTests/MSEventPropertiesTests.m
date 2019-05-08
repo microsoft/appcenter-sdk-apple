6e5b4c1a66bf3a7baa16c6aa2dfd834ba739ff77
Michael Smith <mismith@microsoft.com>
2019-05-08 10:08:36 -0700

    add test coverage

--- AppCenterAnalytics/AppCenterAnalyticsTests/MSEventPropertiesTests.m
+++ AppCenterAnalytics/AppCenterAnalyticsTests/MSEventPropertiesTests.m
@@ -20,7 +20,7 @@
 - (void)testInitWithStringDictionaryWhenStringDictionaryHasValues {
 
   // If
-  NSDictionary *stringProperties = @{ @"key1" : @"val1", @"key2" : @"val2" };
+  NSDictionary *stringProperties = @{@"key1" : @"val1", @"key2" : @"val2"};
 
   // When
   MSEventProperties *sut = [[MSEventProperties alloc] initWithStringDictionary:stringProperties];
@@ -220,6 +220,18 @@
 
   // Then
   XCTAssertTrue(isEmpty);
+}
+
+- (void)testIsNoWhenEqualsWrongClass {
+
+  // If
+  NSObject *invalidEvent = [NSObject new];
+
+  // When
+  BOOL result = [MSEventProperties isEqual:invalidEvent];
+
+  // Then
+  XCTAssertFalse(result);
 }
 
 - (void)testIsEmptyReturnsFalseWhenContainsProperties {
