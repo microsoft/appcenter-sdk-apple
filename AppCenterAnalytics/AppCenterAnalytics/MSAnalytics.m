7e19413988b3454b0375a7470acb5a81114f82f3
Jae Lim <jaelim@microsoft.com>
2019-05-16 11:41:01 -0700

    Revert "Simplify log message"
    This reverts commit 44be3ffb86bddeed494672c09852df446e7e6055.

--- AppCenterAnalytics/AppCenterAnalytics/MSAnalytics.m
+++ AppCenterAnalytics/AppCenterAnalytics/MSAnalytics.m
@@ -393,7 +393,9 @@ __attribute__((used)) static void importCategories() { [NSString stringWithForma
     return;
   }
   if (interval > kMSFlushIntervalMaximum || interval < kMSFlushIntervalMinimum) {
-    MSLogError([MSAnalytics logTag], @"The transmission interval is not valid.");
+    MSLogError(
+        [MSAnalytics logTag], @"The transmission interval is not valid, it should be between %u second(s) and %u second(s) (%u day(s)).",
+        (unsigned int)kMSFlushIntervalMinimum, (unsigned int)kMSFlushIntervalMaximum, (unsigned int)(kMSFlushIntervalMaximum / 86400));
     return;
   }
   self.flushInterval = interval;
