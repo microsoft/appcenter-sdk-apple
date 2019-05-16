84236792237526a1c4f48dd849fa64643fcbb4be
Alexander Chocron <alchocro@microsoft.com>
2019-05-16 09:51:10 -0700

    Move encryption constants

--- AppCenter/AppCenter/Internals/Util/MSConstants+Internal.h
+++ AppCenter/AppCenter/Internals/Util/MSConstants+Internal.h
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#import <CommonCrypto/CommonCryptor.h>
 #import <Foundation/Foundation.h>
 
 // Device manufacturer
@@ -91,22 +90,3 @@ static const NSUInteger kMSPersistenceFlagsMask = 0xFF;
  * Common schema prefix separator used in various field values.
  */
 static NSString *const kMSCommonSchemaPrefixSeparator = @":";
-
-// Encryption constants.
-static int const kMSEncryptionAlgorithm = kCCAlgorithmAES;
-static NSString *const kMSEncryptionAlgorithmName = @"AES";
-static NSString *const kMSEncryptionCipherMode = @"CBC";
-
-// One year.
-static NSTimeInterval const kMSEncryptionKeyLifetimeInSeconds = 365 * 24 * 60 * 60;
-static int const kMSEncryptionKeySize = kCCKeySizeAES256;
-static NSString *const kMSEncryptionKeyMetadataKey = @"MSEncryptionKeyMetadata";
-static NSString *const kMSEncryptionKeyTagAlternate = @"kMSEncryptionKeyTagAlternate";
-static NSString *const kMSEncryptionKeyTagOriginal = @"kMSEncryptionKeyTag";
-
-// This separator is used for key metadata, as well as between metadata that is prepended to the cipher text.
-static NSString *const kMSEncryptionMetadataInternalSeparator = @"/";
-
-// This separator is only used between the metadata and cipher text of the encryption result.
-static NSString *const kMSEncryptionMetadataSeparator = @":";
-static NSString *const kMSEncryptionPaddingMode = @"PKCS7";
