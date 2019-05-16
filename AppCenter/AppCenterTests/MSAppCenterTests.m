179d834c02248d34d3fb2793bad6e8ba78f7454f
Quan Jin <v-qujin@bys-Mac-Pro.guest.corp.microsoft.com>
2019-05-16 10:26:01 +0800

    change missed items

--- AppCenter/AppCenterTests/MSAppCenterTests.m
+++ AppCenter/AppCenterTests/MSAppCenterTests.m
@@ -492,8 +492,8 @@ static NSString *const kMSNullifiedInstallIdString = @"00000000-0000-0000-0000-0
   [MSAppCenter start:@"AppSecret" withServices:@[ MSMockService.class, MSMockSecondService.class ]];
 
   // Then
-  XCTAssertFalse([[MSMockService sharedInstance] isStarted]);
-  XCTAssertFalse([[MSMockSecondService sharedInstance] isStarted]);
+  XCTAssertFalse([MSMockService sharedInstance].started);
+  XCTAssertFalse([MSMockSecondService sharedInstance].started);
 
   // Repeat previous test but with some whitespace.
   // If
@@ -507,8 +507,8 @@ static NSString *const kMSNullifiedInstallIdString = @"00000000-0000-0000-0000-0
   [MSAppCenter start:@"AppSecret" withServices:@[ MSMockService.class, MSMockSecondService.class ]];
 
   // Then
-  XCTAssertFalse([[MSMockService sharedInstance] isStarted]);
-  XCTAssertFalse([[MSMockSecondService sharedInstance] isStarted]);
+  XCTAssertFalse([MSMockService sharedInstance].started);
+  XCTAssertFalse([MSMockSecondService sharedInstance].started);
 
   // Special tear down.
   setenv(disableVariableCstr, "", 1);
