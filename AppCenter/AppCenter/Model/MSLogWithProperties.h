e0000188f7c0ea382e3e02e2da4c192943319bc0
Michael Buckley <michael@buckleyisms.com>
2019-07-23 09:12:40 -0700

    Fix warnings in publc headers caused by missing lifetime attributes for object properties

--- AppCenter/AppCenter/Model/MSLogWithProperties.h
+++ AppCenter/AppCenter/Model/MSLogWithProperties.h
@@ -10,6 +10,6 @@
 /**
  * Additional key/value pair parameters. [optional]
  */
-@property(nonatomic) NSDictionary<NSString *, NSString *> *properties;
+@property(nonatomic, copy) NSDictionary<NSString *, NSString *> *properties;
 
 @end
