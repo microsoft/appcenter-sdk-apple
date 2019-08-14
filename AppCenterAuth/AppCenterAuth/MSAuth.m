ab869bf34d301c78a03dbe24b6b85dfe47ce83d9
Clement Polet <clpolet@microsoft.com>
2019-08-13 17:16:14 -0700

    Use MSALAccount.identifier instead of MSALAccount.tenantId + fix unit tests

--- AppCenterAuth/AppCenterAuth/MSAuth.m
+++ AppCenterAuth/AppCenterAuth/MSAuth.m
@@ -490,7 +490,7 @@ static dispatch_once_t onceToken;
                       NSString *accountId = result.account.identifier;
                       [[MSAuthTokenContext sharedInstance] setAuthToken:result.idToken withAccountId:accountId expiresOn:result.expiresOn];
                       MSLogInfo([MSAuth logTag], @"Silent acquisition of token succeeded.");
-                      MSUserInformation *userInformation = [[MSUserInformation alloc] initWithAccountId:result.tenantProfile.tenantId
+                      MSUserInformation *userInformation = [[MSUserInformation alloc] initWithAccountId:result.tenantProfile.identifier
                                                                                             accessToken:result.accessToken
                                                                                                 idToken:result.idToken];
                       handler(userInformation, nil);
@@ -521,7 +521,7 @@ static dispatch_once_t onceToken;
                 NSString *accountId = result.account.identifier;
                 [[MSAuthTokenContext sharedInstance] setAuthToken:result.idToken withAccountId:accountId expiresOn:result.expiresOn];
                 MSLogInfo([MSAuth logTag], @"User sign-in succeeded.");
-                MSUserInformation *userInformation = [[MSUserInformation alloc] initWithAccountId:result.tenantProfile.tenantId
+                MSUserInformation *userInformation = [[MSUserInformation alloc] initWithAccountId:result.tenantProfile.identifier
                                                                                       accessToken:result.accessToken
                                                                                           idToken:result.idToken];
                 handler(userInformation, nil);
