b34fa7f4277b8536941988adb09f504ab05006a5
Dmitrii Chichuk <v-dmchic@microsoft.com>
2019-08-21 11:01:37 +0300

    Draft changes

--- AppCenterAnalytics/AppCenterAnalytics/AppCenterAnalytics.h
+++ AppCenterAnalytics/AppCenterAnalytics/AppCenterAnalytics.h
@@ -1,13 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-#import "MSAbstractLog.h"
-#import "MSAnalytics.h"
-#import "MSAnalyticsTransmissionTarget.h"
-#import "MSAnalyticsAuthenticationProvider.h"
-#import "MSAnalyticsAuthenticationProviderDelegate.h"
-#import "MSConstants+Flags.h"
-#import "MSEventLog.h"
-#import "MSEventProperties.h"
+//! Project version number for appcenteranalytics.
+FOUNDATION_EXPORT double appcenteranalyticsVersionNumber;
+
+//! Project version string for appcenteranalytics.
+FOUNDATION_EXPORT const unsigned char appcenteranalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <appcenteranalytics/PublicHeader.h>
+
+
--- AppCenterAuth/AppCenterAuth/AppCenterAuth.h
+++ AppCenterAuth/AppCenterAuth/AppCenterAuth.h
@@ -1,8 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-#import "MSAuth.h"
-#import "MSAuthErrors.h"
-#import "MSUserInformation.h"
+//! Project version number for appcenterauth.
+FOUNDATION_EXPORT double appcenterauthVersionNumber;
+
+//! Project version string for appcenterauth.
+FOUNDATION_EXPORT const unsigned char appcenterauthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <appcenterauth/PublicHeader.h>
+
+
