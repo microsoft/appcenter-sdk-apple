77ef281e0ece47d93bb549633d999202866360ae
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-08-22 13:55:28 +0300

    Make scheme not shared

--- Scripts/AppCenter/build-appcenter-ios-framework.sh
+++ Scripts/AppCenter/build-appcenter-ios-framework.sh
@@ -3,7 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-source $(dirname "$0")/../build-ios-framework.sh AppCenterFramework
+source $(dirname "$0")/../build-ios-framework.sh AppCenter
 
 rm -r "${WRK_DIR}"
 
