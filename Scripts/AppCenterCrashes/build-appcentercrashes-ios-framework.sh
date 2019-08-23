b229de28c71d306e25bcbc4a65a45acd1b7dc9cb
Ivan Matkov <v-ivmatk@microsoft.com>
2019-08-23 17:35:34 +0300

    Fix test schemes for Crashes

--- Scripts/AppCenterCrashes/build-appcentercrashes-ios-framework.sh
+++ Scripts/AppCenterCrashes/build-appcentercrashes-ios-framework.sh
@@ -9,7 +9,7 @@ source $(dirname "$0")/../build-ios-framework.sh
 if [ -z $(otool -L "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" | grep 'libCrashReporter') ]; then
   if [ -z "$MS_ARM64E_XCODE_PATH" ] || [ ! -d "$MS_ARM64E_XCODE_PATH" ]; then
     echo "Use legacy Xcode and link PLCR via libtool."
-    libtool -static  "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${SRCROOT}/../Vendor/iOS/PLCrashReporter/CrashReporter.framework/Versions/A/CrashReporter" -o "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}"
+    libtool -static "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${SRCROOT}/../Vendor/iOS/PLCrashReporter/CrashReporter.framework/Versions/A/CrashReporter" -o "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}"
   else
     echo "Use arm64e Xcode and link PLCR via libtool."
     env DEVELOPER_DIR="$MS_ARM64E_XCODE_PATH" /usr/bin/libtool -static "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${SRCROOT}/../Vendor/iOS/PLCrashReporter/CrashReporter.framework/Versions/A/CrashReporter" -o "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}"
