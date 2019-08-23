5458f53d3e7476a0f1a18e0d0ce966d8ee2d3715
Ivan Matkov <v-ivmatk@microsoft.com>
2019-08-23 11:50:33 +0300

    Update Data, Distribute and Push targets

--- Scripts/AppCenterData/build-appcenterdata-ios-framework.sh
+++ Scripts/AppCenterData/build-appcenterdata-ios-framework.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-source $(dirname "$0")/../build-ios-framework.sh AppCenterData
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
-rm -r "${WRK_DIR}"
+source $(dirname "$0")/../build-ios-framework.sh
+
+rm -r "${WORK_DIR}"
