84c475a18dada04bde15785aec50c970e8e3ae12
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-08-29 20:40:51 +0300

    Fix for podspecs

--- Scripts/AppCenterCrashes/build-appcentercrashes-macos-framework.sh
+++ Scripts/AppCenterCrashes/build-appcentercrashes-macos-framework.sh
@@ -5,8 +5,6 @@
 
 source $(dirname "$0")/../build-macos-framework.sh
 
-if [ -z $(otool -L "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" | grep 'libCrashReporter') ]; then
-  libtool -static -o "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${SRCROOT}/../Vendor/macOS/PLCrashReporter/libCrashReporter-MacOSX-Static.a"
-fi
+
 
 rm -r "${WORK_DIR}"
