43a93c90400620330659a8f515516cc680baf32c
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-08-30 10:57:10 +0400

    add PLCrashReporter as static lib

--- Scripts/AppCenterCrashes/build-appcentercrashes-ios-framework.sh
+++ Scripts/AppCenterCrashes/build-appcentercrashes-ios-framework.sh
@@ -5,15 +5,4 @@
 
 source $(dirname "$0")/../build-ios-framework.sh
 
-# Add PLCrashReporter.
-if [ -z $(otool -L "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" | grep 'libCrashReporter') ]; then
-  if [ -z "$MS_ARM64E_XCODE_PATH" ] || [ ! -d "$MS_ARM64E_XCODE_PATH" ]; then
-    echo "Use legacy Xcode and link PLCR via libtool."
-    libtool -static "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${SRCROOT}/../Vendor/iOS/PLCrashReporter/CrashReporter.framework/Versions/A/CrashReporter" -o "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}"
-  else
-    echo "Use arm64e Xcode and link PLCR via libtool."
-    env DEVELOPER_DIR="$MS_ARM64E_XCODE_PATH" /usr/bin/libtool -static "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${SRCROOT}/../Vendor/iOS/PLCrashReporter/CrashReporter.framework/Versions/A/CrashReporter" -o "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}"
-  fi
-fi
-
 rm -r "${WORK_DIR}"
--- Scripts/AppCenterCrashes/build-appcentercrashes-tvos-framework.sh
+++ Scripts/AppCenterCrashes/build-appcentercrashes-tvos-framework.sh
@@ -5,8 +5,4 @@
 
 source $(dirname "$0")/../build-tvos-framework.sh
 
-if [ -z $(otool -L "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" | grep 'libCrashReporter') ]; then
- libtool -static -o "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${PRODUCTS_DIR}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${SRCROOT}/../Vendor/tvOS/PLCrashReporter/CrashReporter.framework/Versions/A/CrashReporter"
-fi
-
 rm -r "${WORK_DIR}"
