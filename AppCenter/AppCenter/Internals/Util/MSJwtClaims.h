6e1d694e339f0b78197a68b61cdf7106f7ff775b
Michael Smith <mismith@microsoft.com>
2019-09-05 14:11:00 -0700

    Added JWTClaims utility

---
+++ AppCenter/AppCenter/Internals/Util/MSJwtClaims.h
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#import <Foundation/Foundation.h>
+
+@interface MSJwtClaims : NSObject
+
+/*
+ * The JWT subject.
+ */
+@property(nonatomic, copy, readonly) NSString *subject;
+
+/*
+ * The JWT expiration date.
+ */
+@property(nonatomic, copy, readonly) NSDate *expirationDate;
+
+/*
+ * Parses the JWT.
+ *
+ * @param jwt The jwt string.
+ *
+ * @return MSJwtCLaims instance if the string is a valid JWT.
+ */
++ (MSJwtClaims *)parse:(NSString *)jwt;
+
+/*
+ * Returns the subject.
+ *
+ * @return NSString The subject.
+ */
+- (NSString *)getSubject;
+
+/*
+ * Returns the expiration date.
+ *
+ * @return NSDate The expiration date.
+ */
+- (NSDate *)getExpirationDate;
+
+/*
+ * Initializes the MSJwtClaims with the subject and expiration
+ *
+ * @param subject The JWT subject.
+ * @param expirationDate the JWT expiration date.
+ *
+ * @return MSJwtClaims instance.
+ */
+- (instancetype)initWithClaims:(NSString *)subject
+               expirationDate:(NSDate *)expirationDate;
+
+@end
---
+++ AppCenter/AppCenter/Internals/Util/MSJwtClaims.m
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#import <Foundation/Foundation.h>
+#import "MSAppCenterInternal.h"
+#import "MSJwtClaims.h"
+#import "MSLogger.h"
+
+static NSString *const JWT_PARTS_SEPARATOR_REGEX = @"\\.";
+static NSString *const SUBJECT = @"sub";
+static NSString *const EXPIRATION = @"exp";
+
+@implementation MSJwtClaims
+
+- (instancetype)initWithClaims:(NSString *)subject
+                              expirationDate:(NSDate *)expirationDate {
+    self = [super init];
+    if (self) {
+        _subject = subject;
+        _expirationDate = expirationDate;
+    }
+    return self;
+}
+
++ (MSJwtClaims *)parse:(NSString *)jwt {
+    NSArray *parts = [jwt componentsSeparatedByString:JWT_PARTS_SEPARATOR_REGEX];
+    if ((sizeof parts) < 2) {
+        MSLogError(MSAppCenter.logTag, @"Failed to parse JWT, not enough parts.");
+        return nil;
+    }
+    NSString *base64ClaimsPart = parts[1];
+    @try {
+        NSError *error;
+        NSData *claimsPartData = [[NSData alloc] initWithBase64EncodedString:base64ClaimsPart options:0];
+        NSDictionary *claims = [NSJSONSerialization JSONObjectWithData:claimsPartData options:0 error:&error];
+        return [[MSJwtClaims alloc] initWithClaims:[claims objectForKey:SUBJECT] expirationDate:[claims objectForKey:EXPIRATION]];
+    }
+    @catch (NSException *e) {
+        MSLogError(MSAppCenter.logTag, @"Failed to parse JWT: %@", e);
+        return nil;
+    }
+}
+
+- (NSString*)getSubject {
+    return self.subject;
+}
+
+- (NSDate*)getExpirationDate {
+    return self.expirationDate;
+}
+
+@end
