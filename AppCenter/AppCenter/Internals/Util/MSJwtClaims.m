af687768d73f456c155cc14ab81ba45b9e266878
Jacob Wallraff <jawal@microsoft.com>
2019-09-17 14:28:54 -0700

    Removing try-catch block

--- AppCenter/AppCenter/Internals/Util/MSJwtClaims.m
+++ AppCenter/AppCenter/Internals/Util/MSJwtClaims.m
@@ -33,24 +33,24 @@ static NSString *const kMSExpirationClaim = @"exp";
     return nil;
   }
   NSString *base64ClaimsPart = parts[1];
-  @try {
-    NSError *error;
-    NSData *claimsPartData = [[NSData alloc] initWithBase64EncodedString:base64ClaimsPart options:0];
-    NSDictionary *claims = [NSJSONSerialization JSONObjectWithData:claimsPartData options:0 error:&error];
-    if ([claims objectForKey:kMSExpirationClaim] == nil || [claims objectForKey:kMSSubjectClaim] == nil) {
-      MSLogError([MSAppCenter logTag], @"Deserialized claims is missing `sub` or `exp`.");
-      return nil;
-    }
-    NSObject *expirationTimeIntervalSince1970 = [claims objectForKey:kMSExpirationClaim];
-    if ([expirationTimeIntervalSince1970 isKindOfClass:[NSNumber class]]) {
-      return [[MSJwtClaims alloc] initWithSubject:[claims objectForKey:kMSSubjectClaim]
-                                   expiration:[[NSDate alloc] initWithTimeIntervalSince1970:[((NSNumber *)expirationTimeIntervalSince1970) intValue]]];
-    }
-    return nil;
-  } @catch (NSException *e) {
-    MSLogError([MSAppCenter logTag], @"Failed to parse JWT: %@", e);
+  NSError *error;
+  NSData *claimsPartData = [[NSData alloc] initWithBase64EncodedString:base64ClaimsPart options:0];
+  NSDictionary *claims = [NSJSONSerialization JSONObjectWithData:claimsPartData options:0 error:&error];
+  if (error != nil) {
+    MSLogError([MSAppCenter logTag], @"Failed to parse JWT: %@", error);
     return nil;
   }
+  if ([claims objectForKey:kMSExpirationClaim] == nil || [claims objectForKey:kMSSubjectClaim] == nil) {
+    MSLogError([MSAppCenter logTag], @"Failed to parse JWT: seserialized claims missing `sub` or `exp`.");
+    return nil;
+  }
+  NSObject *expirationTimeIntervalSince1970 = [claims objectForKey:kMSExpirationClaim];
+  if (![expirationTimeIntervalSince1970 isKindOfClass:[NSNumber class]]) {
+    MSLogError([MSAppCenter logTag], @"Failed to parse JWT, `exp` claim in incorrect format.");
+    return nil;
+  }
+  return [[MSJwtClaims alloc] initWithSubject:[claims objectForKey:kMSSubjectClaim]
+                                   expiration:[[NSDate alloc] initWithTimeIntervalSince1970:[((NSNumber *)expirationTimeIntervalSince1970) intValue]]];
 }
 
 @end
