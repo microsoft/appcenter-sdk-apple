10c2fe9bd11372adb718212e21ee39a1d878fe1d
Jae Lim <jaelim@microsoft.com>
2019-09-17 16:37:38 -0700

    Add carthage in the middle of zip file

--- vsts/publish.sh
+++ vsts/publish.sh
@@ -194,7 +194,7 @@ else
   # Determine the filename for the release
   filename=$(echo $FRAMEWORKS_ZIP_FILENAME | sed 's/.zip/-'${publish_version}'.zip/g')
 fi
-carthage_filename=$(echo $CARTHAGE_ZIP_FILENAME | sed 's/.framework.zip/-'${publish_version}'.framework.zip/g')
+carthage_filename=$(echo $CARTHAGE_ZIP_FILENAME | sed 's/.carthage.framework.zip/-'${publish_version}'.carthage.framework.zip/g')
 
 # Upload binary to Azure Storage
 mv $FRAMEWORKS_ZIP_FILENAME $filename
