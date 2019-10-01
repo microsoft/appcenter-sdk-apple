eee8c94afe1a0b7888ae1982bb50917f94123bcf
Jae Lim <jaelim@microsoft.com>
2019-10-01 15:56:26 -0700

    Use version in dependency zip filename

--- Scripts/restore-dependencies.sh
+++ Scripts/restore-dependencies.sh
@@ -3,6 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+filename=Apple-BYOI-0.0.1.zip
 echo "Change a path to $1"
 cd $1
 if [ -d "$1/Auth0" ] && [ -d "$1/Firebase" ]; then
@@ -10,7 +11,7 @@ if [ -d "$1/Auth0" ] && [ -d "$1/Firebase" ]; then
 else
   rm -rf $1/Auth0
   rm -rf $1/Firebase
-  wget https://mobilecentersdkdev.blob.core.windows.net/sdk-dependencies/Apple-BYOI.zip
-  unzip Apple-BYOI.zip
-  rm Apple-BYOI.zip
+  wget https://mobilecentersdkdev.blob.core.windows.net/sdk-dependencies/$filename
+  unzip $filename
+  rm $filename
 fi
