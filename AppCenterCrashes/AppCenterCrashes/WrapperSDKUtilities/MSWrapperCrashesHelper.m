5d4f54fe0547c85d5640424c377e90a2579b610d
Di Hei <dihei@microsoft.com>
2019-10-28 10:44:17 -0700

    unit tests

--- AppCenterCrashes/AppCenterCrashes/WrapperSDKUtilities/MSWrapperCrashesHelper.m
+++ AppCenterCrashes/AppCenterCrashes/WrapperSDKUtilities/MSWrapperCrashesHelper.m
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#import "MSWrapperCrashesHelper.h"
 #import "MSCrashesInternal.h"
 #import "MSErrorReportPrivate.h"
-#import "MSWrapperCrashesHelper.h"
 
 @interface MSWrapperCrashesHelper ()
 
