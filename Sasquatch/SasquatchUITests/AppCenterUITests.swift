8d79d77ca14af8900d7da159d0e9cc4a9cedb7b7
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-11-18 18:15:46 +0300

    Fix UI tests on Sasquatch

--- Sasquatch/SasquatchUITests/AppCenterUITests.swift
+++ Sasquatch/SasquatchUITests/AppCenterUITests.swift
@@ -47,6 +47,9 @@ class AppCenterUITests: XCTestCase {
     // Push.
     XCTAssertFalse(app.tables["App Center"].switches["Push Enabled"].boolValue)
     
+    // Auth.
+    XCTAssertFalse(app.tables["App Center"].switches["Auth Enabled"].boolValue)
+    
     // Analytics.
     app.tabBars.buttons["Analytics"].tap()
     XCTAssertFalse(app.tables["Analytics"].switches["Set Enabled"].boolValue)
@@ -66,6 +69,9 @@ class AppCenterUITests: XCTestCase {
     // All services should be enabled.
     // Push.
     XCTAssertTrue(app.tables["App Center"].switches["Push Enabled"].boolValue)
+    
+    // Auth.
+    XCTAssertTrue(app.tables["App Center"].switches["Auth Enabled"].boolValue)
     
     // Analytics.
     app.tabBars.buttons["Analytics"].tap()
