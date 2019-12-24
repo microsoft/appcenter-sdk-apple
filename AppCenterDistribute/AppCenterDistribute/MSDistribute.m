0ae0a162f890ef04816fe1a2a361bbbb050178ea
Ilya Shumihin <53528961+ilyashumihin@users.noreply.github.com>
2019-12-24 10:37:51 +0300

    Update AppCenterDistribute/AppCenterDistribute/MSDistribute.m
    Co-Authored-By: Clement Polet <clpolet@microsoft.com>

--- AppCenterDistribute/AppCenterDistribute/MSDistribute.m
+++ AppCenterDistribute/AppCenterDistribute/MSDistribute.m
@@ -604,7 +604,8 @@ static NSString *const kMSUpdateTokenURLInvalidErrorDescFormat = @"Invalid updat
   // Retain the session.
   self.authenticationSession = session;
 
-  /* Request additional background execution time for authorization. If we authorize using third-party services (MS Authenticator)
+  /* 
+   * Request additional background execution time for authorization. If we authorize using third-party services (MS Authenticator)
    * then switching to another application will kill the current session. This line fixes this problem.
    */
   backgroundAuthSessionTask = [[MSUtility sharedApp] beginBackgroundTaskWithName:@"Safari authentication"
