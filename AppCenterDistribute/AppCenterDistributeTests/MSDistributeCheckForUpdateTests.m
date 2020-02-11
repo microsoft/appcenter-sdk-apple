5e46b912fcff56c0f4a2e0212f01a3468bfdc550
Jae Lim <jaelim@microsoft.com>
2020-02-11 10:40:03 -0800

    Add unit test skeleton for checkForUpdate API

---
+++ AppCenterDistribute/AppCenterDistributeTests/MSDistributeCheckForUpdateTests.m
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#import "MSChannelGroupProtocol.h"
+#import "MSDistributePrivate.h"
+#import "MSTestFrameworks.h"
+
+@interface MSDistributeCheckForUpdateTests : XCTestCase
+
+@end
+
+@implementation MSDistributeCheckForUpdateTests
+
+- (void)testCheckForUpdateAuthenticateWhenItHasNotAuthenticated {
+}
+
+- (void)testCheckForUpdateWhenItHasAuthenticated {
+}
+
+- (void)testCheckForUpdateEvenThoughAutomaticUpdateIsDisabled {
+}
+
+- (void)testCheckForUpdateDoesNotUpdateWhenAutomaticAuthenticationIsDisabled {
+
+  // TODO: This test should be written when a new flag is added.
+}
+
+@end
