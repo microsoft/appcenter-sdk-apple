10653be2dd54c5336416ebff1cddefa444c5c52e
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-04-10 14:59:01 +0300

    Fix the app

--- Sasquatch/SasquatchSwift-Extension/ExtensionViewController.swift
+++ Sasquatch/SasquatchSwift-Extension/ExtensionViewController.swift
@@ -22,7 +22,7 @@ class ExtensionViewController: UIViewController, NCWidgetProviding, MSCrashesDel
     let dateString = DateFormatter.localizedString(from: Date.init(), dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.medium)
     extensionLabel.text = "Run #\(dateString)"
     MSAppCenter.setLogLevel(.verbose)
-    MSCrashes.setDelegate(self)
+    MSCrashes.delegate = self
     MSAppCenter.start("238d7788-8e63-478f-a747-33444bdadbda", withServices: [MSCrashes.self])
   }
   
