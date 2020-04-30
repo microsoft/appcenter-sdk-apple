7ed9ab83cf679c0082562e8d11b33aaf06b8053b
Vladimir Vechkanov <v-vlvech@microsoft.com>
2020-04-30 11:36:17 +0400

    Refactor build xcframework script

--- Scripts/build-xcframework.sh
+++ Scripts/build-xcframework.sh
@@ -3,13 +3,17 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# Install dir will be the final output to the framework.
-# The following line create it in the root folder of the current project.
-PRODUCTS_DIR="${SRCROOT}/../AppCenter-SDK-Apple/"
-WORK_DIR="${PRODUCTS_DIR}/xcframework/"
+# Work dir is directory where all XCFramework artifacts is stored.
+WORK_DIR="${SRCROOT}/../AppCenter-SDK-Apple/xcframework"
 
+# Work dir will be the final output to the framework.
+OUTPUT="${WORK_DIR}/Output"
+
+# Clean previus XCFramework build.
 rm -rf ${PROJECT_NAME}.xcframework/
-xcodebuild -create-xcframework -framework $WORK_DIR/Release-iphoneos/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-iphonesimulator/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-appletvos/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-appletvsimulator/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-macos/${PROJECT_NAME}.framework -output "$WORK_DIR/Output/${PROJECT_NAME}.xcframework"
 
+# Build XCFramework.
+xcodebuild -create-xcframework -framework $WORK_DIR/Release-iphoneos/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-iphonesimulator/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-appletvos/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-appletvsimulator/${PROJECT_NAME}.framework -framework $WORK_DIR/Release-macos/${PROJECT_NAME}.framework -output "$OUTPUT/${PROJECT_NAME}.xcframework"
+
+# Clean build frameworks which was used to create XCFramework.
 rm -rf $WORK_DIR/Release-*/${PROJECT_NAME}.framework
-
