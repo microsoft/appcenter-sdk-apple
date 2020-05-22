ae6dc5fa534dd26a2c8068bcac115f243ba3f022
Anastasia Kubova <v-ankubo@microsoft.com>
2020-05-22 14:03:56 +0300

    update the script for build xcframework

--- Scripts/build-xcframework.sh
+++ Scripts/build-xcframework.sh
@@ -4,27 +4,26 @@
 # Licensed under the MIT License.
 
 # Work dir is directory where all XCFramework artifacts is stored.
-WORK_DIR="${SRCROOT}/../AppCenter-SDK-Apple/xcframework"
+ROOT_DIR="${SRCROOT}/../AppCenter-SDK-Apple/"
+WORK_DIR="${ROOT_DIR}/xcframework"
 
 # Work dir will be the final output to the framework.
 XC_FRAMEWORK_PATH="${WORK_DIR}/Output/${PROJECT_NAME}.xcframework"
-
-# Dir where catalyst build framewrok stored after build
-CATALYST_BUILD_DIR="build/Release-maccatalyst/"
 
 # Clean previus XCFramework build.
 rm -rf ${PROJECT_NAME}.xcframework/
 
 # Build and move mac catalyst framework
-xcodebuild -project "${SRCROOT}/${PROJECT_NAME}.xcodeproj" -configuration "Release" -scheme "${PROJECT_NAME} iOS Framework" -destination 'platform=macOS,variant=Mac Catalyst' 
-cp -R "$CATALYST_BUILD_DIR" "${XCFRAMEWORK_DIR}"
+MACCATALYST_BUILD_DIR="${ROOT_DIR}/output/${CONFIGURATION}-maccatalyst"
+if [ -d "${MACCATALYST_BUILD_DIR}" ]; then rm -Rf "${MACCATALYST_BUILD_DIR}"; fi
+xcodebuild -project "${SRCROOT}/${PROJECT_NAME}.xcodeproj" -configuration "${CONFIGURATION}" -scheme "${PROJECT_NAME} iOS Framework" -destination 'platform=macOS,variant=Mac Catalyst' CONFIGURATION_BUILD_DIR="${MACCATALYST_BUILD_DIR}"
 
 # Create a command to build XCFramework.
 for SDK in iphoneos iphonesimulator appletvos appletvsimulator macOS maccatalyst; do
-    FRAMEWORK_PATH="$WORK_DIR/Release-$SDK/${PROJECT_NAME}.framework"
-    [ -e "$FRAMEWORK_PATH" ] && XC_BUILD_COMMAND="$XC_BUILD_COMMAND -framework $FRAMEWORK_PATH";
+    FRAMEWORK_PATH="${ROOT_DIR}/output/${CONFIGURATION}-${SDK}/${PROJECT_NAME}.framework"
+    [ -e "${FRAMEWORK_PATH}" ] && XC_BUILD_COMMAND="${XC_BUILD_COMMAND} -framework ${FRAMEWORK_PATH}";
 done
-XC_BUILD_COMMAND="xcodebuild -create-xcframework $XC_BUILD_COMMAND -output $XC_FRAMEWORK_PATH"
+XC_BUILD_COMMAND="xcodebuild -create-xcframework ${XC_BUILD_COMMAND} -output ${XC_FRAMEWORK_PATH}"
 
 #Build XCFramework
 eval "$XC_BUILD_COMMAND"
