900d5f70fc928b43366930924b04d42ae32a68d3
Ivan Matkov <v-ivmatk@microsoft.com>
2020-05-27 14:35:22 +0300

    Update publish scripts

--- Scripts/build-archive.sh
+++ Scripts/build-archive.sh
@@ -3,45 +3,59 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+# Creates zip archives from frameworks.
+# Usage: build-archive.sh
+
 PROJECT_DIR="$(dirname "$0")/.."
 PRODUCT_NAME="AppCenter-SDK-Apple"
-PRODUCTS_DIR="${PROJECT_DIR}/${PRODUCT_NAME}"
-VERSION_FILE="${PROJECT_DIR}/Config/Version.xcconfig"
-VERSION=`grep "VERSION_STRING" "${VERSION_FILE}" | head -1 | awk -F "[= ]" '{print $4}'`
+PRODUCTS_DIR="$PROJECT_DIR/$PRODUCT_NAME"
 
+# Creates zip archive.
+# Usage: archive <result-name> <list-of-content>
 function archive() {
-    # Create temporary directory.
-    local TEMP_DIR=$(mktemp -d -t $1)
-    mkdir -p "${TEMP_DIR}/${PRODUCT_NAME}"
+  echo "Creating $1 archive from ${@:2}"
 
-    # Copy required files.
-    cp "${PROJECT_DIR}/LICENSE" "${TEMP_DIR}/${PRODUCT_NAME}"
-    cp "${PROJECT_DIR}/README.md" "${TEMP_DIR}/${PRODUCT_NAME}"
-    cp "${PROJECT_DIR}/CHANGELOG.md" "${TEMP_DIR}/${PRODUCT_NAME}"
-    cp -R "${@:2}" "${TEMP_DIR}/${PRODUCT_NAME}"
+  # Create temporary directory.
+  local temp_dir=$(mktemp -d -t $1)
+  mkdir -p "$temp_dir/$PRODUCT_NAME"
 
-    # Remmove old archive if exists.
-    if [ -f "${PRODUCTS_DIR}/$1" ]; then
-        rm "${PRODUCTS_DIR}/$1"
-    fi
+  # Copy required files.
+  cp "$PROJECT_DIR/LICENSE" "$temp_dir/$PRODUCT_NAME"
+  cp "$PROJECT_DIR/README.md" "$temp_dir/$PRODUCT_NAME"
+  cp "$PROJECT_DIR/CHANGELOG.md" "$temp_dir/$PRODUCT_NAME"
+  cp -R "${@:2}" "$temp_dir/$PRODUCT_NAME"
 
-    # Zip content.
-    (cd "${TEMP_DIR}" && zip -9 -r -y "$1" "${PRODUCT_NAME}")
-    mv "${TEMP_DIR}/$1" "${PRODUCTS_DIR}"
+  # Remmove old archive if exists.
+  if [ -f "$PRODUCTS_DIR/$1" ]; then
+    rm "$PRODUCTS_DIR/$1"
+  fi
 
-    # Remove temporary directory.
-    rm -rf "${TEMP_DIR}"
+  # Zip content.
+  (cd "$temp_dir" && zip -9 -r -y "$1" "$PRODUCT_NAME")
+  mv "$temp_dir/$1" "$PRODUCTS_DIR"
+
+  # Remove temporary directory.
+  rm -rf "$temp_dir"
 }
 
-# Archive fat frameworks for CocoaPods.
-archive "${PRODUCT_NAME}-${VERSION}.zip" "${PRODUCT_NAME}/iOS" "${PRODUCT_NAME}/macOS" "${PRODUCT_NAME}/tvOS"
+# Check if the frameworks are already built.
+if [ ! -d "$PRODUCTS_DIR/iOS" ] || [ ! -d "$PRODUCTS_DIR/macOS" ] || [ ! -d "$PRODUCTS_DIR/macOS" ] || [ ! -d "$PRODUCTS_DIR/XCFramework" ]; then
+  echo "Cannot find frameworks to archive"
+  exit 1
+fi
 
-# Move Distribute resources
-mv "${PRODUCTS_DIR}/iOS/AppCenterDistributeResources.bundle" \
-    "${PRODUCTS_DIR}/iOS/AppCenterDistribute.framework/AppCenterDistributeResources.bundle"
+# Get current version.
+VERSION="$($(dirname "$0")/framework-version.sh)"
+
+# Archive fat frameworks for CocoaPods.
+archive "$PRODUCT_NAME-${VERSION}.zip" "$PRODUCT_NAME/iOS" "$PRODUCT_NAME/macOS" "$PRODUCT_NAME/tvOS"
+
+# Move Distribute resources.
+mv "$PRODUCTS_DIR/iOS/AppCenterDistributeResources.bundle" \
+    "$PRODUCTS_DIR/iOS/AppCenterDistribute.framework/AppCenterDistributeResources.bundle"
 
 # Archive fat frameworks for Carthage.
-archive "${PRODUCT_NAME}-${VERSION}.carthage.framework.zip" "${PRODUCT_NAME}/iOS" "${PRODUCT_NAME}/macOS" "${PRODUCT_NAME}/tvOS"
+archive "$PRODUCT_NAME-${VERSION}.carthage.framework.zip" "$PRODUCT_NAME/iOS" "$PRODUCT_NAME/macOS" "$PRODUCT_NAME/tvOS"
 
 # Archive XCFrameworks.
-archive "${PRODUCT_NAME}-XCFramework-${VERSION}.zip" $(ls -d "${PRODUCT_NAME}/XCFramework"/*)
+archive "$PRODUCT_NAME-XCFramework-${VERSION}.zip" $(ls -d "$PRODUCT_NAME/XCFramework"/*)
