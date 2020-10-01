efb506df91d0917450573a720aafd370b108a1bf
Anastasia Kubova <v-ankubo@microsoft.com>
2020-10-01 14:44:44 +0300

    Rename App Center Crashes module

---
+++ AppCenterCrashes/AppCenterCrashesTests/MSACMockCrashesDelegate.h
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#import <Foundation/Foundation.h>
+#import "MSACCrashesDelegate.h"
+
+@interface MSACMockCrashesDelegate : NSObject <MSACCrashesDelegate>
+@end
