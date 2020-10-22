4f0edd0485f1ec339575fed2c61212c98b1cdf11
Vladimir Vechkanov <v-vlvech@microsoft.com>
2020-10-22 15:33:09 +0400

    Fix review comments

--- Scripts/copy-products.sh
+++ Scripts/copy-products.sh
@@ -7,7 +7,7 @@ set -e
 # Clean
 rm -rf "$BUILT_PRODUCTS_DIR"
 mkdir -p "$BUILT_PRODUCTS_DIR"
-echo "BUILT_PRODUCTS_DIR  $BUILT_PRODUCTS_DIR"
+
 # Universal frameworks
 mkdir -p "$BUILT_PRODUCTS_DIR/iOS Framework"
 cp -R "$BUILD_DIR/$CONFIGURATION-iphoneuniversal/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/iOS Framework"
@@ -18,14 +18,6 @@ cp -R "$BUILD_DIR/$CONFIGURATION-appletvuniversal/$PROJECT_NAME.framework" "$BUI
 # Dynamic macOS framework
 mkdir -p "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
 cp -R "$BUILD_DIR/$CONFIGURATION-macosx/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
-#cp -R "$BUILD_DIR/$CONFIGURATION-macosx/$PROJECT_NAME.framework.dSYM" "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
 
 # XCFrameowrk
 cp -R "$BUILD_DIR/$CONFIGURATION-xcframework/$PROJECT_NAME.xcframework" "$BUILT_PRODUCTS_DIR"
-
-# Static libraries
-#mkdir -p "$BUILT_PRODUCTS_DIR/Static/include"
-#cp -R "$BUILD_DIR/$CONFIGURATION-macosx/$PROJECT_NAME.framework/Headers/." "$BUILT_PRODUCTS_DIR/Static/include/"
-#cp "$BUILD_DIR/$CONFIGURATION-iphoneuniversal/lib$PROJECT_NAME.a" "$BUILT_PRODUCTS_DIR/Static/lib$PROJECT_NAME-iOS.a"
-#cp "$BUILD_DIR/$CONFIGURATION-appletvuniversal/lib$PROJECT_NAME.a" "$BUILT_PRODUCTS_DIR/Static/lib$PROJECT_NAME-tvOS.a"
-#cp "$BUILD_DIR/$CONFIGURATION-macosx/lib$PROJECT_NAME.a" "$BUILT_PRODUCTS_DIR/Static/lib$PROJECT_NAME-MacOSX-Static.a"
