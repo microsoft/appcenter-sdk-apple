27bd8086845eb230406ce3ab8f1a39147cd7d712
Ivan Matkov <v-ivmatk@microsoft.com>
2020-10-26 14:27:36 +0300

    Add configs and fix xcframework combining

--- Scripts/combine-xcframework.sh
+++ Scripts/combine-xcframework.sh
@@ -3,17 +3,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# The directory for final output of the framework.
-PRODUCTS_DIR="${SRCROOT}/../AppCenter-SDK-Apple/XCFramework"
-
-# Build result paths.
-SCRIPT_BUILD_DIR="${SRCROOT}/build"
-
 # Cleaning the previous builds.
-rm -rf "${PRODUCTS_DIR}/${PROJECT_NAME}.xcframework"
-
-# Creates the final product folder.
-mkdir -p "${PRODUCTS_DIR}"
+rm -rf "${BUILT_PRODUCTS_DIR}/${PROJECT_NAME}.xcframework"
 
 # Create a command to build XCFramework.
 function add_framework() {
@@ -22,18 +13,18 @@ function add_framework() {
 }
 add_framework "${BUILD_DIR}/${CONFIGURATION}"
 for SDK in iphoneos iphonesimulator appletvos appletvsimulator maccatalyst; do
-  add_framework "${SCRIPT_BUILD_DIR}/${CONFIGURATION}-${SDK}"
+  add_framework "${BUILD_DIR}/${CONFIGURATION}-${SDK}"
 done
 
 # Build XCFramework.
-xcodebuild -create-xcframework "${XC_FRAMEWORKS[@]}" -output "${PRODUCTS_DIR}/${PROJECT_NAME}.xcframework"
+xcodebuild -create-xcframework "${XC_FRAMEWORKS[@]}" -output "${BUILT_PRODUCTS_DIR}/${PROJECT_NAME}.xcframework"
 
 # Copy the resource bundle.
 BUNDLE_NAME="${PROJECT_NAME}Resources.bundle"
-BUNDLE_PATH="${SCRIPT_BUILD_DIR}/${CONFIGURATION}-iphoneos/${BUNDLE_NAME}"
+BUNDLE_PATH="${BUILD_DIR}/${CONFIGURATION}-iphoneos/${BUNDLE_NAME}"
 if [ -e "${BUNDLE_PATH}" ]; then
-  rm -rf "${PRODUCTS_DIR}/${BUNDLE_NAME}"
-  cp -Rv "${BUNDLE_PATH}" "${PRODUCTS_DIR}"
+  rm -rf "${BUILT_PRODUCTS_DIR}/${BUNDLE_NAME}"
+  cp -Rv "${BUNDLE_PATH}" "${BUILT_PRODUCTS_DIR}"
 fi
 echo "Cleanup resource bundles inside frameworks"
-rm -rfv "${PRODUCTS_DIR}/${PROJECT_NAME}.xcframework/*/${PRODUCT_NAME}.framework/${BUNDLE_NAME}"
+rm -rfv "${BUILT_PRODUCTS_DIR}/${PROJECT_NAME}.xcframework/*/${PRODUCT_NAME}.framework/${BUNDLE_NAME}"
