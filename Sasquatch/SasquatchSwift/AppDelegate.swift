89f28f7b2a4b9c4d04938039965535259b628950
Anatoly Pristensky <84129877+AnatolyPristensky@users.noreply.github.com>
2021-07-09 09:35:16 +0300

    Resolved comment

--- Sasquatch/SasquatchSwift/AppDelegate.swift
+++ Sasquatch/SasquatchSwift/AppDelegate.swift
@@ -37,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CrashesDelegate, CLLocati
     while let newTopController = topController.presentedViewController {
       topController = newTopController
     }
-  return topController
+    return topController
   }
     
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
