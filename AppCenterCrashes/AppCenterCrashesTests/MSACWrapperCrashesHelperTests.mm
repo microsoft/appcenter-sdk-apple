7a1b0e06804167d33556b1b751c8a672364b3f82
AnatolyPristensky <84129877+AnatolyPristensky@users.noreply.github.com>
2021-09-15 11:27:20 +0300

    Added test

--- AppCenterCrashes/AppCenterCrashesTests/MSACWrapperCrashesHelperTests.mm
+++ AppCenterCrashes/AppCenterCrashesTests/MSACWrapperCrashesHelperTests.mm
@@ -301,4 +301,19 @@ static NSString *const kMSACTypeHandledError = @"handledError";
   XCTAssertEqualObjects(actualErrorId, errorId);
 }
 
+- (void)testBuildHandledErrorReportWithErrorID {
+  MSACErrorReport *report = [MSACWrapperCrashesHelper buildHandledErrorReportWithErrorID:@"errorID"];
+  XCTAssertNil(report.codeType);
+  XCTAssertNil(report.archName);
+  XCTAssertNil(report.applicationPath);
+  XCTAssertNil(report.threads);
+  XCTAssertNil(report.binaries);
+  XCTAssertNil(report.reporterKey);
+  XCTAssertNil(report.signal);
+  XCTAssertNil(report.exceptionName);
+  XCTAssertNil(report.exceptionReason);
+  XCTAssertEqualObjects(report.appStartTime, [MSACCrashes sharedInstance].appStartTime);
+  XCTAssertNotNil(report.appErrorTime);
+}
+
 @end
