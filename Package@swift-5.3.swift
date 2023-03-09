788afb9af24e217380f46900957a5d41b607023a
Anatoly Pristensky <v-anatpr@microsoft.com>
2023-03-09 12:09:37 +0100

    Bump version to 5.0.2 (#2471)

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -79,7 +79,7 @@ let package = Package(
             exclude: ["Support"],
             cSettings: {
                 var settings: [CSetting] = [
-                    .define("APP_CENTER_C_VERSION", to:"\"5.0.1\""),
+                    .define("APP_CENTER_C_VERSION", to:"\"5.0.2\""),
                     .define("APP_CENTER_C_BUILD", to: "\"1\"")
                 ]
                 settings.append(contentsOf: cHeaderSearchPaths)
